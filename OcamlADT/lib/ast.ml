855c79e9a43df73cd012932707dc2a28e91ebe62
qrutyy <qrutyq@gmail.com>
2024-09-25 22:12:47 +0300

    fix: resolve mutual definitions

--- OcamlADT/lib/ast.ml
+++ OcamlADT/lib/ast.ml
@@ -5,77 +5,82 @@ type constant =
 [@@deriving eq, show { with_path = false }]
 (* Generates fun's for equality check, string type output and removing the full path to module*)
 
-type binop = 
-| Add (* + *)
-| Sub (* - *)
-| Mul (* * *)
-| Div (* / *)
-| Eq (* = *)
-| And (* && *)
-| Or (* || *)
-| Cons (* :: *)
-| Neq (* <> *)
-| Les (* < *)
-| Leq (* <= *)
-| Gre (* > *)
-| Geq (* >= *)
+type binop =
+  | Add (* + *)
+  | Sub (* - *)
+  | Mul (* * *)
+  | Div (* / *)
+  | Eq (* = *)
+  | And (* && *)
+  | Or (* || *)
+  | Cons (* :: *)
+  | Neq (* <> *)
+  | Les (* < *)
+  | Leq (* <= *)
+  | Gre (* > *)
+  | Geq (* >= *)
 [@@deriving eq, show { with_path = false }]
 
-type decl_type = 
-| Type_int
-| Type_string
-| Type_bool
-| Type_fun of decl_type * decl_type
-| Type_var of string
-| Type_list of decl_type
-| Type_tuple of decl_type list
-| Type_variant of decl_name * (decl_name * decl_type) list (* TODO *)
+type decl_name =
+  | Up_name of string (* vars / functions*)
+  | Down_name of string (* types, constructors*)
 [@@deriving eq, show { with_path = false }]
 
-type decl_name = 
-| Up_name of string (* vars / functions*)
-| Down_name of string (* types, constructors*)
+type decl_type =
+  | Type_int
+  | Type_string
+  | Type_bool
+  | Type_fun of decl_type * decl_type
+  | Type_var of string
+  | Type_list of decl_type
+  | Type_tuple of decl_type list
+  | Type_variant of decl_name * (decl_name * decl_type) list (* TODO *)
 [@@deriving eq, show { with_path = false }]
 
-type pattern = 
-| Pat_any
-| Pat_var of string decl_name
-| Pat_alias of pattern * string decl_name
-| Pat_constant of constant
-| Pat_interval of constant * constant
-| Pat_tuple of pattern list
-| Pat_construct (* Do we need it?*)
+type pattern =
+  | Pat_any
+  | Pat_var of decl_name
+  | Pat_alias of pattern * decl_name
+  | Pat_constant of constant
+  | Pat_interval of constant * constant
+  | Pat_tuple of pattern list
+  | Pat_construct (* Do we need it?*)
 [@@deriving eq, show { with_path = false }]
 
-type rec_flag = 
-|	Nonrecursive
-|	Recursive
+type rec_flag =
+  | Nonrecursive
+  | Recursive
 
-type value_binding = 
-  {
-    pat: pattern;
-    expr: decl_expr;
+type decl_expr =
+  | Exp_constant of constant (** Expressions constant such as [1], ['a'], ["true"]**)
+  | Exp_emptyList
+  | Exp_var of decl_name
+  | Exp_tuple of decl_expr list (** can be changed to [expr*expr*(expr list)] **)
+  | Exp_function of case list
+  | Exp_fun of pattern list * decl_expr
+  | Exp_apply of decl_expr * decl_expr
+  | Exp_match of decl_expr * case list
+  | Exp_try of decl_expr * case list
+  | Exp_if of decl_expr * decl_expr * decl_expr option
+  | Exp_let of rec_flag * value_binding list * decl_expr
+  | Exp_binop of binop * decl_expr * decl_expr
+[@@deriving eq, show { with_path = false }]
+
+and value_binding =
+  { pat : pattern
+  ; expr : decl_expr
   }
 
-type case =
-{
-  left: pattern;
-  right: decl_expr;
-}
+and case =
+  { left : pattern
+  ; right : decl_expr
+  }
 
-type decl_expr = 
-| Exp_constant of constant (** Expressions constant such as [1], ['a'], ["true"]**)
-| Exp_emptyList
-| Exp_var of decl_name
-| Exp_tuple of decl_expr list (** can be changed to [expr*expr*(expr list)] **)
-| Exp_function of case list
-| Exp_fun of pattern list * decl_expr
-| Exp_apply of decl_expr * decl_expr 
-| Exp_match of decl_expr * case list
-| Exp_try of decl_expr * case list
-| Exp_if of decl_expr * decl_expr * decl_expr option
-| Exp_let of rec_flag * value_binding list * decl_expr
-| Exp_binop of binop * decl_expr * decl_expr
-[@@deriving eq, show { with_path = false }]
+(*
+   Example of factorial ast
 
-(* I guess ?? should be replaced with smth near to pattern... *)
+Exp_let(
+  rec_flag(Recursive), 
+  value_binding() здесь становится непонято
+]
+*)
