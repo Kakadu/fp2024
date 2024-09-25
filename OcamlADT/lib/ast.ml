c2a5ac48c7a7191cb4d3d855b90a725da07243b4
qrutyy <qrutyq@gmail.com>
2024-09-25 22:09:25 +0300

    feat: add type_decl and fix let

--- OcamlADT/lib/ast.ml
+++ OcamlADT/lib/ast.ml
@@ -38,9 +38,9 @@ type decl_type =
 | TString
 | TBool
 | TFun of decl_type * decl_type
-| TVar of decl_type
+| TVar of string
 | TList of decl_type
-| TTuple of decl_type * decl_type (*list of decl_type, im dumb*)
+| TTuple of decl_type list
 
 let tint = TInt
 let tstring = TString
@@ -57,7 +57,14 @@ type decl_name =
 let upname str = UpName str
 let downname str = DownName str
 
-type match_pattern (* TODO *)
+type pattern = 
+| Ppat_any
+| Ppat_var of string decl_name
+| Ppat_alias of pattern * string decl_name
+| Ppat_constant of constant
+| Ppat_interval of constant * constant
+| Ppat_tuple of pattern list
+| Ppat_construct (* Do we need it?*)
 
 type rec_flag = 
 |	Nonrecursive
@@ -85,7 +92,7 @@ type decl_expr =
 | Exp_apply of decl_expr * decl_expr 
 | Exp_match of decl_expr * case list
 | Exp_try of decl_expr * case list
-| ExpIf of decl_expr * decl_expr * decl_expr 
+| ExpIf of decl_expr * decl_expr * decl_expr option
 | Exp_Let of rec_flag * value_binding list * decl_expr
 | ExpBinOp of binop * decl_expr * decl_expr
 
@@ -96,12 +103,14 @@ let expemptylist = ExpEmptyList
 let expvar decl_name = ExpVar (decl_name)
 let exptuple declaration_expression = ExpTuple declaration_expression
 let expfun ??
-let expmatch decl_exp (match_pattern * decl_exp) list = ExpMatch (decl_exp, match_pattern, decl_exp)
+let expmatch decl_exp (match_pattern * decl_exp) list = ExpMatch (decl_exp, match_pattern, decl_exp) (* Matched value, pattern, expresion after pattern match*)
 let expif decl_exp1 decl_exp2 decl_exp3 = ExpIf (decl_exp1, decl_exp2, decl_exp3) (* Statement is also a expression *)
-let explet decl_name decl_exp = ExpLet (decl_name, decl_exp)
+let explet decl_name decl_exp1 decl_exp2 = ExpLet (decl_name, decl_exp1, decl_exp2)
 let expbinop binop decl_exp decl_exp = ExpBinOp (binop, decl_exp, decl_exp)
 
 (* I guess ?? should be replaced with smth near to pattern... *)
 
 type decl_rec (* TODO *)
-type decl_type (* TODO *)
+
+(* ADT specific *)
+type type_decl = decl_name * (decl_name * decl_type) list
