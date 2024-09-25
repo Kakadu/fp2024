dcf015b3648bbceb9aeea51ce2c4eaf43b091067
Ilia Suponev <suponeff.i@gmail.com>
2024-09-25 20:24:51 +0300

    feat: initialize project
    Initialize project by task: OCaml + polymorphic variants types.
    Add implementation of miniML's AST.
    Add implementation of factorial function AST representation.
    
    Signed-off-by: Ilia Suponev <suponeff.i@gmail.com>

---
+++ OCamlPolymorphicVariantsTypes/bin/REPL.ml
@@ -0,0 +1,45 @@
+(** Copyright 2021-2024, Ilia Suponev *)
+(** SPDX-License-Identifier: LGPL-3.0-or-later *)
+
+open Miniml.Ast
+
+let () = 
+  let fact_ast: program = "factorial_example.ml", [
+    DefinitionStatement(
+      Recursive,
+      Global, 
+      "factorial", 
+      ["n"], 
+      [
+        IfStatement(
+          BinaryExpression(
+            Variable "n",
+            Gt,
+            Const (IntLiteral 1)
+          ),
+          [
+            EvalStatement(
+              BinaryExpression(
+                Variable "n",
+                Multiply,
+                Apply(
+                  "factorial",
+                  [
+                    BinaryExpression(
+                      Variable "n",
+                      BinaryMinus,
+                      Const (IntLiteral 1)
+                    )
+                  ]
+                )
+              )
+            )
+          ],
+          [
+            EvalStatement(Const (IntLiteral 1))
+          ]
+        )
+      ]
+      )
+  ] in
+  print_endline (show_program fact_ast)
