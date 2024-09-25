dc4112f654bd541abea93aa61766656f6b52f941
qrutyy <qrutyq@gmail.com>
2024-09-25 22:09:50 +0300

    feat: add types deriving

--- OcamlADT/lib/ast.ml
+++ OcamlADT/lib/ast.ml
@@ -2,6 +2,8 @@ type constant =
   | Const_integer of int (** integer as [52] **)
   | Const_char of char (** char as ['w'] **)
   | Const_string of string (** string as ["Kakadu"] **)
+[@@deriving eq, show { with_path = false }]
+(* Generates fun's for equality check, string type output and removing the full path to module*)
 
 type binop = 
 | Add (* + *)
@@ -17,21 +19,7 @@ type binop =
 | Leq (* <= *)
 | Gre (* > *)
 | Geq (* >= *)
-
-(**Misha we can use  https://github.com/ocaml-ppx/ppx_deriving for generating this code if we are nerds**)
-let bop_add = Add
-let bop_sub = Sub
-let bop_mul = Mul
-let bop_div = Div
-let bop_eq = Eq
-let bop_and = And 
-let bop_or = Or
-let bop_cons = Cons
-let bop_neq = Neq
-let bio_les = Les
-let bio_leq = Leq
-let bio_gre = Gre
-let bio_geq = Geq
+[@@deriving eq, show { with_path = false }]
 
 type decl_type = 
 | TInt
@@ -41,21 +29,12 @@ type decl_type =
 | TVar of string
 | TList of decl_type
 | TTuple of decl_type list
-
-let tint = TInt
-let tstring = TString
-let tbool = TBool
-let tfun = TFun
-let tvar = TVar (*drozhashiaia*)
-let tlist = TList
-let ttuple = TTuple
+[@@deriving eq, show { with_path = false }]
 
 type decl_name = 
 | UpName of string (* vars / functions*)
 | DownName of string (* types, constructors*)
-
-let upname str = UpName str
-let downname str = DownName str
+[@@deriving eq, show { with_path = false }]
 
 type pattern = 
 | Ppat_any
@@ -65,6 +44,7 @@ type pattern =
 | Ppat_interval of constant * constant
 | Ppat_tuple of pattern list
 | Ppat_construct (* Do we need it?*)
+[@@deriving eq, show { with_path = false }]
 
 type rec_flag = 
 |	Nonrecursive
@@ -95,22 +75,13 @@ type decl_expr =
 | ExpIf of decl_expr * decl_expr * decl_expr option
 | Exp_Let of rec_flag * value_binding list * decl_expr
 | ExpBinOp of binop * decl_expr * decl_expr
-
-let expint num = ExpInt num
-let expstring str = ExpString str
-let expbool bool = ExpBool bool
-let expemptylist = ExpEmptyList
-let expvar decl_name = ExpVar (decl_name)
-let exptuple declaration_expression = ExpTuple declaration_expression
-let expfun ??
-let expmatch decl_exp (match_pattern * decl_exp) list = ExpMatch (decl_exp, match_pattern, decl_exp) (* Matched value, pattern, expresion after pattern match*)
-let expif decl_exp1 decl_exp2 decl_exp3 = ExpIf (decl_exp1, decl_exp2, decl_exp3) (* Statement is also a expression *)
-let explet decl_name decl_exp1 decl_exp2 = ExpLet (decl_name, decl_exp1, decl_exp2)
-let expbinop binop decl_exp decl_exp = ExpBinOp (binop, decl_exp, decl_exp)
+[@@deriving eq, show { with_path = false }]
 
 (* I guess ?? should be replaced with smth near to pattern... *)
 
 type decl_rec (* TODO *)
+[@@deriving eq, show { with_path = false }]
 
 (* ADT specific *)
 type type_decl = decl_name * (decl_name * decl_type) list
+[@@deriving eq, show { with_path = false }]
