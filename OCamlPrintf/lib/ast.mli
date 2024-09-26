6e40cb383f8e28bd9d2a64b8d2fdf43bef4cb275
Maxim Rodionov <rodionov.maxim05@gmail.com>
2024-09-26 21:52:47 +0300

    chore: fix linter's comments and delete unnecessary file
    Signed-off-by: Maxim Rodionov <rodionov.maxim05@gmail.com>

--- OCamlPrintf/lib/ast.mli
+++ OCamlPrintf/lib/ast.mli
@@ -1,10 +1,6 @@
-[@@@ocaml.text "/*"]
-
 (** Copyright 2024-2025, Friend-zva, RodionovMaxim05 *)
 
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
-
-[@@@ocaml.text "/*"]
 
 (** Identifier *)
 type ident = string
