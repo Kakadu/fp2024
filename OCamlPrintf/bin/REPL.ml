11e4d91edee1f8e71c3d1e212c9c4b42f4859533
Maxim Rodionov <rodionov.maxim05@gmail.com>
2024-09-28 22:48:07 +0300

    fix: an example of representing a factorial check by compiler
    Signed-off-by: Maxim Rodionov <rodionov.maxim05@gmail.com>

--- OCamlPrintf/bin/REPL.ml
+++ OCamlPrintf/bin/REPL.ml
@@ -2,45 +2,32 @@
 
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
+open Ocaml_printf_lib.Ast
+
 let () =
-  print_endline
-    {|"let rec factorial n = if n <= 1 then 1 else n * factorial (n - 1)"
-        (Str_value(
-          Recursive,
-          {
-            pat = (Pat_var "factorial");
-            exp = (
-              Exp_fun(
-                Pat_var "n",
-                Exp_ifelsethen(
-                  Exp_apply(
-                    Exp_apply(
-                      Exp_ident "<=",
-                      Exp_ident "n"
-                    ),
-                    Exp_constant(Const_integer "1")
-                  ),
-                  Exp_ident "1",
-                  Some(Exp_apply(
-                    Exp_apply(
-                      Exp_ident "*",
-                      Exp_ident "n"
-                    ),
-                    Exp_apply(
-                      Exp_ident "factorial",
-                      Exp_apply(
-                        Exp_apply(
-                          Exp_ident "-",
-                          Exp_ident "n"
-                        ),
-                        Exp_constant(Const_integer "1")
-                      )
-                    )
-                  ))
-                )
-              )
-            )
-          }
-        ))
-    |}
+  let fact_ast : structure =
+    [ Str_value
+        ( Recursive
+        , [ { pat = Pat_var "factorial"
+            ; exp =
+                Exp_fun
+                  ( Pat_var "n"
+                  , Exp_ifthenelse
+                      ( Exp_apply
+                          ( Exp_apply (Exp_ident "<=", Exp_ident "n")
+                          , Exp_constant (Const_integer 1) )
+                      , Exp_constant (Const_integer 1)
+                      , Some
+                          (Exp_apply
+                             ( Exp_apply (Exp_ident "*", Exp_ident "n")
+                             , Exp_apply
+                                 ( Exp_ident "factorial"
+                                 , Exp_apply
+                                     ( Exp_apply (Exp_ident "-", Exp_ident "n")
+                                     , Exp_constant (Const_integer 1) ) ) )) ) )
+            }
+          ] )
+    ]
+  in
+  print_endline (show_structure fact_ast)
 ;;
