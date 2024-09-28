01e53480ddbb44f92021dd4accac0c2f32df9a10
qrutyy <qrutyq@gmail.com>
2024-09-28 14:30:48 +0300

    fix: add program type expr in draft

--- OcamlADT/bin/REPL.ml
+++ OcamlADT/bin/REPL.ml
@@ -2,31 +2,34 @@
 
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
-open Ocamladt
+open OcamlADT.Lib
 
-
-let factorial_ast = 
-  Exp_let (
-    Recursive, 
-    [ 
-      { pat = Pat_var (Up_name "fact");
-        expr = Exp_fun (
-          [Pat_var (Up_name "n")],
-          Exp_if (
-            Exp_binop (Eq, Exp_var (Up_name "n"), Exp_constant (Const_integer 0)),
-            Exp_constant (Const_integer 1 ),
-            Some (
-              Exp_binop (
-                Mul,
-                Exp_var (Up_name "n"),
-                Exp_apply (Exp_var (Up_name "fact"),
-                  Exp_binop(Sub, Exp_var (Up_name "n"), Exp_constant (Const_integer 1))
+let () = 
+  let factorial_ast: program = 
+    (Exp_let (
+      Recursive, 
+      [
+        { pat = Pat_var (Up_name "fact");
+          expr = Exp_fun (
+            [Pat_var (Up_name "n")],
+            Exp_if (
+              Exp_binop (Eq, Exp_var (Up_name "n"), Exp_constant (Const_integer 0)),
+              Exp_constant (Const_integer 1 ),
+              Some (
+                Exp_binop (
+                  Mul,
+                  Exp_var (Up_name "n"),
+                  Exp_apply (Exp_var (Up_name "fact"),
+                    Exp_binop(Sub, Exp_var (Up_name "n"), Exp_constant (Const_integer 1))
+                  )
                 )
               )
             )
           )
-        )
-      }
-    ], 
-    Exp_var (Up_name "fact")
-  )
+        }
+      ], 
+      Exp_var (Up_name "fact"))
+    )
+  in
+  print_endline(show_program factorial_ast)
+;;
