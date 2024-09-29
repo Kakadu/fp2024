ed94889b32b2dd563d590090f0750a6d4911a9d3
Viacheslav Sidorov <me@xamelllion.ru>
2024-09-29 19:20:06 +0300

    feat: added few more expressions

--- OcamlRV/lib/ast.ml
+++ OcamlRV/lib/ast.ml
@@ -1,4 +1,4 @@
-(** Copyright 2024-2025, Viacheslav Sidorov *)
+(** Copyright 2024-2025, Viacheslav Sidorov and Danila Rudnev-Stepanyan *)
 
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
@@ -40,7 +40,8 @@ type literal =
   | IntLiteral of int (* 123 *)
   | BoolLiteral of bool (* true | false *)
   | StringLiteral of string (* "string" *)
-  | UnitLiteral (* () value *)
+  | UnitLiteral (* () *)
+  | NilLiteral (* [] *)
 [@@deriving show { with_path = false }]
 
 type pattern =
@@ -49,6 +50,7 @@ type pattern =
   | PVar of identifier (* x *)
   | PTuple of pattern list (* p_1 ,..., p_n *)
   | PCons of pattern * pattern (* p1::p2 *)
+  | PPoly of identifier * pattern option (* `Tag p *)
 [@@deriving show { with_path = false }]
 
 type expression =
@@ -57,20 +59,24 @@ type expression =
   | ExprBinOperation of binary_operator * expression * expression (* 1 + 1 | 2 * 2 *)
   | ExprUnOperation of unary_operator * expression (* -x | not true *)
   | ExprIf of expression * expression * expression (* if expr1 then expr2 else expr3 *)
+  | ExprMatch of expression * case list (* match e with p_1 -> e_1 |...| p_n -> e_n *)
   | ExprLet of rec_flag * binding * expression (* let x = expr1 in expr2 *)
   | ExprApply of identifier * expression list (* fact n *)
   | ExprTuple of expression list (* 1, 2, 3 *)
+  | ExprCons of expression * expression (* t::tl *)
+  | ExprPoly of identifier * expression option (* `Tag expr *)
   | ExprDefinition of
       placement
       * rec_flag
       * identifier
       * identifier list
-      * expression list (* let [rec] f = (..) *)
+      * expression list (* let `rec` f a b = [ some_expressions ] *)
 [@@deriving show { with_path = false }]
 
 (* Used in `let` expr *)
 and binding = pattern * expression [@@deriving show { with_path = false }]
 
 (* Used in `match` expr *)
-type case = pattern * expression [@@deriving show { with_path = false }]
+and case = pattern * expression [@@deriving show { with_path = false }]
+
 type program = expression list [@@deriving show { with_path = false }]
