26faf3698c91df2d98251d06d7c02b24d6fd9c10
Viacheslav Sidorov <me@xamelllion.ru>
2024-09-29 14:42:40 +0300

    feat: added AST mvp

--- OcamlRV/bin/main.ml
+++ OcamlRV/bin/main.ml
@@ -1,3 +1,32 @@
-let () = print_endline "Hello world."
+(** Copyright 2024-2025, Viacheslav Sidorov *)
+
+(** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
 open OCamlRV_lib.Ast
+
+(* Factorial example *)
+(* let rec fact n = if n <= 1 then 1 else n * fact (n - 1);; *)
+
+let () =
+  let fact_ast : program =
+    [ ExprDefinition
+        ( Global
+        , Rec
+        , "fact"
+        , [ "n" ]
+        , [ ExprIf
+              ( ExprBinOperation (Lte, ExprVariable "n", ExprLiteral (IntLiteral 1))
+              , ExprLiteral (IntLiteral 1)
+              , ExprBinOperation
+                  ( Mul
+                  , ExprVariable "n"
+                  , ExprApply
+                      ( "fact"
+                      , [ ExprBinOperation
+                            (Sub, ExprVariable "n", ExprLiteral (IntLiteral 1))
+                        ] ) ) )
+          ] )
+    ]
+  in
+  print_endline (show_program fact_ast)
+;;
--- OcamlRV/lib/ast.ml
+++ OcamlRV/lib/ast.ml
@@ -1,4 +1,11 @@
-type bin_op =
+(** Copyright 2024-2025, Viacheslav Sidorov *)
+
+(** SPDX-License-Identifier: LGPL-3.0-or-later *)
+
+(* [a-zA-Z_0-9] *)
+type identifier = string [@@deriving show { with_path = false }]
+
+type binary_operator =
   | Add (* + *)
   | Sub (* - *)
   | Mul (* * *)
@@ -11,3 +18,59 @@ type bin_op =
   | Gte (* >= *)
   | And (* && *)
   | Or (* || *)
+[@@deriving show { with_path = false }]
+
+type unary_operator =
+  | UnPlus (* + *)
+  | UnMinus (* - *)
+  | UnNeg (* not *)
+[@@deriving show { with_path = false }]
+
+type rec_flag =
+  | NonRec (* let a *)
+  | Rec (* let rec a *)
+[@@deriving show { with_path = false }]
+
+type placement =
+  | Global
+  | Inline of identifier
+[@@deriving show { with_path = false }]
+
+type literal =
+  | IntLiteral of int (* 123 *)
+  | BoolLiteral of bool (* true | false *)
+  | StringLiteral of string (* "string" *)
+  | UnitLiteral (* () value *)
+[@@deriving show { with_path = false }]
+
+type pattern =
+  | PAny (* _ *)
+  | PLiteral of literal (* 123, true, "string" *)
+  | PVar of identifier (* x *)
+  | PTuple of pattern list (* p_1 ,..., p_n *)
+  | PCons of pattern * pattern (* p1::p2 *)
+[@@deriving show { with_path = false }]
+
+type expression =
+  | ExprVariable of identifier (* x | y | z*)
+  | ExprLiteral of literal (* 123 | true | "string" *)
+  | ExprBinOperation of binary_operator * expression * expression (* 1 + 1 | 2 * 2 *)
+  | ExprUnOperation of unary_operator * expression (* -x | not true *)
+  | ExprIf of expression * expression * expression (* if expr1 then expr2 else expr3 *)
+  | ExprLet of rec_flag * binding * expression (* let x = expr1 in expr2 *)
+  | ExprApply of identifier * expression list (* fact n *)
+  | ExprTuple of expression list (* 1, 2, 3 *)
+  | ExprDefinition of
+      placement
+      * rec_flag
+      * identifier
+      * identifier list
+      * expression list (* let [rec] f = (..) *)
+[@@deriving show { with_path = false }]
+
+(* Used in `let` expr *)
+and binding = pattern * expression [@@deriving show { with_path = false }]
+
+(* Used in `match` expr *)
+type case = pattern * expression [@@deriving show { with_path = false }]
+type program = expression list [@@deriving show { with_path = false }]
