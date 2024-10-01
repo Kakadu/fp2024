39a39b36ffb2283a643ebe025c9271cdfeb7b5fe
Ksenia Kotelnikova <xeniia.ka@gmail.com>
2024-10-01 10:04:07 +0300

    feat: add mails to copyright
    Signed-off-by: Ksenia Kotelnikova <xeniia.ka@gmail.com>

--- FSharpActivePatterns/lib/printAST.ml
+++ FSharpActivePatterns/lib/printAST.ml
@@ -1,4 +1,4 @@
-(** Copyright 2024, Ksenia Kotelnikova, Gleb Nasretdinov *)
+(** Copyright 2024, Ksenia Kotelnikova <xeniia.ka@gmail.com>, Gleb Nasretdinov <gleb.nasretdinov@proton.me> *)
 
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
