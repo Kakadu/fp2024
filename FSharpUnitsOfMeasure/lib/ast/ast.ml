6fc7d010808e7ab6d49ff1f2ca05711a839c4e68
Daniel Vlasenco <vlasenko.daniil26@gmail.com>
2024-10-01 23:18:41 +0300

    Update AST, add cram test for factorial AST (#1)
    * feat(ast): update units of measure, remove unneeded types, refactor comments
    
    Signed-off-by: Daniel Vlasenco <vlasenko.daniil26@gmail.com>
    
    * feat: add structure items to ast, append deriving show
    
    Signed-off-by: Daniel Vlasenco <vlasenko.daniil26@gmail.com>
    
    * feat: add program type to ast
    
    Signed-off-by: Daniel Vlasenco <vlasenko.daniil26@gmail.com>
    
    * feat: add test for factorial ast
    
    Signed-off-by: Daniel Vlasenco <vlasenko.daniil26@gmail.com>
    
    * refactor: comment of factorial in OCaml
    
    Signed-off-by: Daniel Vlasenco <vlasenko.daniil26@gmail.com>
    
    ---------
    
    Signed-off-by: Daniel Vlasenco <vlasenko.daniil26@gmail.com>

--- FSharpUnitsOfMeasure/lib/ast/ast.ml
+++ FSharpUnitsOfMeasure/lib/ast/ast.ml
@@ -2,50 +2,48 @@
 
 (** SPDX-License-Identifier: MIT *)
 
-type binary_operation =
-  | Add (** 1 + 2 *)
-  | Sub (** 1 - 2 *)
-  | Mul (** * *)
-  | Div (** / *)
-  | Mod (** % *)
-  | And (** && *)
-  | Or (** || *)
-  | Eq (** = *)
-  | Neq (** <> *)
-  | Less (** < *)
-  | Gre (** > *)
-  | Leq (** <= *)
-  | Greq (** >= *)
-[@@deriving eq, show { with_path = false }]
-
 type constant =
   | Const_bool of bool (** Boolean constants [true] and [false] *)
-  | Const_int of int (** Integer constant such as [1] *)
-  | Const_char of char (** Char constant such as ['a'] *)
-  | Const_string of string (** String constant such as ["foo"] *)
-  | Const_float of float (** Float constants such as [3.14], [1e+5], [5.9E-3] *)
-  | Measure_float of constant * measure_type (** 5.0<cm> *)
-  
-and measure_type =
-  | Single_measure of string * pow (** single measure: <m>*)
-  | Multiple_measure of measure_type * binary_operation * measure_type
-  (** multiple measure: <m / sec * h ... >*)
+  | Const_int of int (** Integer constants: [1] *)
+  | Const_char of char (** Char constants: ['a'] *)
+  | Const_string of string (** String constants: ["foo"] *)
+  | Const_float of float (** Float constants: [3.14], [1e+5], [5.9E-3] *)
+  | Const_measure of measure_val * measure
+  (** Measure constants: [5.0<cm>], [3<kg>] *)
+[@@deriving show { with_path = false }]
 
-and pow = Pow of constant (** ^ *) 
-[@@deriving eq, show { with_path = false }]
+and measure_val =
+  | Mval_int of int
+  | Mval_float of float
+[@@deriving show { with_path = false }]
+
+and measure =
+  | Measure_ident of string (** Measure identificator: [<m>] *)
+  | Measure_prod of measure * measure (** Measure product: [<sec * h>], [<kg m>] *)
+  | Measure_div of measure * measure (** Measure division: [<m / sec>] *)
+  | Measure_pow of measure * rational_exp
+  (** Measure to the rational power: [<cm^3>] *)
+[@@deriving show { with_path = false }]
+
+and rational_exp =
+  | Integer of int (** Integer exponent: [2] *)
+  | Rational of int * int (** Rational exponent: [3/2] *)
+  | Negate of rational_exp (** Negation of exponent: [-1], [-(1/2)] *)
+  | Paren of rational_exp (** Parentheses around exponent: [(5)], [(-2/3)] *)
+[@@deriving show { with_path = false }]
 
 type pattern =
   | Pattern_wild (** Wildcard patterns [_] *)
-  | Pattern_ident of string (** Identificator name patterns such as [x] *)
+  | Pattern_ident of string (** Identificator name patterns: [x] *)
   | Pattern_const of constant
-  (** Constant patterns such as [1], ['a'], ["foo"], [3.14] *)
+  (** Constant patterns: [1], ['a'], ["foo"], [3.14], [5.0<cm>] *)
   | Pattern_tuple of pattern list
-  (** Tuple patterns [(P1; ..., Pn)]
+  (** Tuple patterns: [(P1; ..., Pn)]
 
       Invariant: [n >= 2]
     *)
-  | Pattern_or of pattern * pattern (** Pattern [P1 | P2] *)
-[@@deriving eq, show { with_path = false }]
+  | Pattern_or of pattern * pattern (** Or patterns: [P1 | P2] *)
+[@@deriving show { with_path = false }]
 
 type rec_flag =
   | Nonrecursive
@@ -58,26 +56,37 @@ type val_binding = Binding of pattern * expression
 
 and expression =
   | Expr_const of constant
-  (** Constant expressions such as [1], ['a'], ["foo"], [3.14], [true] *)
+  (** Constant expressions: [1], ['a'], ["foo"], [3.14], [true], [5.0<cm>] *)
   | Expr_ident of string
-  (** Identificator name expressions such as [x] *)
-  | Expr_binary of expression * binary_operation * expression 
-  (** Expression with binary operation such as 302 * 228 *)
+  (** Identificator name expressions: [x] *)
   | Expr_tuple of expression list
-  (** Tuple expressions [(E1, ..., En)]
+  (** Tuple expressions: [(E1, ..., En)]
+
       Invariant: [n >= 2]
-  *)
-  | Expr_fun of pattern * expression (** [Exp_fun (P, E)] represents [fun P -> E] *)
-  | Expr_let of rec_flag * val_binding list * expression
+    *)
+  | Expr_fun of pattern * expression
+  (** Anonimous functions: [Exp_fun(P, E)] represents [fun P -> E] *)
+  | Expr_let of rec_flag * val_binding list * expression option
   (** [Expr_let(rec_flag, [(P1, E1); ...; (Pn, En)], E)] represents:
         - [let P1 = E1 and ... and Pn = En in E] when rec_flag is Nonrecursive
         - [let rec P1 = E1 and ... and Pn = En in E] when rec_flag is Recursive
-  *)
+    *)
   | Expr_ifthenelse of expression * expression * expression option
   (** [if E1 then E2 else E3] *)
-  | Expr_apply of expression * expression
-  (** Application [E1 E2]  *)
+  | Expr_apply of expression * expression (** Application [E1 E2] *)
   | Expr_match of expression * (pattern * expression) list
-  (** [match E0 with P1 -> E1 | ... | Pn -> En] *)
-  | Expr_measure of measure_type (** measure *)
+  (** [match E with P1 -> E1 | ... | Pn -> En] *)
+[@@deriving show { with_path = false }]
+
+type structure_item =
+  | Str_eval of expression
+  (** Structure item which is single expression: [E] *)
+  | Str_value of rec_flag * val_binding list
+  (** [Str_value(rec_flag, [(P1, E1); ...; (Pn, En)])] represents:
+      - [let P1 = E1 and ... and Pn = En] when rec_flag is Nonrecursive
+      - [let rec P1 = E1 and ... and Pn = En] when rec_flag is Recursive
+    *)
+[@@deriving show { with_path = false }]
+
+type program = structure_item list
 [@@deriving show { with_path = false }]
