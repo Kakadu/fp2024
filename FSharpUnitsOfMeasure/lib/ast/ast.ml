a53fa30b28e5664e53de8335bbef5d0ca8973f02
Daniel Vlasenco <vlasenko.daniil26@gmail.com>
2024-10-02 11:41:52 +0300

    Remove extra mutrec in AST, add more docs info (#2)
    * fix: remove unneeded mutual recursion
    
    Signed-off-by: Daniel Vlasenco <vlasenko.daniil26@gmail.com>
    
    * refactor: add more comments to ast.ml
    
    Signed-off-by: Daniel Vlasenco <vlasenko.daniil26@gmail.com>
    
    * docs: add emails to dune-project
    
    Signed-off-by: Daniel Vlasenco <vlasenko.daniil26@gmail.com>
    
    * refactor: add comments for measure_val
    
    Signed-off-by: Daniel Vlasenco <vlasenko.daniil26@gmail.com>
    
    * feat: update .opam file
    
    Signed-off-by: Daniel Vlasenco <vlasenko.daniil26@gmail.com>
    
    ---------
    
    Signed-off-by: Daniel Vlasenco <vlasenko.daniil26@gmail.com>

--- FSharpUnitsOfMeasure/lib/ast/ast.ml
+++ FSharpUnitsOfMeasure/lib/ast/ast.ml
@@ -2,6 +2,26 @@
 
 (** SPDX-License-Identifier: MIT *)
 
+type rational_exp =
+  | Integer of int (** Integer exponent: [2] *)
+  | Rational of int * int (** Rational exponent: [3/2] *)
+  | Negate of rational_exp (** Negation of exponent: [-1], [-(1/2)] *)
+  | Paren of rational_exp (** Parentheses around exponent: [(5)], [(-2/3)] *)
+[@@deriving show { with_path = false }]
+
+type measure =
+  | Measure_ident of string (** Measure identificator: [<m>] *)
+  | Measure_prod of measure * measure (** Measure product: [<sec * h>], [<kg m>] *)
+  | Measure_div of measure * measure (** Measure division: [<m / sec>] *)
+  | Measure_pow of measure * rational_exp
+  (** Measure to the rational power: [<cm^3>] *)
+[@@deriving show { with_path = false }]
+
+type measure_val =
+  | Mval_int of int (** Integer numbers with units of measure *)
+  | Mval_float of float (** Real numbers with units of measure *)
+[@@deriving show { with_path = false }]
+
 type constant =
   | Const_bool of bool (** Boolean constants [true] and [false] *)
   | Const_int of int (** Integer constants: [1] *)
@@ -12,26 +32,6 @@ type constant =
   (** Measure constants: [5.0<cm>], [3<kg>] *)
 [@@deriving show { with_path = false }]
 
-and measure_val =
-  | Mval_int of int
-  | Mval_float of float
-[@@deriving show { with_path = false }]
-
-and measure =
-  | Measure_ident of string (** Measure identificator: [<m>] *)
-  | Measure_prod of measure * measure (** Measure product: [<sec * h>], [<kg m>] *)
-  | Measure_div of measure * measure (** Measure division: [<m / sec>] *)
-  | Measure_pow of measure * rational_exp
-  (** Measure to the rational power: [<cm^3>] *)
-[@@deriving show { with_path = false }]
-
-and rational_exp =
-  | Integer of int (** Integer exponent: [2] *)
-  | Rational of int * int (** Rational exponent: [3/2] *)
-  | Negate of rational_exp (** Negation of exponent: [-1], [-(1/2)] *)
-  | Paren of rational_exp (** Parentheses around exponent: [(5)], [(-2/3)] *)
-[@@deriving show { with_path = false }]
-
 type pattern =
   | Pattern_wild (** Wildcard patterns [_] *)
   | Pattern_ident of string (** Identificator name patterns: [x] *)
@@ -39,15 +39,14 @@ type pattern =
   (** Constant patterns: [1], ['a'], ["foo"], [3.14], [5.0<cm>] *)
   | Pattern_tuple of pattern list
   (** Tuple patterns: [(P1; ..., Pn)]
-
       Invariant: [n >= 2]
     *)
   | Pattern_or of pattern * pattern (** Or patterns: [P1 | P2] *)
 [@@deriving show { with_path = false }]
 
 type rec_flag =
-  | Nonrecursive
-  | Recursive
+  | Nonrecursive (** For nonrecursive function declarations *)
+  | Recursive (** For recursive function declarations *)
 [@@deriving show { with_path = false }]
 
 type val_binding = Binding of pattern * expression
@@ -61,21 +60,25 @@ and expression =
   (** Identificator name expressions: [x] *)
   | Expr_tuple of expression list
   (** Tuple expressions: [(E1, ..., En)]
-
       Invariant: [n >= 2]
     *)
   | Expr_fun of pattern * expression
-  (** Anonimous functions: [Exp_fun(P, E)] represents [fun P -> E] *)
+  (** Anonimous functions: [Exp_fun(P, E)] represents [fun P -> E]
+      Invariant: [n >= 1]
+    *)
   | Expr_let of rec_flag * val_binding list * expression option
   (** [Expr_let(rec_flag, [(P1, E1); ...; (Pn, En)], E)] represents:
         - [let P1 = E1 and ... and Pn = En in E] when rec_flag is Nonrecursive
         - [let rec P1 = E1 and ... and Pn = En in E] when rec_flag is Recursive
+      Invariant: [n >= 1]
     *)
   | Expr_ifthenelse of expression * expression * expression option
   (** [if E1 then E2 else E3] *)
   | Expr_apply of expression * expression (** Application [E1 E2] *)
   | Expr_match of expression * (pattern * expression) list
-  (** [match E with P1 -> E1 | ... | Pn -> En] *)
+  (** [match E with P1 -> E1 | ... | Pn -> En]
+      Invariant: [n >= 1]
+    *)
 [@@deriving show { with_path = false }]
 
 type structure_item =
@@ -85,6 +88,7 @@ type structure_item =
   (** [Str_value(rec_flag, [(P1, E1); ...; (Pn, En)])] represents:
       - [let P1 = E1 and ... and Pn = En] when rec_flag is Nonrecursive
       - [let rec P1 = E1 and ... and Pn = En] when rec_flag is Recursive
+      Invariant: [n >= 1]
     *)
 [@@deriving show { with_path = false }]
 
