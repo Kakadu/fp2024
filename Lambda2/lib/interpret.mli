853c94351dd3e69717c2a78c4f0dcf2aa69e9460
fp2022-helper <113180750+fp2022-helper@users.noreply.github.com>
2024-10-02 18:02:15 +0000

    Lambda2

---
+++ Lambda2/lib/interpret.mli
@@ -0,0 +1,9 @@
+[@@@ocaml.text "/*"]
+
+(** Copyright 2021-2024, Kakadu and contributors *)
+
+(** SPDX-License-Identifier: LGPL-3.0-or-later *)
+
+[@@@ocaml.text "/*"]
+
+val parse_and_run : string -> unit
