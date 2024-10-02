a72b5f3b04897786b5437cf568cb1d685cf8d1e3
Kostya <koreshin67@gmail.com>
2024-10-03 00:59:21 +0300

    test: fix test, rename variable

--- Haskell/bin/main.ml
+++ Haskell/bin/main.ml
@@ -3,50 +3,72 @@ open Haskell_lib.Ast
 (* fac n = if n < 0 then Nothing else Just (save_fac n) where save_fac y | y == 0  = 1 | otherwise = y * save_fac (y - 1) *)
 
 let () =
-  let fac : binding =
+  let fac_ast : binding =
     FunBind
-      ( Ident ("fac", None) (* name of the function *)
-      , (None, PIdentificator (Ident ("n", None))) (* parameter *)
-      , []
+      (* bind a function name with it's body *)
+      ( (Ident "fac", None) (* name of the function without explicit return type *)
+      , (None, PIdentificator (Ident "n"), None)
+        (* single parameter n without explicit type *)
+      , [] (* no more parameters *)
       , OrdBody
-          (* function body *)
-          (IfThenEsle
-             ( Binop (Identificator (Ident ("n", None)), Less, Const (Int 0))
-               (* comparison n with 0 *)
-             , OptionBld Nothing (* case n < 0: return Nothing *)
-             , OptionBld
-                 (Just
+          (* function contains ordinary body (without guards) *)
+          ( IfThenEsle
+              (* start of conditional operator if-then-else *)
+              ( ( Binop ((Identificator (Ident "n"), None), Less, (Const (Int 0), None))
+                , None )
+                (* comparison of parameter n with 0 *)
+              , (OptionBld Nothing, None)
+                (* case n < 0: return constructor "Nothing" of type "Maybe" *)
+              , ( OptionBld
                     (* else case *)
-                    (FunctionApply
-                       (* call a function "save_fac" with argument n *)
-                       ( Identificator (Ident ("save_fac", None))
-                       , Identificator (Ident ("n", None))
-                       , [] ))) ))
+                    (Just
+                       (* due to the fact that function returns value of type "Maybe", we need to wrap result in "Just" *)
+                       ( FunctionApply
+                           (* call a function "save_fac" with argument n *)
+                           ( (Identificator (Ident "save_fac"), None)
+                             (* name of function without it's type *)
+                           , (Identificator (Ident "n"), None)
+                             (* argument n without type *)
+                           , [] )
+                         (* no more arguments *)
+                       , None ))
+                , None ) )
+          , None )
       , [ FunBind
             (* function binding, in our case - "where save_fuc" *)
-            ( Ident ("save_fac", None)
-            , (None, PIdentificator (Ident ("y", None)))
-            , []
+            ( (Ident "save_fac", None) (* name of function *)
+            , (None, PIdentificator (Ident "y"), None) (* parameter of "save_fac" *)
+            , [] (* no more arguments *)
             , Guards
                 (* pattern match y with 0 and other cases *)
-                (* case y == 0 *)
-                ( ( Binop (Identificator (Ident ("y", None)), Equality, Const (Int 0))
-                  , Const (Int 1) )
+                (* case y == 0: return 1 *)
+                ( ( ( Binop
+                        ( (Identificator (Ident "y"), None)
+                        , Equality
+                        , (Const (Int 0), None) )
+                    , None )
+                  , (Const (Int 1), None) )
                   (* otherwise case *)
-                , [ ( Const (Bool true)
-                    , Binop
-                        (* multiply y with called function *)
-                        ( Identificator (Ident ("y", None))
-                        , Multiply
-                        , FunctionApply
-                            (* call "save_fac" with argument (y - 1)*)
-                            ( Identificator (Ident ("save_fac", None))
-                            , Binop
-                                (Identificator (Ident ("y", None)), Minus, Const (Int 1))
-                            , [] ) ) )
+                , [ ( (Const (Bool true), None)
+                    , ( Binop
+                          (* multiply y with called function *)
+                          ( (Identificator (Ident "y"), None)
+                          , Multiply
+                          , ( FunctionApply
+                                (* call "save_fac" with argument (y - 1)*)
+                                ( (Identificator (Ident "save_fac"), None)
+                                , ( Binop
+                                      (* decrement of y *)
+                                      ( (Identificator (Ident "y"), None)
+                                      , Minus
+                                      , (Const (Int 1), None) )
+                                  , None )
+                                , [] )
+                            , None ) )
+                      , None ) )
                   ] )
             , [] )
         ] )
   in
-  print_endline (show_binding fac)
+  print_endline (show_binding fac_ast)
 ;;
--- Haskell/lib/ast.ml
+++ Haskell/lib/ast.ml
@@ -2,12 +2,15 @@ type const =
   | Int of int (** e.g. [18] *)
   | Bool of bool (** e.g. [True] *)
   | Unit (** () *)
+[@@deriving show { with_path = false }]
 
 type 'a maybe =
   | Nothing (** Nothing *)
   | Just of 'a (** e.g. [Just 5] *)
+[@@deriving show { with_path = false }]
 
 type functype = FuncT of tp * tp * tp list (** e.g. [Int -> Bool -> (Int,Bool)] *)
+[@@deriving show { with_path = false }]
 
 (** explicit type indication*)
 and tp =
@@ -18,6 +21,7 @@ and tp =
   | ListParam of tp (** e.g. [[Int]] *)
   | TupleParams of tp * tp * tp list (** e.g. [(Int, Bool)] *)
   | FunctionType of functype
+[@@deriving show { with_path = false }]
 
 type binop =
   | Plus (** + *)
@@ -31,25 +35,29 @@ type binop =
   | Greater (** > *)
   | EqualityOrLess (** <= *)
   | EqualityOrGreater (** >= *)
+[@@deriving show { with_path = false }]
 
 type unop =
   | Minus (** - *)
   | Not (** not *)
+[@@deriving show { with_path = false }]
 
 (** variable's / function's name*)
-type ident = Ident of string
+type ident = Ident of string [@@deriving show { with_path = false }]
 
 (** e.g. [lst@(_:xs) :: [Int]] *)
-type pattern = ident option * pat * tp option
+type pattern = ident option * pat * tp option [@@deriving show { with_path = false }]
 
 and listpat =
   | Nil (** \[] *)
   | PCons of pattern * listpat (** e.g. [x:xs] *)
   | PEnum of pattern * pattern list (** e.g. [[x, y, z]] *)
+[@@deriving show { with_path = false }]
 
 and treepat =
   | PNul (** nul tree i.e. [$] *)
   | PNode of pattern * treepat * treepat (** tree's node e.g [(x; y; z)]*)
+[@@deriving show { with_path = false }]
 
 and pat =
   | PWildcard (** _ *)
@@ -60,20 +68,24 @@ and pat =
   | PJust of pattern (** e.g. [Just x] *)
   | PNothing (** i.e [Nothing] *)
   | PTree of treepat
+[@@deriving show { with_path = false }]
 
 type comprehension =
   | Condition of expr (** e.g. [x < 10] *)
   | Generator of (pattern * expr) (** e.g. [x <- [1 ..]] *)
+[@@deriving show { with_path = false }]
 
 and ordinarylistbld =
   | ComprehensionList of expr * comprehension * comprehension list
   (** e.g [[x * y | x <- [1, 10, 100], y <- [2, 3]], y mod 2 == 0] *)
   | IncomprehensionlList of expr (**e.g. [[1,2]] *)
+[@@deriving show { with_path = false }]
 
 and listbld =
   | LazyList of expr * expr option * expr option
   (** e.g. [[1.. ] or [1..10] or [1, 2 .. 10] or [1, 3..]] *)
   | OrdList of ordinarylistbld
+[@@deriving show { with_path = false }]
 
 and binding =
   | VarsBind of pattern * bindingbody * binding list
@@ -81,17 +93,20 @@ and binding =
   | FunBind of
       (ident * functype option) * pattern * pattern list * bindingbody * binding list
   (** e.g [f x y = x + y + z where z = 2 ]*)
+[@@deriving show { with_path = false }]
 
 (** examples below are for function binding with due body *)
 and bindingbody =
   | Guards of (expr * expr) * (expr * expr) list
   (** (condition, branch) pairs e.g [f x | x > 0 = x | otherwise = -1] *)
   | OrdBody of expr (** e.g [f x = if x > 0 then x else -1] *)
+[@@deriving show { with_path = false }]
 
 and binary_tree_bld =
   | Nul (** node that not exists (notation: [$]) *)
   | Node of expr * binary_tree_bld * binary_tree_bld
   (** node is data and two 'sons' e.g  [(x^y; $; (2; $; $))] *)
+[@@deriving show { with_path = false }]
 
 and expression =
   | Const of const
@@ -116,6 +131,7 @@ and expression =
         y = 2
       in
       x + y] *)
+[@@deriving show { with_path = false }]
 
 (** e.g. [(x + 1) :: Int]*)
-and expr = expression * tp option
+and expr = expression * tp option [@@deriving show { with_path = false }]
