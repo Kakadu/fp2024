40f12a1ad13d5bfc148cdc253602c781cda4e04d
Gleb Nasretdinov <gleb.nasretdinov@proton.me>
2024-10-04 05:26:32 +0300

    feat: add Active Patterns, separated Let from expressions
    Signed-off-by: Gleb Nasretdinov <gleb.nasretdinov@proton.me>

--- FSharpActivePatterns/bin/main.ml
+++ FSharpActivePatterns/bin/main.ml
@@ -11,29 +11,28 @@ let () =
       ( Rec
       , Some "factorial"
       , [ Variable (Ident "n") ]
-      , [ If_then_else
-            ( Bin_expr
-                ( Logical_or
-                , Bin_expr (Binary_equal, Variable (Ident "n"), Const (Int_lt 0))
-                , Bin_expr (Binary_equal, Variable (Ident "n"), Const (Int_lt 1)) )
-            , [ Const (Int_lt 1) ]
-            , Some
-                [ Bin_expr
-                    ( Binary_multiply
-                    , Variable (Ident "n")
-                    , Function_call
-                        ( "factorial"
-                        , [ Bin_expr
-                              (Binary_subtract, Variable (Ident "n"), Const (Int_lt 1))
-                          ] ) )
-                ] )
-        ] )
+      , If_then_else
+          ( Bin_expr
+              ( Logical_or
+              , Bin_expr (Binary_equal, Variable (Ident "n"), Const (Int_lt 0))
+              , Bin_expr (Binary_equal, Variable (Ident "n"), Const (Int_lt 1)) )
+          , [ Const (Int_lt 1) ]
+          , Some
+              [ Bin_expr
+                  ( Binary_multiply
+                  , Variable (Ident "n")
+                  , Function_call
+                      ( Variable (Ident "factorial")
+                      , [ Bin_expr
+                            (Binary_subtract, Variable (Ident "n"), Const (Int_lt 1))
+                        ] ) )
+              ] ) )
   in
   let program =
-    [ Let (Ident "a", Const (Int_lt 10))
-    ; factorial
-    ; Function_call ("factorial", [ Variable (Ident "a") ])
+    [ Statement (Let (Ident "a", Const (Int_lt 10)))
+    ; Expr factorial
+    ; Expr (Function_call (Variable (Ident "factorial"), [ Variable (Ident "a") ]))
     ]
   in
-  List.iter print_expr program
+  List.iter print_construction program
 ;;
