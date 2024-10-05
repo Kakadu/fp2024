eef71142e883216f806c8ba27af2da5bd7d5a1b7
Ilia Suponev <suponeff.i@gmail.com>
2024-10-05 20:00:06 +0300

    refactor: update change definition signature
    Signed-off-by: Ilia Suponev <suponeff.i@gmail.com>

--- OCamlPolymorphicVariantsTypes/lib/ast.ml
+++ OCamlPolymorphicVariantsTypes/lib/ast.ml
@@ -51,25 +51,24 @@ type expression =
   | Unary of unary_operator * expression (* ~-123 *)
   | Binary of expression * binary_operator * expression (* 12 + 34 | true && (x > y) *)
   | Tuple of expression list (* (1, 2, (let x = 6 in x)) *)
-  | If of expression * expression * expression (* if x then false else true *)
+  | If of expression * expression * expression option (* if x then false else true *)
   | Lambda of pattern list * expression (* fun (x, (y,z)) -> x / (y + z) *)
   | Apply of expression * expression list
     (* factorial (n / 2) | (fun (x, (y,z)) -> x / (y + z)) (5, (2, 1)) *)
-  | Define of definition * expression (* let <definition> in <expr>*)
+  | Define of definition * expression (* let <definition> in <expr> *)
   | EpressionBlock of expression list (* (let g x = x / 2 in g y; y); *)
 [@@deriving show { with_path = false }]
 
-and value_binding = identifier * pattern list * expression
-[@@deriving show { with_path = false }]
+and value_binding = pattern * expression [@@deriving show { with_path = false }]
 
 and definition =
   recursive_type
-  * value_binding list (* [rec] <identifier1> P1 = E1 and ... and <identifierN> PN = EN *)
+  * value_binding list (* [rec] P1 = E1 and ... and <identifierN> PN = EN *)
 [@@deriving show { with_path = false }]
 
 type struct_item =
   | DefineItem of definition (* let f x = x;; *)
-  | EvalItem of expression
+  | EvalItem of expression (* (fun x -> print_endline x) "Hello world";; *)
 [@@deriving show { with_path = false }]
 
-type program = identifier * struct_item list [@@deriving show { with_path = false }]
+type program = struct_item list [@@deriving show { with_path = false }]
