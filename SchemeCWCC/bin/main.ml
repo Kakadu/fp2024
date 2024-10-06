7b408973e3c0dddc13d5664ce0e991cff4f89623
kinokotakenoko9 <chdmitry2@gmail.com>
2024-10-06 13:22:00 +0300

    fix: format

--- SchemeCWCC/bin/main.ml
+++ SchemeCWCC/bin/main.ml
@@ -4,17 +4,24 @@
 
 open Schemecwcc_lib.Ast
 
-let () = let factorial : program = [
-  Expr_define (
-    "factorial", 
-    Expr_lambda (
-      ["n"],
-      [Expr_if (
-        Expr_apply (Expr_id "=", [Expr_id "n"; Expr_int 0]),
-        Expr_int 1,
-        Expr_apply (Expr_id "*", [Expr_id "n"; Expr_apply (Expr_id "factorial", [Expr_apply (Expr_id "-", [Expr_id "n"; Expr_int 1])])])
-      )]
-    )
-  )  
-] in 
-print_endline (show_program factorial)
+let () =
+  let factorial : program =
+    [ Expr_define
+        ( "factorial"
+        , Expr_lambda
+            ( [ "n" ]
+            , [ Expr_if
+                  ( Expr_apply (Expr_id "=", [ Expr_id "n"; Expr_int 0 ])
+                  , Expr_int 1
+                  , Expr_apply
+                      ( Expr_id "*"
+                      , [ Expr_id "n"
+                        ; Expr_apply
+                            ( Expr_id "factorial"
+                            , [ Expr_apply (Expr_id "-", [ Expr_id "n"; Expr_int 1 ]) ] )
+                        ] ) )
+              ] ) )
+    ]
+  in
+  print_endline (show_program factorial)
+;;
--- SchemeCWCC/lib/ast.ml
+++ SchemeCWCC/lib/ast.ml
@@ -2,19 +2,19 @@
 
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
-type id = string (* identifier *)
-[@@deriving eq, ord, show { with_path = false }]
+type id = string (* identifier *) [@@deriving eq, ord, show { with_path = false }]
 
-type expr = 
+type expr =
   | Expr_unit (* type for expr without meaningful return value *)
   | Expr_int of int (* int numbers *)
-  | Expr_id of id (* identidier *)
+  | Expr_id of id (* identifier *)
   | Expr_bool of bool (* booleans *)
   | Expr_define of id * expr (* defines id with expr *)
   | Expr_if of expr * expr * expr (* condition then else *)
-  | Expr_lambda of id list * expr list (* anonymous functions, made of list of parameters and its body *)
-  | Expr_apply of expr * expr list (* represents application of a funcrion. identifier or lambda is called with the list of arguments *)
-  [@@deriving eq, ord, show { with_path = false }]
-  
-type program = expr list 
+  | Expr_lambda of
+      id list * expr (* anonymous functions, made of list of parameters and its body *)
+  | Expr_apply of expr * expr list
+    (* represents application of a function. identifier or lambda is called with the list of arguments *)
 [@@deriving eq, ord, show { with_path = false }]
+
+type program = expr list [@@deriving eq, ord, show { with_path = false }]
