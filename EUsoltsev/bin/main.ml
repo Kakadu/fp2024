896e2e134b3254f7f85030e8617ac42b0adffc73
Sibiri4ok <danilusoltsev05@gmail.com>
2024-10-10 00:25:03 +0300

    fix: ocaml format

--- EUsoltsev/bin/main.ml
+++ EUsoltsev/bin/main.ml
@@ -5,16 +5,24 @@
 open EUsoltsev_lib.Ast
 
 let () =
-  let factorial: expr = 
-    ExpLet(true, 
-    PatVariable "factorial", 
-    ExpLambda([PatVariable "n"], 
-      ExpIfThenElse(
-        ExpBinOper(LowerThan, 
-          ExpIdent "n", 
-          ExpConst( ConstInt 2)),
-      ExpConst( ConstInt 1),
-      Some( ExpBinOper(Multiply, ExpIdent "n", ExpFunction(ExpIdent "factorial",ExpBinOper (Minus, ExpIdent "n", ExpConst (ConstInt 1)) ))) ))
-    , ExpIdent "factorial")
-
-  in print_endline(show_expr factorial);;  
+  let factorial : expr =
+    ExpLet
+      ( true
+      , PatVariable "factorial"
+      , ExpLambda
+          ( [ PatVariable "n" ]
+          , ExpIfThenElse
+              ( ExpBinOper (LowerThan, ExpIdent "n", ExpConst (ConstInt 2))
+              , ExpConst (ConstInt 1)
+              , Some
+                  (ExpBinOper
+                     ( Multiply
+                     , ExpIdent "n"
+                     , ExpFunction
+                         ( ExpIdent "factorial"
+                         , ExpBinOper (Minus, ExpIdent "n", ExpConst (ConstInt 1)) ) )) )
+          )
+      , ExpIdent "factorial" )
+  in
+  print_endline (show_expr factorial)
+;;
--- EUsoltsev/lib/ast.ml
+++ EUsoltsev/lib/ast.ml
@@ -2,8 +2,8 @@
 
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
-type ident = string [@@deriving show {with_path = false}]
-and is_rec = bool [@@deriving show {with_path = false}]
+type ident = string [@@deriving show { with_path = false }]
+and is_rec = bool [@@deriving show { with_path = false }]
 
 type bin_oper =
   | Plus (* [+] *)
@@ -18,28 +18,27 @@ type bin_oper =
   | LowerThan (* [<] *)
   | Equal (* [=] *)
   | NotEqual (* [<>] *)
-[@@deriving show {with_path=false}]
+[@@deriving show { with_path = false }]
 
-and unar_oper = 
+and unar_oper =
   | Negative (* [-x] *)
   | Not (* [not x]*)
-[@@deriving show {with_path=false}]
+[@@deriving show { with_path = false }]
 
-type const = 
+type const =
   | ConstInt of int (* Integer constant: Example - [21] *)
   | ConstBool of bool (* Boolean constant: Example - [true] or [false] *)
   | ConstString of string (* String constant: Example - "I like OCaml!" *)
-[@@deriving show {with_path=false}]
+[@@deriving show { with_path = false }]
 
-type pattern = 
+type pattern =
   | PatVariable of ident (* [x] *)
   | ParConst of const (* [21] or [true] or [false] *)
   | PatTuple of pattern list (* (x1; x2 ... xn) *)
-  | PatOption of pattern option 
-  (* Matching with an optional value (Some/None) *)
-[@@deriving show {with_path=false}]
+  | PatOption of pattern option (* Matching with an optional value (Some/None) *)
+[@@deriving show { with_path = false }]
 
-type expr = 
+type expr =
   | ExpIfThenElse of expr * expr * expr option
   (* ExpIfThenElse (x > 5, 10, 20(may return null) *)
   | ExpIdent of ident (* ExpIdent "x" *)
@@ -52,8 +51,7 @@ type expr =
   | ExpFunction of expr * expr (* ExpFunction(x, y)*)
   | ExpMatch of expr * (pattern * expr) list
   (* ExpMatch (x, [PatVariable "y", 10; PatConst (ConstInt 0), 0]) *)
-  | ExpLet of is_rec * pattern * expr * expr 
-  (* let x = 10 in x + 5 <=> ExpLet(false, "x", 10, x + 5) *)
-  (* let x = 10 <=> ExpLet(false, "x", 10, "x")*)
-[@@deriving show {with_path=false}]
-
+  | ExpLet of is_rec * pattern * expr * expr
+(* let x = 10 in x + 5 <=> ExpLet(false, "x", 10, x + 5) *)
+(* let x = 10 <=> ExpLet(false, "x", 10, "x")*)
+[@@deriving show { with_path = false }]
