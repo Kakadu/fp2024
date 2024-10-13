ad0a028beb05aea618aaf4202635b5964faff3c5
mshipilov5 <shipilovmaksim05@gmail.com>
2024-10-13 21:23:09 +0300

    feat: add flag -fromfile name_file in repl  for parsing from near file

--- OcamlBR/repl/repl.ml
+++ OcamlBR/repl/repl.ml
@@ -11,18 +11,21 @@ type stop_after =
 type opts =
   { mutable dump_parsetree : bool
   ; mutable stop_after : stop_after
+  ; mutable input_file : string option
   }
 
 
-
 let eval ast = 
-  print_endline (show_expr ast); (* eval will be here soon *)
+  ignore (show_expr ast); (* eval will be here soon *)
   ()
 ;;
 
-
-let run_single dump_parsetree stop_after eval =
-  let text = In_channel.input_all stdin |> Stdlib.String.trim in
+let run_single dump_parsetree stop_after eval input_source =
+  let text = 
+    match input_source with
+    | Some file_name -> In_channel.read_all file_name |> Stdlib.String.trim
+    | None -> In_channel.input_all stdin |> Stdlib.String.trim
+  in
   match parse_expr text with
   | Error e -> Format.printf "Parse Error: %s\n%!" e
   | Ok ast ->
@@ -34,7 +37,7 @@ let run_single dump_parsetree stop_after eval =
   
 
 let () =
-  let opts = { dump_parsetree = false; stop_after = SA_never } in
+  let opts = { dump_parsetree = false; stop_after = SA_never; input_file = None } in
   let () =
     let open Stdlib.Arg in
     parse
@@ -46,12 +49,16 @@ let () =
             (function
               | "parsing" -> opts.stop_after <- SA_parsing
               | _ -> failwith "Bad argument for -stop-after")
-        , "" )
+        , "Stop after parsing" )
+      ; ( "-fromfile"
+        , String (fun filename -> opts.input_file <- Some filename)
+        , "Read code from the specified file" )
       ]
       (fun _ -> Stdlib.Format.eprintf "Positional arguments are not supported\n";
        Stdlib.exit 1)
       "Read-Eval-Print-Loop for custom language"
   in
 
-  run_single opts.dump_parsetree opts.stop_after (fun ast -> eval ast)
+
+  run_single opts.dump_parsetree opts.stop_after (fun ast -> eval ast) opts.input_file
 ;;
