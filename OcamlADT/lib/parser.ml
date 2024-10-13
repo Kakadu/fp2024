2569f0e0c4d561ca4cc2775a42d3f6789b1d14fe
Rodion Suvorov <rodion.suvorov.94@mail.ru>
2024-10-13 12:07:53 +0000

    feat: implement let expression parser and recursion flag

--- OcamlADT/lib/parser.ml
+++ OcamlADT/lib/parser.ml
@@ -20,7 +20,6 @@ let pconstintexpr =
   let* sign = choice [ token "+"; token "-"; token " "] in 
   let* n = take_while1 (function '0' .. '9' -> true | _ -> false) in
   return (Exp_constant(Const_integer (int_of_string (sign ^ n))))
-;;
 
 (* let pconstchar = 
   let* _ = token "'" in 
@@ -33,7 +32,6 @@ let pconststringexpr =
   let* _ = token "\"" in
   let* str = take_while1 (function '"' -> false | _ -> true) in 
   return (Exp_constant(Const_string (str)))
-;;
 
 let lchain p op =
   let rec loop acc =
@@ -44,7 +42,6 @@ let lchain p op =
   in
   let* x = p in
   loop x
-;;
 
 
 (** Parser that takese pexpr (parser of expressions) and returns parsed arithm expression, will be a part of whole expression parser*)
@@ -62,7 +59,6 @@ let parithmexpr pexpr =
   let pand = lchain pexpr (token "&&" *> return (fun exp1 exp2 -> Exp_apply (Exp_ident "&&", Exp_tuple (exp1, exp2, [])))) <|> pexpr in
   let por = lchain pexpr (token "||" *> return (fun exp1 exp2 -> Exp_apply (Exp_ident "||", Exp_tuple (exp1, exp2, [])))) <|> pexpr in
   pexpr
-;;
 
 let pexpr = 
   parithmexpr <|> ptupleexpr <|> pidentexpr <|> pletexpr <|> pifexpr <|> pconstintexpr <|> pconststringexpr
@@ -72,6 +68,19 @@ let pvalue_binding =
   let* _ = token "=" in
   let* expr = pexpr in
   return { pat; expr }
+
+let prec_flag = 
+  let* _ = token "rec" *> return Recursive <|> return Nonrecursive
+
+let pletexpr =
+  let* _ = token "let" in
+  let* rec_flag = prec_flag in
+  let* many1 value_binding = pvalue_binding in
+  let* exrpession = pexpr in
+  return (rec_flag, value_binding, expression)
+
+let ptupleexpr =
+  
 
 
 (** It applies Str_eval to output of expression parser *)
@@ -83,7 +92,7 @@ let pstr_item =
 
   let psvalue = 
     (* we cant use let+ bc previous results are necessary *)
-    let* rec = token "rec" *> return Recursive <|> return Nonrecursive in
+    let* rec_flag = prec_flag in
     let* value_binding = many pvalue_binding in 
     Str_value (rec, value_binding)
   in
