e259cb1e491dca18e6d39cf5b4a3512c21055f59
Kostya <koreshin67@gmail.com>
2024-10-14 02:13:03 +0300

    fix: remove extra type

--- Haskell/lib/ast.ml
+++ Haskell/lib/ast.ml
@@ -13,9 +13,6 @@ type 'a maybe =
   | Just of 'a (** e.g. [Just 5] *)
 [@@deriving qcheck, show { with_path = false }]
 
-type functype = FuncT of tp * tp * tp list (** e.g. [Int -> Bool -> (Int,Bool)] *)
-[@@deriving show { with_path = false }]
-
 (** explicit type indication*)
 type tp =
   | TUnit (** () *)
