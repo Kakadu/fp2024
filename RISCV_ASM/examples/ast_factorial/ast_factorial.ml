830d6c25366b021aa38c731bc3a172ed3826493a
Roman Mukovenkov <roman.mukovenkov@gmail.com>
2024-10-14 20:21:00 +0300

    add: draft of new test, comparing the ast of parser to predefined ast

--- RISCV_ASM/examples/ast_factorial/ast_factorial.ml
+++ RISCV_ASM/examples/ast_factorial/ast_factorial.ml
@@ -1,29 +1,49 @@
 open Riscv_asm_interpreter_lib.Ast
+open Riscv_asm_interpreter_lib.Parser
+open Angstrom
+open Printf
+
+let expected_ast : ast = 
+  [ LabelExpr "factorial"
+  ; (* factorial: *)
+    InstructionExpr (Mv (X15, X10))
+  ; (* mv a5, a0 *)
+    InstructionExpr (Li (X10, Immediate32(1)))
+  ; (* li a0, 1 *)
+    InstructionExpr (Beq (X15, X0, LabelAddress12 ".L5"))
+  ; (* beq a5, zero, .L5 *)
+    LabelExpr ".L2"
+  ; (* .L2: *)
+    InstructionExpr (Mv (X14, X15))
+  ; (* mv a4, a5 *)
+    InstructionExpr (Addiw (X15, X15, Immediate12(-1)))
+  ; (* addiw a5, a5, -1 *)
+    InstructionExpr (Mulw (X10, X14, X10))
+  ; (* mulw a0, a4, a0 *)
+    InstructionExpr (Bne (X15, X0, LabelAddress12 ".L2"))
+  ; (* bne a5, zero, .L2 *)
+    LabelExpr ".L5"
+  ; (* .L5: *)
+    InstructionExpr Ret (* ret *)
+  ]
 
 let () =
-  let factorial_ast : ast =
-    [ Label "factorial"
-    ; (* factorial: *)
-      Instruction (Mv (X15, X10))
-    ; (* mv a5, a0 *)
-      Instruction (Li (X10, 1))
-    ; (* li a0, 1 *)
-      Instruction (Beq (X15, X0, Label ".L5"))
-    ; (* beq a5, zero, .L5 *)
-      Label ".L2"
-    ; (* .L2: *)
-      Instruction (Mv (X14, X15))
-    ; (* mv a4, a5 *)
-      Instruction (Addiw (X15, X15, -1))
-    ; (* addiw a5, a5, -1 *)
-      Instruction (Mulw (X10, X14, X10))
-    ; (* mulw a0, a4, a0 *)
-      Instruction (Bne (X15, X0, Label ".L2"))
-    ; (* bne a5, zero, .L2 *)
-      Label ".L5"
-    ; (* .L5: *)
-      Instruction Ret (* ret *)
-    ]
-  in
-  print_endline (show_ast factorial_ast)
-;;
+  print_endline (show_ast expected_ast)
+
+let read_file file_name =
+  let input_channel = open_in file_name in
+  let file_length = in_channel_length input_channel in
+  let file_content = really_input_string input_channel file_length in
+  close_in input_channel;
+  file_content
+
+let parse_input_file filename =
+  let input = read_file filename in
+  match parse_string ~consume:All parse_ast input with
+  | Ok ast -> ast
+  | Error msg -> failwith (sprintf "Failed to parse file: %s" msg)
+
+let () =
+  let parsed_ast = parse_input_file "../examples/ast_factorial/factorial.s" in
+  print_endline (show_ast parsed_ast)
+
--- RISCV_ASM/tests/ast_factorial_test.t
+++ RISCV_ASM/tests/ast_factorial_test.t
@@ -1,7 +1,9 @@
   $ ../examples/ast_factorial/ast_factorial.exe
-  [(Label "factorial"); (Instruction (Mv (X15, X10)));
-    (Instruction (Li (X10, 1))); (Instruction (Beq (X15, X0, (Label ".L5"))));
-    (Label ".L2"); (Instruction (Mv (X14, X15)));
-    (Instruction (Addiw (X15, X15, -1))); (Instruction (Mulw (X10, X14, X10)));
-    (Instruction (Bne (X15, X0, (Label ".L2")))); (Label ".L5");
-    (Instruction Ret)]
+  [(LabelExpr "factorial"); (InstructionExpr (Mv (X15, X10)));
+    (InstructionExpr (Li (X10, (Immediate32 1))));
+    (InstructionExpr (Beq (X15, X0, (LabelAddress12 ".L5"))));
+    (LabelExpr ".L2"); (InstructionExpr (Mv (X14, X15)));
+    (InstructionExpr (Addiw (X15, X15, (Immediate12 -1))));
+    (InstructionExpr (Mulw (X10, X14, X10)));
+    (InstructionExpr (Bne (X15, X0, (LabelAddress12 ".L2"))));
+    (LabelExpr ".L5"); (InstructionExpr Ret)]
