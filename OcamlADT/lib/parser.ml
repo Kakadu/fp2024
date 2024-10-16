23a0ff5a4765c57ce231081eafa32f1c11f6a818
qrutyy <qrutyq@gmail.com>
2024-10-16 23:46:32 +0300

    refactor: partialy move from let* to combinators

--- OcamlADT/lib/parser.ml
+++ OcamlADT/lib/parser.ml
@@ -26,15 +26,19 @@ let ptowhitespace = function 'a'..'z' | 'A'..'Z' | '0'..'9' | '_' -> true | _ ->
 
 
 let pident =
-  let* first = pletters in
-  let* rest = take_while ptowhitespace in
-  return ((String.make 1 first) ^ rest)
+  lift2 (fun first rest -> (String.make 1 first) ^ rest)
+    pletters
+    (take_while ptowhitespace)
+
 
 (*                   Constant expressions                         *)
-let pconstintexpr = 
-  let* sign = choice [ token "+"; token "-"; token " "] in 
-  let* n = take_while1 (function '0' .. '9' -> true | _ -> false) in
-  return (Exp_constant (Const_integer (int_of_string (sign ^ n))))
+let pconstintexpr =
+  let parse_sign = choice [ token "+"; token "-"; token " "] in
+  let parse_number = take_while1 (function '0' .. '9' -> true | _ -> false) in
+  lift2 (fun sign n -> Exp_constant (Const_integer (int_of_string (sign ^ n))))
+    parse_sign
+    parse_number
+
 
 let pconstcharexpr =   
   let* _ = token "'" in 
@@ -43,9 +47,9 @@ let pconstcharexpr =
   return (Exp_constant(Const_char c))
 
 let pconststringexpr = 
-  let* _ = token "\"" in
-  let* str = take_while1 (function '"' -> false | _ -> true) in 
-  return (Exp_constant(Const_string str))
+  token "\"" *> lift 
+    (fun str -> Exp_constant(Const_string str)) 
+    (take_while1 (function '"' -> false | _ -> true))
 
 (*                   Arithm utils + ident parser                         *)
 let lchain p op =
@@ -69,36 +73,28 @@ let lchain p op =
     let* x = p in
     loop x
 
-let pidentexpr = 
-  let* ident = pident in 
-  return (Exp_ident(ident))
+let pidentexpr = lift (fun ident -> Exp_ident ident) pident
 
 (*                   Patterns                         *)
 let pany = token "_" *> return Pat_any
-let pvar =
-  let* ident = pident in
-  return (Pat_var ident)
+let pvar = lift (fun ident -> Pat_var ident) pident
 let ppattern =
   pany <|> pvar (* <|> pconstant <|> ptuple <|> pconstruct, will be added in future, not necessary for fact *)
 
 (*                   Exptessions                         *)
 
 let pvalue_binding pexpr =
-  let* pat = ppattern in
-  let* _ = token "=" in
-  let* expr = pexpr in
-  let value_binding = { pat; expr } in
-  return value_binding
-
-let prec_flag = 
-  token "rec" *> return Recursive <|> return Nonrecursive
-
+  let parse_pattern_and_expr =
+    lift2 (fun pat expr -> { pat; expr }) ppattern (token "=" *> pexpr)
+  in
+  parse_pattern_and_expr
+;;
+let prec_flag = token "rec" *> return Recursive <|> return Nonrecursive
+;;
 let pletexpr pexpr =
-  let* _ = token "let" in
-  let* rec_flag = prec_flag in
-  let* value_binding = many1 (pvalue_binding pexpr) in
-  let* expr = pexpr in
-  return (Exp_let(rec_flag, value_binding, expr))
+  token "let" *> lift3 (fun rec_flag value_binding expr -> Exp_let(rec_flag, value_binding, expr))
+  prec_flag (many1 (pvalue_binding pexpr)) pexpr
+;;
 
 let ptupleexpr =
   let* _ = token "(" in
@@ -123,22 +119,23 @@ let pifexpr pexpr =
   return (Exp_if(condition, expr, alternative))
 
 let papplyexpr pexpr =
-  let* func = pexpr in
-  let* argument = pexpr in
-  return (Exp_apply(func, argument))
+  lift2 (fun fexpr sexpr -> Exp_apply(fexpr, sexpr)) pexpr pexpr
+;;
 
 let pfunexpr pexpr = 
-  lift3
+  lift3 
     (fun first_pattern rest_patterns body_expr -> 
       Exp_fun (first_pattern, rest_patterns, body_expr))
     (token "fun" *> ppattern)
     (many ppattern)
     (token "->" *> pexpr)
+;;
 
 let parsebinop binoptoken =
-  let* op = pass_ws *> token binoptoken in  (* Capture the operator from binoptoken *)
-  return (fun e1 e2 -> Exp_apply (Exp_ident op, Exp_tuple(e1, e2, [])))
+  let parse_operator = pass_ws *> token binoptoken in
+  lift (fun op e1 e2 -> Exp_apply (Exp_ident op, Exp_tuple (e1, e2, []))) parse_operator
 ;;
+
 
 let padd = parsebinop "+"
 let psub = parsebinop "-"
@@ -178,15 +175,11 @@ let pexpr = fix (fun expr ->
 
 (*                   Structure items                         *)
 
-let pseval = 
-    let* expr = pexpr in
-    return (Str_eval (expr))
+let pseval = lift (fun expr -> Str_eval(expr)) pexpr
 
 let psvalue = 
-  (* we cant use let+ bc previous results are necessary *)
-  let* rec_flag = prec_flag in
-  let* value_binding = many (pvalue_binding pexpr) in 
-  return (Str_value (rec_flag, value_binding))
+  lift2 (fun rec_flag value_binding -> Str_value (rec_flag, value_binding)) prec_flag (many (pvalue_binding pexpr))
+;;
 
 (** It applies Str_eval to output of expression parser *)
 let pstr_item =
