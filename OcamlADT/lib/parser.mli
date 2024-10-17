568f30b2cf9f48865a1f8e9e80b1697ede7f323b
qrutyy <qrutyq@gmail.com>
2024-10-17 14:38:54 +0300

    feat: add parser mli and remove main

--- OcamlADT/lib/parser.mli
+++ OcamlADT/lib/parser.mli
@@ -1,3 +1,11 @@
 (** Copyright 2024-2025, Rodion Suvorov, Mikhail Gavrilenko *)
 
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
+
+(** 
+  Parses a given input string into a list of structure items.
+
+  @param str The input string to parse.
+  @return An [Ok] containing the parsed structure or an [Error] message.
+*)
+val parse : string -> (Ast.program, string) result
