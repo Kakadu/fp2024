590cb793acf6c4c8ab746b8db378a9cae251f60f
Kostya <koreshin67@gmail.com>
2024-10-18 22:58:24 +0300

    feat: implement REPL

--- Haskell/bin/main.ml
+++
@@ -1,9 +0,0 @@
-(** Copyright 2024, Kostya Oreshin and Nikita Shchutskii *)
-
-(** SPDX-License-Identifier: MIT *)
-
-open Haskell_lib.Parser
-
-(* fac n = if n < 0 then Nothing else Just (save_fac n) where save_fac y | y == 0  = 1 | otherwise = y * save_fac (y - 1) *)
-
-let () = parse_and_print_line (read_line ())
