a79089ec1cc173e1f3c2ae12b9bd2e23270a938d
Daniel Vlasenco <vlasenko.daniil26@gmail.com>
2024-10-19 12:53:37 +0300

    feat(ast): add idents and more patterns, edit measures
    Signed-off-by: Daniel Vlasenco <vlasenko.daniil26@gmail.com>

--- FSharpUnitsOfMeasure/lib/ast/ast.ml
+++ FSharpUnitsOfMeasure/lib/ast/ast.ml
@@ -2,23 +2,27 @@
 
 (** SPDX-License-Identifier: MIT *)
 
+(** Identificators: [x], [foo], [_a] *)
+type ident = string [@@deriving show { with_path = false }]
+
 type rational_exp =
   | Integer of int (** Integer exponent: [2] *)
   | Rational of int * int (** Rational exponent: [3/2] *)
   | Negate of rational_exp (** Negation of exponent: [-1], [-(1/2)] *)
-  | Paren of rational_exp (** Parentheses around exponent: [(5)], [(-2/3)] *)
+  | Paren of rational_exp (** Parentheses around exponent: [(2/3)] *)
 [@@deriving show { with_path = false }]
 
 type measure =
-  | Measure_ident of string (** Measure identificator: [<m>] *)
-  | Measure_prod of measure * measure (** Measure product: [<sec * h>], [<kg m>] *)
-  | Measure_div of measure * measure (** Measure division: [<m / sec>] *)
-  | Measure_pow of measure * rational_exp (** Measure to the rational power: [<cm^3>] *)
+  | Measure_ident of ident (** Measure identificator: [m] *)
+  | Measure_prod of measure * measure (** Measure product: [sec * h], [kg m] *)
+  | Measure_div of measure * measure (** Measure division: [m / sec] *)
+  | Measure_pow of measure * rational_exp (** Measure to the rational power: [cm^3] *)
+  | Measure_paren of measure (** Parentheses around measure: [(kg / m^3)]*)
 [@@deriving show { with_path = false }]
 
-type measure_val =
-  | Mval_int of int (** Integer numbers with units of measure *)
-  | Mval_float of float (** Real numbers with units of measure *)
+type measure_num =
+  | Mnum_int of int (** Integer numbers with units of measure *)
+  | Mnum_float of float (** Real numbers with units of measure *)
 [@@deriving show { with_path = false }]
 
 type constant =
@@ -27,18 +31,20 @@ type constant =
   | Const_char of char (** Char constants: ['a'] *)
   | Const_string of string (** String constants: ["foo"] *)
   | Const_float of float (** Float constants: [3.14], [1e+5], [5.9E-3] *)
-  | Const_measure of measure_val * measure (** Measure constants: [5.0<cm>], [3<kg>] *)
+  | Const_measure of measure_num * measure (** Measure constants: [5.0<cm>], [3<kg>] *)
 [@@deriving show { with_path = false }]
 
 type pattern =
   | Pattern_wild (** Wildcard patterns [_] *)
-  | Pattern_ident of string (** Identificator name patterns: [x] *)
+  | Pattern_ident of ident (** Identificator patterns: [x] *)
   | Pattern_const of constant
   (** Constant patterns: [1], ['a'], ["foo"], [3.14], [5.0<cm>] *)
   | Pattern_tuple of pattern list
   (** Tuple patterns: [(P1; ..., Pn)]
       Invariant: [n >= 2] *)
   | Pattern_or of pattern * pattern (** Or patterns: [P1 | P2] *)
+  | Pattern_cons of pattern * pattern (** Cons patterns: [P1 :: P2] *)
+  | Pattern_list of pattern list (** List patterns: [P1, ..., Pn] *)
 [@@deriving show { with_path = false }]
 
 type rec_flag =
@@ -53,12 +59,12 @@ type val_binding =
 and expression =
   | Expr_const of constant
   (** Constant expressions: [1], ['a'], ["foo"], [3.14], [true], [5.0<cm>] *)
-  | Expr_ident of string (** Identificator name expressions: [x] *)
+  | Expr_ident of ident (** Identificator name expressions: [x] *)
   | Expr_tuple of expression list
   (** Tuple expressions: [(E1, ..., En)]
       Invariant: [n >= 2] *)
   | Expr_fun of pattern * expression
-  (** Anonimous functions: [Exp_fun(P, E)] represents [fun P -> E] *)
+  (** Anonimous functions: [Expr_fun(P, E)] represents [fun P -> E] *)
   | Expr_let of rec_flag * val_binding list * expression
   (** [Expr_let(rec_flag, [(P1, E1); ...; (Pn, En)], E)] represents:
       - [let P1 = E1 and ... and Pn = En in E] when rec_flag is Nonrecursive
@@ -72,13 +78,22 @@ and expression =
       Invariant: [n >= 1] *)
 [@@deriving show { with_path = false }]
 
+type type_def =
+  | Measure_type_def of ident * measure option
+  (** Measure type definition:
+      - [[<Measure>] type I] when measure is None
+      - [[<Measure>] type I = M] when measure is Some M *)
+[@@deriving show { with_path = false }]
+
 type structure_item =
-  | Str_eval of expression (** Structure item which is single expression: [E] *)
-  | Str_value of rec_flag * val_binding list
-  (** [Str_value(rec_flag, [(P1, E1); ...; (Pn, En)])] represents:
+  | Str_item_eval of expression
+  (** Structure item which is single expression: [E] or [do E] *)
+  | Str_item_def of rec_flag * val_binding list
+  (** [Str_item_def(rec_flag, [(P1, E1); ...; (Pn, En)])] represents:
       - [let P1 = E1 and ... and Pn = En] when rec_flag is Nonrecursive
       - [let rec P1 = E1 and ... and Pn = En] when rec_flag is Recursive
         Invariant: [n >= 1] *)
+  | Str_item_type_def of type_def
 [@@deriving show { with_path = false }]
 
 type program = structure_item list [@@deriving show { with_path = false }]
