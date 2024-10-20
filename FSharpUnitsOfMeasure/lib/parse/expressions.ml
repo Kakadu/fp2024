3e953a75fe9082013d864d70b9e95effdf2726d4
Daniel Vlasenco <vlasenko.daniil26@gmail.com>
2024-10-20 12:31:04 +0300

    feat: add parser of ifthenelse expression
    Signed-off-by: Daniel Vlasenco <vlasenko.daniil26@gmail.com>

--- FSharpUnitsOfMeasure/lib/parse/expressions.ml
+++ FSharpUnitsOfMeasure/lib/parse/expressions.ml
@@ -10,6 +10,17 @@ open Ast
 open Angstrom
 open Common
 
-let parse_expr_const = parse_const >>| fun c -> Expr_const c
 let parse_expr_ident = parse_ident >>| fun i -> Expr_ident i
-let parse_expr = choice [ parse_expr_const; parse_expr_ident ]
+let parse_expr_const = parse_const >>| fun c -> Expr_const c
+
+let parse_expr_ite parse_expr =
+  let* cond = skip_token "if" *> parse_expr in
+  let* branch1 = skip_token "then" *> parse_expr in
+  let* branch2 = skip_token "else" *> option None (parse_expr >>| fun e -> Some e) in
+  return (Expr_ifthenelse (cond, branch1, branch2))
+;;
+
+let parse_expr =
+  fix (fun parse_expr ->
+    choice [ parse_expr_const; parse_expr_ident; parse_expr_ite parse_expr ])
+;;
