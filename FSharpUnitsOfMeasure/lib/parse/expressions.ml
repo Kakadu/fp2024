60c877e22ff0c224072703fc8a7ff659c5cde08b
Daniel Vlasenco <vlasenko.daniil26@gmail.com>
2024-10-20 20:55:07 +0300

    feat: add parser of application expression
    Signed-off-by: Daniel Vlasenco <vlasenko.daniil26@gmail.com>

--- FSharpUnitsOfMeasure/lib/parse/expressions.ml
+++ FSharpUnitsOfMeasure/lib/parse/expressions.ml
@@ -6,8 +6,8 @@
     https://fsharp.org/specs/language-spec/4.1/FSharpSpec-4.1-latest.pdf, page 292 *)
 
 open Base
-open Ast
 open Angstrom
+open Ast
 open Common
 
 let parse_expr_ident = parse_ident >>| fun i -> Expr_ident i
@@ -16,11 +16,22 @@ let parse_expr_const = parse_const >>| fun c -> Expr_const c
 let parse_expr_ite parse_expr =
   let* cond = skip_token "if" *> parse_expr in
   let* branch1 = skip_token "then" *> parse_expr in
-  let* branch2 = skip_token "else" *> option None (parse_expr >>| fun e -> Some e) in
+  let* branch2 = option None (skip_token "else" *> parse_expr >>| fun e -> Some e) in
   return (Expr_ifthenelse (cond, branch1, branch2))
+;;
+
+let parse_expr_app parse_expr =
+  let* expr1 = parse_expr in
+  let* expr2 = parse_expr in
+  return (Expr_apply (expr1, expr2))
 ;;
 
 let parse_expr =
   fix (fun parse_expr ->
-    choice [ parse_expr_const; parse_expr_ident; parse_expr_ite parse_expr ])
+    choice
+      [ parse_expr_const
+      ; parse_expr_ident
+      ; parse_expr_ite parse_expr
+      ; parse_expr_app parse_expr
+      ])
 ;;
--- FSharpUnitsOfMeasure/lib/parse/expressions.mli
+++ FSharpUnitsOfMeasure/lib/parse/expressions.mli
@@ -1,6 +1,13 @@
+(** Copyright 2024, Vlasenco Daniel and Strelnikov Andrew *)
+
+(** SPDX-License-Identifier: MIT *)
+
+(** This file contains parsers for part of F# 4.1 grammar, taken from
+    https://fsharp.org/specs/language-spec/4.1/FSharpSpec-4.1-latest.pdf, page 292 *)
+
 open Base
-open Ast
 open Angstrom
+open Ast
 
 (** [parse_expr] parses expression and returns it *)
 val parse_expr : expression t
