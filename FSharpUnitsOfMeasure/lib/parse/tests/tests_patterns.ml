00298b238b6512e5e1c5a544aedf7c9eb9a2674a
Daniel Vlasenco <vlasenko.daniil26@gmail.com>
2024-10-31 11:47:43 +0300

    feat: add parser for tuple pats and pats in paren, add tests for them
    Signed-off-by: Daniel Vlasenco <vlasenko.daniil26@gmail.com>

---
+++ FSharpUnitsOfMeasure/lib/parse/tests/tests_patterns.ml
@@ -0,0 +1,83 @@
+(** Copyright 2024, Vlasenco Daniel and Strelnikov Andrew *)
+
+(** SPDX-License-Identifier: MIT *)
+
+open Base
+open Ast
+open Pp
+open Parse.Patterns
+
+(************************** Idents **************************)
+
+let%expect_test "single underscore should be parsed as a wildcard" =
+  pp pp_pattern parse_pat {| _ |};
+  [%expect {| Pattern_wild |}]
+;;
+
+let%expect_test "parse ident as pattern ident" =
+  pp pp_pattern parse_pat {| x |};
+  [%expect {| (Pattern_ident "x") |}]
+;;
+
+(************************** Consts **************************)
+
+let%expect_test "parse const int as pattern const int" =
+  pp pp_pattern parse_pat {| 1 |};
+  [%expect {| (Pattern_const (Const_int 1)) |}]
+;;
+
+(************************** Tuples **************************)
+
+let%expect_test "parse pattern tuple with 0 elements should fail" =
+  pp pp_pattern parse_pat {| , |};
+  [%expect {| : no more choices |}]
+;;
+
+let%expect_test "parse pattern tuple with 1 element should fail" =
+  pp pp_pattern parse_pat {| 1, |};
+  [%expect {| : end_of_input |}]
+;;
+
+let%expect_test "parse pattern tuple with 2 elements" =
+  pp pp_pattern parse_pat {| 1, 2 |};
+  [%expect {| (Pattern_tuple [(Pattern_const (Const_int 1)); (Pattern_const (Const_int 2))]) |}]
+;;
+
+let%expect_test "parse pattern tuple with 3 elements" =
+  pp pp_pattern parse_pat {| 1, 2, myname |};
+  [%expect {|
+    (Pattern_tuple
+       [(Pattern_const (Const_int 1)); (Pattern_const (Const_int 2));
+         (Pattern_ident "myname")]) |}]
+;;
+
+let%expect_test "parse tuples of tuples" =
+  pp pp_pattern parse_pat {| (1, 2), (3, 4) |};
+  [%expect {|
+    (Pattern_tuple
+       [(Pattern_tuple
+           [(Pattern_const (Const_int 1)); (Pattern_const (Const_int 2))]);
+         (Pattern_tuple
+            [(Pattern_const (Const_int 3)); (Pattern_const (Const_int 4))])
+         ]) |}]
+;;
+
+(************************** Parentheses **************************)
+
+let%expect_test "parse const in parentheses as pattern const" =
+  pp pp_pattern parse_pat {| ( 1 ) |};
+  [%expect {| (Pattern_const (Const_int 1)) |}]
+;;
+
+let%expect_test "parse pattern in unbalanced parentheses should fail" =
+  pp pp_pattern parse_pat {| ( 1 ))) |};
+  [%expect {| : end_of_input |}]
+;;
+
+let%expect_test "parse tuple in parentheses" =
+  pp pp_pattern parse_pat {| (1, 2, 3, 4) |};
+  [%expect {|
+    (Pattern_tuple
+       [(Pattern_const (Const_int 1)); (Pattern_const (Const_int 2));
+         (Pattern_const (Const_int 3)); (Pattern_const (Const_int 4))]) |}]
+;;
