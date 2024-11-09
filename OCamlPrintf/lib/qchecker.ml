cb2f4b711b0ba5ddd6915932853c9a8bb7535421
Maxim Rodionov <rodionov.maxim05@gmail.com>
2024-11-09 07:25:12 +0300

    feat: rework ast generation
    Signed-off-by: Maxim Rodionov <rodionov.maxim05@gmail.com>

--- OCamlPrintf/lib/qchecker.ml
+++ OCamlPrintf/lib/qchecker.ml
@@ -5,14 +5,17 @@
 open Ast
 open Pprinter
 
+(* Only for debug *)
+let coefficient = 50
+
 module TestQCheck = struct
   open QCheck.Gen
 
   (* Manual generator *)
 
-  let gen_char = map Char.chr (int_range (Char.code 'a') (Char.code 'z'))
+  let gen_char = map Char.chr (int_range (Char.code 'a') (Char.code 'h'))
   let gen_int = int_range 0 10000
-  let gen_ident = string_size (int_range 1 2) ~gen:gen_char
+  let gen_ident = string_size (int_range 1 3) ~gen:gen_char
 
   let gen_constant =
     frequency
@@ -28,19 +31,30 @@ module TestQCheck = struct
       match n with
       | 0 ->
         frequency
-          [ (* 1, return Pat_any
-               ; *)
-            1, map (fun i -> Pat_var i) gen_ident
+          [ 1, return Pat_any
+          ; 1, map (fun i -> Pat_var i) gen_ident
           ; 1, map (fun c -> Pat_constant c) gen_constant
           ]
       | n ->
         frequency
-          [ 1, map (fun t -> Pat_tuple t) (list_size (int_range 2 2) (self (n / 10)))
-            (* ; ( 1
-               , map2
-               (fun name pat_opt -> Pat_construct (name, pat_opt))
-               gen_ident
-               (option (self (n / 10))) ) *)
+          [ ( 1
+            , map
+                (fun t -> Pat_tuple t)
+                (list_size (int_range 2 5) (self (n / coefficient))) )
+          ; 1, return (Pat_construct ("[]", None))
+          ; ( 1
+            , let rec gen_list n =
+                if n = 0
+                then return (Pat_construct ("[]", None))
+                else (
+                  let element = self 0 in
+                  let tail = gen_list (n - 1) in
+                  map2
+                    (fun e t -> Pat_construct ("::", Some (Pat_tuple [ e; t ])))
+                    element
+                    tail)
+              in
+              gen_list (Random.int 5) )
           ])
   ;;
 
@@ -55,59 +69,78 @@ module TestQCheck = struct
           ]
       | n ->
         frequency
-          [ (* ( 1
-               , map3
-               (fun rec_fl value exp -> Exp_let (rec_fl, value, exp))
-               (frequency [ 1, return Nonrecursive; 1, return Recursive ])
-               (list_size (int_range 1 2) gen_value_binding)
-               (self (n / 2)) )
-               ; *)
-            ( 1
+          [ ( 1
+            , map3
+                (fun rec_fl value_binding_list exp ->
+                  Exp_let (rec_fl, value_binding_list, exp))
+                (frequency [ 1, return Nonrecursive; 1, return Recursive ])
+                (list_size
+                   (int_range 1 3)
+                   (map2 (fun pat exp -> { pat; exp }) gen_pattern (self (Random.int 3))))
+                (self (n / coefficient)) )
+          ; ( 1
             , map2
                 (fun pat exp -> Exp_fun (pat, exp))
-                (list_size (int_range 1 2) gen_pattern)
-                (self (n / 10)) )
+                (list_size (int_range 1 3) gen_pattern)
+                (self (n / coefficient)) )
           ; ( 1
             , map2
                 (fun exp exp_list -> Exp_apply (exp, exp_list))
-                (self (n / 10))
-                (list_size (int_range 1 2) (self (n / 10))) )
-            (* ; ( 1
-               , map2
-               (fun exp case -> Exp_match (exp, case))
-               (self (n / 2))
-               (list_size (int_range 1 2) gen_case) ) *)
-            (* ; 1, map (fun exp -> Exp_tuple exp) (list_size (int_range 1 2) (self (n / 10))) *)
-            (* ; ( 1
-               , map2
-               (fun name pat_opt -> Exp_construct (name, pat_opt))
-               gen_ident
-               (option (self (n / 10))) ) *)
+                (self 0)
+                (list_size (int_range 1 3) (self (n / coefficient))) )
+          ; ( 1
+            , map2
+                (fun exp case_list -> Exp_match (exp, case_list))
+                (self (n / coefficient))
+                (list_size
+                   (int_range 1 3)
+                   (map2
+                      (fun left right -> { left; right })
+                      gen_pattern
+                      (self (Random.int 3)))) )
+          ; ( 1
+            , map
+                (fun exp_list -> Exp_tuple exp_list)
+                (list_size (int_range 2 5) (self 0)) )
+          ; 1, return (Exp_construct ("[]", None))
+          ; ( 1
+            , let rec gen_list n =
+                if n = 0
+                then return (Exp_construct ("[]", None))
+                else (
+                  let element = self 0 in
+                  let tail = gen_list (n - 1) in
+                  map2
+                    (fun e t -> Exp_construct ("::", Some (Exp_tuple [ e; t ])))
+                    element
+                    tail)
+              in
+              gen_list (Random.int 5) )
           ; ( 1
             , map3
                 (fun i t e -> Exp_ifthenelse (i, t, e))
-                (self (n / 10))
-                (self (n / 10))
-                (option (self (n / 10))) )
+                (self (n / coefficient))
+                (self (n / coefficient))
+                (option (self (n / coefficient))) )
           ; ( 1
             , map2
                 (fun exp1 exp2 -> Exp_sequence (exp1, exp2))
-                (self (n / 10))
-                (self (n / 10)) )
+                (self (n / coefficient))
+                (self (n / coefficient)) )
           ])
   ;;
 
   let gen_value_binding = map2 (fun pat exp -> { pat; exp }) gen_pattern gen_expression
-  (* and gen_case = map2 (fun left right -> { left; right }) gen_pattern gen_expression *)
 
   let gen_structure_item =
     frequency
       [ 1, map (fun exp -> Struct_eval exp) gen_expression
       ; ( 1
         , map2
-            (fun rec_flag value -> Struct_value (rec_flag, value))
+            (fun rec_flag value_binding_list ->
+              Struct_value (rec_flag, value_binding_list))
             (frequency [ 1, return Nonrecursive; 1, return Recursive ])
-            (list_size (int_range 1 2) gen_value_binding) )
+            (list_size (int_range 1 3) gen_value_binding) )
       ]
   ;;
 
@@ -121,6 +154,7 @@ module TestQCheck = struct
     QCheck_runner.run_tests
       [ QCheck.(
           Test.make arbitrary_lam_manual (fun l ->
+            Format.printf "%a \n" Pprinter.pp_structure l;
             Result.ok l = Parser.parse (Format.asprintf "%a" Pprinter.pp_structure l)))
       ]
   ;;
