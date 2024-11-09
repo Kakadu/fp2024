c9ecc23429abf7ab461604ca497ef597556a1b03
Lutsyuk Victoria <victorialutsyuk.t@gmail.com>
2024-11-09 23:48:21 +0300

    feat: add ast expressions
    Signed-off-by: Lutsyuk Victoria <victorialutsyuk.t@gmail.com>

--- ELutsyuk/lib/ast.ml
+++ ELutsyuk/lib/ast.ml
@@ -1,10 +1,9 @@
-(* не пойму, зачем в каждом файле писать, что под MIT... *)
 (* CFG grammar for OCaml https://askra.de/software/ocaml-doc/4.02/full-grammar.html#sec91 *)
 
-type id = Id of string (* for expressions' identification *)
-[@@deriving show { with_path = false }]
+type id = string (* expression identificator *) [@@deriving show { with_path = false }]
 
-type const =
+(* constants *)
+type literal =
   | Int of int (* ex: 1, 2, 3 *)
   | Str of string (* ex: "hello", "miniML" *)
   | Bool of bool (* true, false *)
@@ -12,18 +11,46 @@ type const =
 
 type binary_op =
   | Eq (* = *)
-  | Less (* < *)
-  | More (* > *)
+  | NotEq (* != *)
+  | Lt (* < *)
+  | LtEq (* <= *)
+  | Gt (* > *)
+  | GtEq (* >= *)
   | And (* && *)
   | Or (* || *)
-  | Plus (* + *)
-  | Min (* - *)
-  | Mult (* * *)
+  | Add (* + *)
+  | Sub (* - *)
+  | Mul (* * *)
   | Div (* / *)
 [@@deriving show { with_path = false }]
 
-type unary_op =
-  | Not (* ! *)
-  | UnPlus (* + *)
-  | UnMin (* - *)
+type pattern =
+  | PatVar of id (* | var -> ... *)
+  | PatAny (* | _ -> ... *)
+  | PatLiteral of literal (* | literal -> ... *)
+  | PatCons of pattern * pattern (* | expr1 :: expr2 -> ... *)
+  | PatSome of pattern (* | Some(pattern) -> ... *)
+  | PatNone (* | None -> ... *)
+[@@deriving show { with_path = false }]
+
+type recursion_state =
+  | Rec
+  | NonRec
+[@@deriving show { with_path = false }]
+
+type expression =
+  | ExpVar of id (* variable name *)
+  | ExpConst of literal (* literal *)
+  | ExpBinop of binary_op * expression * expression (* expr binop expr *)
+  | ExpLet of
+      recursion_state * (id * expression) list * expression (* let [rec] expr in expr *)
+  | ExpApp of expression * expression (* expr expr *)
+  | ExpLambda of id * expression (* fun id -> expr *)
+  | ExpIf of expression * expression * expression option (* if exp then exp else exp *)
+  | ExpWhile of expression * expression (* while expr do expr done *)
+  | ExpTuple of expression * expression * expression list (* at least two elements *)
+  | ExpList of expression list
+  | ExpOptSome of expression (* Some(expr) *)
+  | ExpOptNone (* None *)
+  | ExpMatch of expression * (pattern * expression) list
 [@@deriving show { with_path = false }]
