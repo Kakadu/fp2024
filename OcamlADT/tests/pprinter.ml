837549188efcf28213690b40cfc244e699588f77
qrutyy <qrutyq@gmail.com>
2024-11-11 13:59:04 +0300

    refactor: style up pprinter

--- OcamlADT/tests/pprinter.ml
+++ OcamlADT/tests/pprinter.ml
@@ -1,7 +1,7 @@
-open Ocamladt_lib.Parser
-open Ocamladt_lib.Ast
+open Angstrom
 
 let pp printer parser str =
-  match Angstrom.parse_string ~consume:Angstrom.Consume.All parser str with
+  match parse_string ~consume:Angstrom.Consume.All parser str with
   | Ok res -> printer Format.std_formatter res
   | Error _ -> print_endline "Syntax error"
+;;
