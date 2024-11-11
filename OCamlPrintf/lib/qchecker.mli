2aac8ea36a3250d4d1bb89e547ad7d2ef20b688d
Maxim Rodionov <rodionov.maxim05@gmail.com>
2024-11-11 16:29:22 +0300

    fix: zanuda comment
    Signed-off-by: Maxim Rodionov <rodionov.maxim05@gmail.com>

--- OCamlPrintf/lib/qchecker.mli
+++ OCamlPrintf/lib/qchecker.mli
@@ -2,7 +2,6 @@
 
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
-module TestQCheck :
-  sig
-    val run_manual : unit -> int
-  end
+module TestQCheck : sig
+  val run_manual : unit -> int
+end
