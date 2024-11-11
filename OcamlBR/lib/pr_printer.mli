c50eeb7bea6b699fac7a96910cee308b513fd419
mshipilov5 <shipilovmaksim05@gmail.com>
2024-11-12 02:07:13 +0300

    feat: changed .gitignore
    Signed-off-by: mshipilov5 <shipilovmaksim05@gmail.com>

--- OcamlBR/lib/pr_printer.mli
+++ OcamlBR/lib/pr_printer.mli
@@ -1,6 +1,7 @@
 (** Copyright 2024, Sofya Kozyreva, Maksim Shipilov *)
 
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
+
 open Ast
 
 val prpr_structure : Format.formatter -> Ast.structure_item list -> unit
