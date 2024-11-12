5a7c6d64ab5a630d8185696b60a316562b3ca13a
Daniel Vlasenco <vlasenko.daniil26@gmail.com>
2024-11-12 17:23:23 +0300

    feat: add chainr, parsers for logic and unstrict comparison ops
    Signed-off-by: Daniel Vlasenco <vlasenko.daniil26@gmail.com>

--- FSharpUnitsOfMeasure/lib/parse/tests/tests_expressions.ml
+++ FSharpUnitsOfMeasure/lib/parse/tests/tests_expressions.ml
@@ -127,6 +127,7 @@ let%expect_test "parse float with rational part, (e|E) but without exponent shou
   [%expect {| : no more choices  |}]
 ;;
 
+<<<<<<< HEAD
 =======
 (* Is incorrect *)
 let%expect_test "parse simple float as expr const float" =
@@ -137,6 +138,8 @@ let%expect_test "parse simple float as expr const float" =
 >>>>>>> a5e8666 (Add main common, patterns, expressions and structure items parsers, sufficient for parsing factorial. (#5))
 (* Need to try to test measures *)
 
+=======
+>>>>>>> 9645b37 (feat: add chainr, parsers for logic and unstrict comparison ops)
 (************************** If then else **************************)
 
 let%expect_test "parse ite with else branch" =
@@ -253,6 +256,40 @@ let%expect_test "parse a+b*c" =
       (Expr_apply ((Expr_apply ((Expr_ident_or_op "+"), (Expr_ident_or_op "a"))),
          (Expr_apply (
             (Expr_apply ((Expr_ident_or_op "*"), (Expr_ident_or_op "b"))),
+            (Expr_ident_or_op "c")))
+         )) |}]
+;;
+
+let%expect_test "parse a <= b <= c" =
+  pp pp_expression parse_expr {| a <= b <= c |};
+  [%expect {|
+      (Expr_apply (
+         (Expr_apply ((Expr_ident_or_op "<="),
+            (Expr_apply (
+               (Expr_apply ((Expr_ident_or_op "<="), (Expr_ident_or_op "a"))),
+               (Expr_ident_or_op "b")))
+            )),
+         (Expr_ident_or_op "c"))) |}]
+;;
+
+let%expect_test "parse a || b || c" =
+  pp pp_expression parse_expr {| a || b || c |};
+  [%expect
+    {|
+      (Expr_apply ((Expr_apply ((Expr_ident_or_op "||"), (Expr_ident_or_op "a"))),
+         (Expr_apply (
+            (Expr_apply ((Expr_ident_or_op "||"), (Expr_ident_or_op "b"))),
+            (Expr_ident_or_op "c")))
+         )) |}]
+;;
+
+let%expect_test "parse a && b && c" =
+  pp pp_expression parse_expr {| a && b && c |};
+  [%expect
+    {|
+      (Expr_apply ((Expr_apply ((Expr_ident_or_op "&&"), (Expr_ident_or_op "a"))),
+         (Expr_apply (
+            (Expr_apply ((Expr_ident_or_op "&&"), (Expr_ident_or_op "b"))),
             (Expr_ident_or_op "c")))
          )) |}]
 ;;
