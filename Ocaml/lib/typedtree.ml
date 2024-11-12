8a68a7de209d219ef2c1b2acc113f74439fe708c
Kadochnikov-Daniil <daniil@Daniil>
2024-11-13 02:38:20 +0300

    Adds typed tree for inferencer

---
+++ Ocaml/lib/typedtree.ml
@@ -0,0 +1,31 @@
+(** Copyright 2021-2023, Kakadu *)
+
+(** SPDX-License-Identifier: LGPL-3.0-or-later *)
+
+type binder = int [@@deriving show { with_path = false }]
+
+module VarSet = struct
+  include Stdlib.Set.Make (Int)
+
+  let pp ppf s =
+    Format.fprintf ppf "[ ";
+    iter (Format.fprintf ppf "%d; ") s;
+    Format.fprintf ppf "]"
+  ;;
+end
+
+type binder_set = VarSet.t [@@deriving show { with_path = false }]
+
+type ty =
+  | TyInt
+  | TyBool
+  | TyString
+  | TyTuple of ty list
+  | TyList of ty
+  | TyOption of ty
+  | Ty_var of binder
+  | Arrow of ty * ty
+  | Ty_And of ty list
+[@@deriving show { with_path = false }]
+
+type scheme = S of binder_set * ty [@@deriving show { with_path = false }]
