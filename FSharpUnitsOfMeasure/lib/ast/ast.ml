d21941c326218140c5613b2de7dc8078e9523377
Daniel Vlasenco <vlasenko.daniil26@gmail.com>
2024-11-13 20:23:51 +0300

    feat: add ppx_deriving_qcheck to deps
    Signed-off-by: Daniel Vlasenco <vlasenko.daniil26@gmail.com>

--- FSharpUnitsOfMeasure/lib/ast/ast.ml
+++ FSharpUnitsOfMeasure/lib/ast/ast.ml
@@ -5,7 +5,7 @@
 type integer_exp =
   | Pos_int_exp of int (** Positive integer exponent: [2] *)
   | Neg_int_exp of int (** Negative integer exponent: [-1] *)
-[@@deriving show { with_path = false }]
+[@@deriving qcheck, show { with_path = false }]
 
 type measure =
   | Measure_ident of string (** Measure identificator: [m] *)
@@ -13,16 +13,16 @@ type measure =
   | Measure_div of measure * measure (** Measure division: [m / sec] *)
   | Measure_pow of measure * integer_exp (** Measure to the integer power: [cm^3] *)
   | Measure_paren of measure (** Parentheses around measure: [(kg / m^3)]*)
-[@@deriving show { with_path = false }]
+[@@deriving qcheck, show { with_path = false }]
 
 type measure_num =
   | Mnum_int of int (** Integer numbers with units of measure *)
   | Mnum_float of float (** Real numbers with units of measure *)
-[@@deriving show { with_path = false }]
+[@@deriving qcheck, show { with_path = false }]
 
 (** Units of measure: [1<m>], [9.8<kg m / s>], [0.3<kg^3>] etc. *)
 type unit_of_measure = Unit_of_measure of measure_num * measure
-[@@deriving show { with_path = false }]
+[@@deriving qcheck, show { with_path = false }]
 
 type constant =
   | Const_bool of bool (** Boolean constants [true] and [false] *)
@@ -32,7 +32,7 @@ type constant =
   | Const_float of float (** Float constants: [3.14], [1e+5], [5.9E-3f] *)
   | Const_unit_of_measure of unit_of_measure
   (** Units of measure constants: [5.0<cm>], [3<kg>] *)
-[@@deriving show { with_path = false }]
+[@@deriving qcheck, show { with_path = false }]
 
 type pattern =
   | Pattern_wild (** Wildcard patterns [ _ ] *)
@@ -44,16 +44,16 @@ type pattern =
       Invariant: [n >= 2] *)
   | Pattern_or of pattern * pattern (** WIP Or patterns: [P1 | P2] *)
   | Pattern_list of pattern list (** List patterns: [P1, ..., Pn] *)
-[@@deriving show { with_path = false }]
+[@@deriving qcheck, show { with_path = false }]
 
 type rec_flag =
   | Nonrecursive (** For nonrecursive function declarations *)
   | Recursive (** For recursive function declarations *)
-[@@deriving show { with_path = false }]
+[@@deriving qcheck, show { with_path = false }]
 
 type val_binding =
   | Binding of pattern * expression (** [Binding(P, E)] represents [let P = E] *)
-[@@deriving show { with_path = false }]
+[@@deriving qcheck, show { with_path = false }]
 
 and expression =
   | Expr_const of constant
@@ -75,14 +75,14 @@ and expression =
   | Expr_match of expression * (pattern * expression) list
   (** [match E with P1 -> E1 | ... | Pn -> En]
       Invariant: [n >= 1] *)
-[@@deriving show { with_path = false }]
+[@@deriving qcheck, show { with_path = false }]
 
 type type_def =
   | Measure_type_def of string * measure option
   (** Measure type definition:
       - [[<Measure>] type I] when measure is None
       - [[<Measure>] type I = M] when measure is Some M *)
-[@@deriving show { with_path = false }]
+[@@deriving qcheck, show { with_path = false }]
 
 type structure_item =
   | Str_item_eval of expression
@@ -93,6 +93,6 @@ type structure_item =
       - [let rec P1 = E1 and ... and Pn = En] when rec_flag is Recursive
         Invariant: [n >= 1] *)
   | Str_item_type_def of type_def (** Structure item which is type definition *)
-[@@deriving show { with_path = false }]
+[@@deriving qcheck, show { with_path = false }]
 
-type program = structure_item list [@@deriving show { with_path = false }]
+type program = structure_item list [@@deriving qcheck, show { with_path = false }]
