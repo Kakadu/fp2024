78a8b7570e9b49dfc9f8e8b8d9c8b3b6df3cb389
Vladimir Zaikin <friend.zva@yandex.ru>
2024-11-16 01:44:47 +0300

    test: add pprinter to parser tests
    Signed-off-by: Vladimir Zaikin <friend.zva@yandex.ru>

--- OCamlPrintf/tests/test_parser.ml
+++ OCamlPrintf/tests/test_parser.ml
@@ -2,279 +2,59 @@
 
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
-open Ocaml_printf_lib.Ast
 open Ocaml_printf_lib.Parser
+open Ocaml_printf_lib.Pprinter
 
 let run str =
   match parse str with
-  | Ok ast -> print_endline (show_structure ast)
-  | Error error -> print_endline error
+  | Ok ast -> Format.printf "%a \n" pp_structure ast
+  | Error error -> Format.printf "%s" error
 ;;
 
-let%expect_test "parse_factorial_with_match" =
+let%expect_test "parsing value structure and factorial with `match'" =
   run
     {|
-    let rec factorial n =
-      match n with
-      | 0 -> 1
-      | 1 -> 1
-      | _ -> n * factorial (n - 1)
-    ;;
-    |};
+  let rec factorial n =
+    match n with
+    | 0 -> 1
+    | 1 -> 1
+    | _ -> n * factorial (n - 1)
+  ;;
+  |};
   [%expect
     {|
-    [(Struct_value (Recursive,
-        [{ pat = (Pat_var "factorial");
-           exp =
-           (Exp_fun ([(Pat_var "n")],
-              (Exp_match ((Exp_ident "n"),
-                 [{ left = (Pat_constant (Const_integer 0));
-                    right = (Exp_constant (Const_integer 1)) };
-                   { left = (Pat_constant (Const_integer 1));
-                     right = (Exp_constant (Const_integer 1)) };
-                   { left = Pat_any;
-                     right =
-                     (Exp_apply ((Exp_ident "*"),
-                        [(Exp_ident "n");
-                          (Exp_apply ((Exp_ident "factorial"),
-                             [(Exp_apply ((Exp_ident "-"),
-                                 [(Exp_ident "n");
-                                   (Exp_constant (Const_integer 1))]
-                                 ))
-                               ]
-                             ))
-                          ]
-                        ))
-                     }
-                   ]
-                 ))
-              ))
-           }
-          ]
-        ))
-      ]
-    |}]
+  let rec factorial = (fun n -> (match n with | 0 -> 1 | 1 -> 1 | _ -> n * (factorial (n - 1))));;
+  |}]
 ;;
 
-let%expect_test "parse_exp_fun" =
-  run "let sum = fun x -> (fun y -> x + y);;";
-  [%expect
-    {|
-    [(Struct_value (Nonrecursive,
-        [{ pat = (Pat_var "sum");
-           exp =
-           (Exp_fun ([(Pat_var "x")],
-              (Exp_fun ([(Pat_var "y")],
-                 (Exp_apply ((Exp_ident "+"), [(Exp_ident "x"); (Exp_ident "y")]
-                    ))
-                 ))
-              ))
-           }
-          ]
-        ))
-      ]
-    |}]
+let%expect_test "parsing expression with `fun'" =
+  run {|
+  let sum = fun x -> (fun y -> x + y);;
+  |};
+  [%expect {|
+  let sum = (fun x -> (fun y -> x + y));;
+  |}]
 ;;
 
-let%expect_test "parse_pat_exp_tuples" =
-  run "let a, b = 1, 2";
-  [%expect
-    {|
-    [(Struct_value (Nonrecursive,
-        [{ pat = (Pat_tuple [(Pat_var "a"); (Pat_var "b")]);
-           exp =
-           (Exp_tuple
-              [(Exp_constant (Const_integer 1)); (Exp_constant (Const_integer 2))
-                ])
-           }
-          ]
-        ))
-      ]
-    |}]
+let%expect_test "parsing pattern and expression tuples" =
+  run {|
+  let a, b = 1, 2
+  |};
+  [%expect {|
+  let (a, b) = (1, 2);;
+  |}]
 ;;
 
-let%expect_test "parse_exp_construct" =
-  run "let list_ = [1; 2; 3]";
-  [%expect
-    {|
-    [(Struct_value (Nonrecursive,
-        [{ pat = (Pat_var "list_");
-           exp =
-           (Exp_construct ("::",
-              (Some (Exp_tuple
-                       [(Exp_constant (Const_integer 1));
-                         (Exp_construct ("::",
-                            (Some (Exp_tuple
-                                     [(Exp_constant (Const_integer 2));
-                                       (Exp_construct ("::",
-                                          (Some (Exp_tuple
-                                                   [(Exp_constant
-                                                       (Const_integer 3));
-                                                     (Exp_construct ("[]", None))
-                                                     ]))
-                                          ))
-                                       ]))
-                            ))
-                         ]))
-              ))
-           }
-          ]
-        ))
-      ]
-    |}]
+let%expect_test "parsing expression list" =
+  run {|
+  let list_ = [1; 2; 3]
+  |};
+  [%expect {|
+  let list_ = [1; 2; 3];;
+  |}]
 ;;
 
-let%expect_test "check parse_chain_right_associative" =
-  run "let f x y z = if x = 0 && y = 1 || z >= 2 then 2 else 26;;";
-  [%expect
-    {|
-    [(Struct_value (Nonrecursive,
-        [{ pat = (Pat_var "f");
-           exp =
-           (Exp_fun ([(Pat_var "x"); (Pat_var "y"); (Pat_var "z")],
-              (Exp_ifthenelse (
-                 (Exp_apply ((Exp_ident "||"),
-                    [(Exp_apply ((Exp_ident "&&"),
-                        [(Exp_apply ((Exp_ident "="),
-                            [(Exp_ident "x"); (Exp_constant (Const_integer 0))]));
-                          (Exp_apply ((Exp_ident "="),
-                             [(Exp_ident "y"); (Exp_constant (Const_integer 1))]
-                             ))
-                          ]
-                        ));
-                      (Exp_apply ((Exp_ident ">="),
-                         [(Exp_ident "z"); (Exp_constant (Const_integer 2))]))
-                      ]
-                    )),
-                 (Exp_constant (Const_integer 2)),
-                 (Some (Exp_constant (Const_integer 26)))))
-              ))
-           }
-          ]
-        ))
-      ]
-    |}]
-;;
-
-let%expect_test "parse_struct_eval" =
-  run "8 / 800 - 555 * (35 + 35)";
-  [%expect
-    {|
-    [(Struct_eval
-        (Exp_apply ((Exp_ident "-"),
-           [(Exp_apply ((Exp_ident "/"),
-               [(Exp_constant (Const_integer 8));
-                 (Exp_constant (Const_integer 800))]
-               ));
-             (Exp_apply ((Exp_ident "*"),
-                [(Exp_constant (Const_integer 555));
-                  (Exp_apply ((Exp_ident "+"),
-                     [(Exp_constant (Const_integer 35));
-                       (Exp_constant (Const_integer 35))]
-                     ))
-                  ]
-                ))
-             ]
-           )))
-      ]
-    |}]
-;;
-
-let%expect_test "parse_exp_let" =
-  run "1 + let two = 2 in two * 3";
-  [%expect
-    {|
-      [(Struct_eval
-          (Exp_apply ((Exp_ident "+"),
-             [(Exp_constant (Const_integer 1));
-               (Exp_let (Nonrecursive,
-                  [{ pat = (Pat_var "two"); exp = (Exp_constant (Const_integer 2))
-                     }
-                    ],
-                  (Exp_apply ((Exp_ident "*"),
-                     [(Exp_ident "two"); (Exp_constant (Const_integer 3))]))
-                  ))
-               ]
-             )))
-        ]
-    |}]
-;;
-
-let%expect_test "parse_sequence_and_construct" =
-  run {| [1; 2; 3]; "qwerty123" |};
-  [%expect
-    {|
-    [(Struct_eval
-        (Exp_sequence (
-           (Exp_construct ("::",
-              (Some (Exp_tuple
-                       [(Exp_constant (Const_integer 1));
-                         (Exp_construct ("::",
-                            (Some (Exp_tuple
-                                     [(Exp_constant (Const_integer 2));
-                                       (Exp_construct ("::",
-                                          (Some (Exp_tuple
-                                                   [(Exp_constant
-                                                       (Const_integer 3));
-                                                     (Exp_construct ("[]", None))
-                                                     ]))
-                                          ))
-                                       ]))
-                            ))
-                         ]))
-              )),
-           (Exp_constant (Const_string "qwerty123")))))
-      ]
-    |}]
-;;
-
-let%expect_test "parse_several_structure_items" =
-  run "let squared x = x * x;; squared 5";
-  [%expect
-    {|
-    [(Struct_value (Nonrecursive,
-        [{ pat = (Pat_var "squared");
-           exp =
-           (Exp_fun ([(Pat_var "x")],
-              (Exp_apply ((Exp_ident "*"), [(Exp_ident "x"); (Exp_ident "x")]))))
-           }
-          ]
-        ));
-      (Struct_eval
-         (Exp_apply ((Exp_ident "squared"), [(Exp_constant (Const_integer 5))])))
-      ]
-    |}]
-;;
-
-let%expect_test "parse_sequence_and_list" =
-  run {| [1; 2; 3]; "qwerty123" |};
-  [%expect
-    {|
-    [(Struct_eval
-        (Exp_sequence (
-           (Exp_construct ("::",
-              (Some (Exp_tuple
-                       [(Exp_constant (Const_integer 1));
-                         (Exp_construct ("::",
-                            (Some (Exp_tuple
-                                     [(Exp_constant (Const_integer 2));
-                                       (Exp_construct ("::",
-                                          (Some (Exp_tuple
-                                                   [(Exp_constant
-                                                       (Const_integer 3));
-                                                     (Exp_construct ("[]", None))
-                                                     ]))
-                                          ))
-                                       ]))
-                            ))
-                         ]))
-              )),
-           (Exp_constant (Const_string "qwerty123")))))
-      ]
-    |}]
-;;
-
-let%expect_test "parse_option_and_bool" =
+let%expect_test "parsing option and bool types" =
   run {|
   let f a =
     match a with
@@ -282,23 +62,54 @@ let%expect_test "parse_option_and_bool" =
     | None -> false
   ;;
   |};
+  [%expect {|
+  let f = (fun a -> (match a with | Some (_) -> true | None -> false));;
+  |}]
+;;
+
+let%expect_test "parsing chain right associative" =
+  run {|
+  let f x y z = if x = 0 && y = 1 || z >= 2 then 2 else 26;;
+  |};
   [%expect
     {|
-    [(Struct_value (Nonrecursive,
-        [{ pat = (Pat_var "f");
-           exp =
-           (Exp_fun ([(Pat_var "a")],
-              (Exp_match ((Exp_ident "a"),
-                 [{ left = (Pat_construct ("Some", (Some Pat_any)));
-                    right = (Exp_construct ("true", None)) };
-                   { left = (Pat_construct ("None", None));
-                     right = (Exp_construct ("false", None)) }
-                   ]
-                 ))
-              ))
-           }
-          ]
-        ))
-      ]
-    |}]
+  let f = (fun x y z -> (if x = 0 && y = 1 || z >= 2 then 2 else 26));;
+  |}]
+;;
+
+let%expect_test "parsing evaluation structure and chain left associative" =
+  run {|
+  8 / 800 - 555 * (35 + 35)
+  |};
+  [%expect {|
+  8 / 800 - 555 * (35 + 35);;
+  |}]
+;;
+
+let%expect_test "parsing expression with `let'" =
+  run {|
+  1 + let two = 2 in two * 3
+  |};
+  [%expect {|
+  1 + (let two = 2 in two * 3);;
+  |}]
+;;
+
+let%expect_test "parsing several structure items" =
+  run {|
+  let squared x = x * x;; squared 5
+  |};
+  [%expect {|
+  let squared = (fun x -> x * x);;
+  (squared 5);;
+  |}]
+;;
+
+let%expect_test "parsing sequence and exepression construct" =
+  run {|
+  [1; 2; 3]; "qwerty123"
+  |};
+  [%expect {| 
+  ([1; 2; 3]); ("qwerty123");;
+  |}]
 ;;
--- OCamlPrintf/tests/test_pprinter.mli
+++
@@ -1,3 +0,0 @@
-(** Copyright 2024-2025, Friend-zva, RodionovMaxim05 *)
-
-(** SPDX-License-Identifier: LGPL-3.0-or-later *)
