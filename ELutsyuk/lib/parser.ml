79f6a8b392ea59dd9189cd91475799717156a20a
Lutsyuk Victoria <victorialutsyuk.t@gmail.com>
2024-11-18 22:31:09 +0300

    feat: add int and str parser
    Signed-off-by: Lutsyuk Victoria <victorialutsyuk.t@gmail.com>

--- ELutsyuk/lib/parser.ml
+++ ELutsyuk/lib/parser.ml
@@ -3,6 +3,7 @@
 (* SPDX-License-Identifier: MIT *)
 
 open Angstrom
+open Ast
 
 (*
    from homk: >>| : 'a t -> ('a -> 'b) -> 'b
@@ -50,15 +51,37 @@ let is_keyword = function
 let parse_spaces = skip_while is_space
 let parse_token t = parse_spaces *> t
 
+(** Parse first letter then try parse rest of id *)
 let parse_id =
   let parse_first =
     satisfy (fun ch -> is_letter ch || Char.equal '_' ch) >>| fun ch -> Char.escaped ch
   in
   let parse_rest =
-    take_while (fun ch -> is_letter ch || is_digit ch || Char.equal '_' ch)
+    take_while1 (fun ch -> is_letter ch || is_digit ch || Char.equal '_' ch)
   in
   (* мб функцию применения вынести? *)
   parse_token @@ lift2 (fun x y -> x ^ y) parse_first parse_rest
   >>= fun id ->
   if is_keyword id then fail "Identifier must not match the keyword." else return id
+;;
+
+(* take_while захватывает любое кличество символов
+   take_while1 захватывает хотя бы 1 символ*)
+let parse_int =
+  parse_token @@ take_while1 is_digit >>| fun digit -> Int (int_of_string digit)
+;;
+
+let parse_string =
+  parse_token
+  @@ (char '"'
+      *> many
+           (choice
+              [ take_while1 (function
+                  | '"' | '\\' -> false
+                  | _ -> true)
+              ; string "\\\"" *> return "\""
+              ; string "\\\\" *> return "\\"
+              ]))
+  <* char '"'
+  >>| fun chars -> Str (String.concat "" chars)
 ;;
