12588ba2bc2437b308b741ba5871b89af7f23fa5
mshipilov5 <shipilovmaksim05@gmail.com>
2024-11-27 22:14:40 +0300

    fix: formater
    Signed-off-by: mshipilov5 <shipilovmaksim05@gmail.com>

--- OcamlBR/lib/pr_printer.ml
+++ OcamlBR/lib/pr_printer.ml
@@ -63,116 +63,113 @@ let precedence_bin_op = function
   | Gt | Lt | Eq | Neq | Gte | Lte -> -1
 ;;
 
-  let rec pp_expr ppf expr =
-    let rec needs_parens parent_prec child_prec =
-      child_prec < parent_prec || child_prec == -1
+let rec pp_expr ppf expr =
+  let rec needs_parens parent_prec child_prec =
+    child_prec < parent_prec || child_prec == -1
+  in
+  match expr with
+  | Econst c -> pp_const ppf c
+  | Evar (Id (name, None)) -> fprintf ppf "%s" name
+  | Evar (Id (name, Some suffix)) -> fprintf ppf "%s.%s" name suffix
+  | Eif_then_else (e1, e2, None) -> fprintf ppf "if %a then %a" pp_expr e1 pp_expr e2
+  | Eif_then_else (e1, e2, Some e3) ->
+    fprintf ppf "if %a then %a else %a" pp_expr e1 pp_expr e2 pp_expr e3
+  | Ematch (exp, Ecase (first_pat, first_expr), rest_cases) ->
+    let case_to_string (Ecase (pat, expr)) =
+      asprintf "| %a -> %a" pp_pattern pat pp_expr expr
     in
-    match expr with
-    | Econst c -> pp_const ppf c
-    | Evar (Id (name, None)) -> fprintf ppf "%s" name
-    | Evar (Id (name, Some suffix)) -> fprintf ppf "%s.%s" name suffix
-    | Eif_then_else (e1, e2, None) -> fprintf ppf "if %a then %a" pp_expr e1 pp_expr e2
-    | Eif_then_else (e1, e2, Some e3) ->
-      fprintf ppf "if %a then %a else %a" pp_expr e1 pp_expr e2 pp_expr e3
-    | Ematch (exp, Ecase (first_pat, first_expr), rest_cases) ->
-      let case_to_string (Ecase (pat, expr)) =
-        asprintf "| %a -> %a" pp_pattern pat pp_expr expr
-      in
-      let case_list_str =
-        String.concat
-          ~sep:" "
-          (case_to_string (Ecase (first_pat, first_expr))
-           :: List.map ~f:case_to_string rest_cases)
-      in
-      fprintf ppf "match %a with %s" pp_expr exp case_list_str
-    | Eoption (Some e) -> fprintf ppf "(Some %a)" pp_expr e
-    | Eoption None -> fprintf ppf "None"
-    | Etuple (e1, e2, es) ->
-      fprintf
-        ppf
-        "(%a, %a%a)"
-        pp_expr
-        e1
-        pp_expr
-        e2
-        (fun ppf -> List.iter ~f:(fprintf ppf ", %a" pp_expr))
-        es
-    | Elist es ->
-      fprintf
-        ppf
-        "[%a]"
-        (fun ppf ->
-          List.iteri ~f:(fun i e ->
-            if i > 0 then fprintf ppf "; %a" pp_expr e else pp_expr ppf e))
-        es
-    | Efun (first_pattern, rest_patterns, e) ->
-      fprintf
-        ppf
-        "(fun %a%a -> %a)"
-        pp_pattern
-        first_pattern
-        (fun ppf patterns ->
-          List.iter patterns ~f:(fun pat -> fprintf ppf " %a" pp_pattern pat))
-        rest_patterns
-        pp_expr
-        e
-    | Ebin_op (op, e1, e2) ->
-      let op_prec = precedence_bin_op op in
-      fprintf
-        ppf
-        "%a %a %a"
-        (fun ppf e ->
-          if needs_parens op_prec (precedence e) then
-            fprintf ppf "(%a)" pp_expr e
-          else
-            pp_expr ppf e)
-        e1
-        pp_bin_op
-        op
-        (fun ppf e ->
-          if needs_parens op_prec (precedence e) then
-            fprintf ppf "(%a)" pp_expr e
-          else
-            pp_expr ppf e)
-        e2
-    | Eun_op (op, e) ->
-      fprintf ppf "%a(%a)" pp_un_op op pp_expr e
-    | Elet (rec_flag, vb, vb_l, e) ->
-      fprintf
-        ppf
-        "let %a %a in %a"
-        pp_rec_flag
-        rec_flag
-        (fun ppf () ->
-          fprintf ppf "%a" pp_value_binding vb;
-          List.iter vb_l ~f:(fun vb' -> fprintf ppf " and %a" pp_value_binding vb'))
-        ()
-        pp_expr
-        e
-    | Efun_application (e1, e2) ->
-      let needs_parens = function
-        | Econst _ | Evar _ | Ebin_op _ -> false
-        | _ -> true
-      in
-      fprintf
-        ppf
-        "%a %a"
-        (fun ppf e ->
-          if needs_parens e then fprintf ppf "(%a)" pp_expr e else pp_expr ppf e)
-        e1
-        (fun ppf e ->
-          if needs_parens e then fprintf ppf "(%a)" pp_expr e else pp_expr ppf e)
-        e2
-  
-  and precedence = function
-    | Ebin_op (op, _, _) -> precedence_bin_op op
-    | _ -> 2
-  
-  and pp_value_binding ppf = function
-    | Evalue_binding (Id (name, None), e) -> fprintf ppf "%s = %a" name pp_expr e
-    | Evalue_binding (Id (name, Some suffix), e) ->
-      fprintf ppf "%s%s = %a" name suffix pp_expr e
-  ;;
+    let case_list_str =
+      String.concat
+        ~sep:" "
+        (case_to_string (Ecase (first_pat, first_expr))
+         :: List.map ~f:case_to_string rest_cases)
+    in
+    fprintf ppf "match %a with %s" pp_expr exp case_list_str
+  | Eoption (Some e) -> fprintf ppf "(Some %a)" pp_expr e
+  | Eoption None -> fprintf ppf "None"
+  | Etuple (e1, e2, es) ->
+    fprintf
+      ppf
+      "(%a, %a%a)"
+      pp_expr
+      e1
+      pp_expr
+      e2
+      (fun ppf -> List.iter ~f:(fprintf ppf ", %a" pp_expr))
+      es
+  | Elist es ->
+    fprintf
+      ppf
+      "[%a]"
+      (fun ppf ->
+        List.iteri ~f:(fun i e ->
+          if i > 0 then fprintf ppf "; %a" pp_expr e else pp_expr ppf e))
+      es
+  | Efun (first_pattern, rest_patterns, e) ->
+    fprintf
+      ppf
+      "(fun %a%a -> %a)"
+      pp_pattern
+      first_pattern
+      (fun ppf patterns ->
+        List.iter patterns ~f:(fun pat -> fprintf ppf " %a" pp_pattern pat))
+      rest_patterns
+      pp_expr
+      e
+  | Ebin_op (op, e1, e2) ->
+    let op_prec = precedence_bin_op op in
+    fprintf
+      ppf
+      "%a %a %a"
+      (fun ppf e ->
+        if needs_parens op_prec (precedence e)
+        then fprintf ppf "(%a)" pp_expr e
+        else pp_expr ppf e)
+      e1
+      pp_bin_op
+      op
+      (fun ppf e ->
+        if needs_parens op_prec (precedence e)
+        then fprintf ppf "(%a)" pp_expr e
+        else pp_expr ppf e)
+      e2
+  | Eun_op (op, e) -> fprintf ppf "%a(%a)" pp_un_op op pp_expr e
+  | Elet (rec_flag, vb, vb_l, e) ->
+    fprintf
+      ppf
+      "let %a %a in %a"
+      pp_rec_flag
+      rec_flag
+      (fun ppf () ->
+        fprintf ppf "%a" pp_value_binding vb;
+        List.iter vb_l ~f:(fun vb' -> fprintf ppf " and %a" pp_value_binding vb'))
+      ()
+      pp_expr
+      e
+  | Efun_application (e1, e2) ->
+    let needs_parens = function
+      | Econst _ | Evar _ | Ebin_op _ -> false
+      | _ -> true
+    in
+    fprintf
+      ppf
+      "%a %a"
+      (fun ppf e ->
+        if needs_parens e then fprintf ppf "(%a)" pp_expr e else pp_expr ppf e)
+      e1
+      (fun ppf e ->
+        if needs_parens e then fprintf ppf "(%a)" pp_expr e else pp_expr ppf e)
+      e2
+
+and precedence = function
+  | Ebin_op (op, _, _) -> precedence_bin_op op
+  | _ -> 2
+
+and pp_value_binding ppf = function
+  | Evalue_binding (Id (name, None), e) -> fprintf ppf "%s = %a" name pp_expr e
+  | Evalue_binding (Id (name, Some suffix), e) ->
+    fprintf ppf "%s%s = %a" name suffix pp_expr e
+;;
 
 let pp_structure_item ppf (item : structure_item) =
   match item with
