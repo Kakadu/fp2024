8f171dc0d3f5c59c8a4b59cbfb7caa831d99ff3f
Lutsyuk Victoria <victorialutsyuk.t@gmail.com>
2024-12-01 20:36:57 +0300

    refactor: literals
    Signed-off-by: Lutsyuk Victoria <victorialutsyuk.t@gmail.com>

---
+++ ELutsyuk/lib/parser/literals.ml
@@ -0,0 +1,34 @@
+(* Copyright 2024, Victoria Lutsyuk *)
+
+(* SPDX-License-Identifier: MIT *)
+
+open Angstrom
+open Auxiliaries
+open Ast
+
+let parse_int =
+  clean_up
+  @@
+  let* sign = choice [ char '+' *> return 1; char '-' *> return (-1); return 1 ] in
+  let* digit = take_while1 is_digit >>| int_of_string in
+  return @@ Int (sign * digit)
+;;
+
+let parse_str =
+  let parse_empty_string = string "{||}" >>| fun _ -> "" in
+  let parse_content = string "{|" *> take_till (Char.equal '|') <* string "|}" in
+  let* str = parse_empty_string <|> parse_content in
+  return @@ Str str
+;;
+
+let parse_bool =
+  let* bool = choice [ token "true" *> return true; token "false" *> return false ] in
+  return @@ Bool bool
+;;
+
+let parse_unit =
+  let* _ = token "()" in
+  return Unit
+;;
+
+let parse_literal = choice [ parse_int; parse_str; parse_bool; parse_unit ]
