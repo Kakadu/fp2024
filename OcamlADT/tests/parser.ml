24307e427707b6ff0ad187159f85aa414e7e7b31
qrutyy <qrutyq@gmail.com>
2024-12-05 21:31:06 +0300

    test: add parser adt

--- OcamlADT/tests/parser.ml
+++ OcamlADT/tests/parser.ml
@@ -1046,28 +1046,31 @@ let%expect_test "keyword" =
 
 let%expect_test "adt v0" =
   test_programm {|type shape = Circle;;|};
-  [%expect{| [(Str_adt (None, "shape", (("Circle", None), [])))] |}]
+  [%expect {| [(Str_adt ([], "shape", (("Circle", []), [])))] |}]
 ;;
 
 let%expect_test "adt v1" =
   test_programm {|type shape = Circle | Square of int;;|};
-  [%expect{|
-    [(Str_adt (None, "shape",
-        (("Circle", None), [("Square", (Some (Type_var "int")))])))
+  [%expect
+    {|
+    [(Str_adt ([], "shape",
+        (("Circle", []), [("Square", [(Type_construct ("int", []))])])))
       ] |}]
 ;;
 
 let%expect_test "adt v2" =
   test_programm {|type shape = Circle | Square;;|};
-  [%expect{| [(Str_adt (None, "shape", (("Circle", None), [("Square", None)])))] |}]
+  [%expect {|
+    [(Str_adt ([], "shape", (("Circle", []), [("Square", [])])))] |}]
 ;;
 
 let%expect_test "adt v3" =
   test_programm {|type shape = Circle | Square of int * int;;|};
-  [%expect{|
-    [(Str_adt (None, "shape",
-        (("Circle", None),
-         [("Square", (Some (Type_tuple ((Type_var "int"), (Type_var "int"), []))))
+  [%expect
+    {|
+    [(Str_adt ([], "shape",
+        (("Circle", []),
+         [("Square", [(Type_construct ("int", [])); (Type_construct ("int", []))])
            ])
         ))
       ] |}]
@@ -1075,16 +1078,94 @@ let%expect_test "adt v3" =
 
 let%expect_test "adt with poly" =
   test_programm {|type 'a shape = Circle | Square of 'a;;|};
-  [%expect{|
-    [(Str_adt ((Some (Type_var "'a")), "shape",
-        (("Circle", None), [("Square", (Some (Type_var "'a")))])))
+  [%expect
+    {|
+    [(Str_adt (["a"], "shape", (("Circle", []), [("Square", [(Type_var "a")])])))
       ] |}]
 ;;
 
 let%expect_test "bad adt with poly (wrong types)" =
+  test_programm {|type 'a shape = Circle | Square of 'b;;|};
+  [%expect
+    {|
+    [(Str_adt (["a"], "shape", (("Circle", []), [("Square", [(Type_var "b")])])))
+      ] |}]
+;;
+
+let%expect_test "adt with poly (not poly in variant)" =
   test_programm {|type 'a shape = Circle | Square of int;;|};
-  [%expect{|
-    [(Str_adt ((Some (Type_var "'a")), "shape",
-        (("Circle", None), [("Square", (Some (Type_var "int")))])))
+  [%expect
+    {|
+    [(Str_adt (["a"], "shape",
+        (("Circle", []), [("Square", [(Type_construct ("int", []))])])))
+      ] |}]
+;;
+
+let%expect_test "adt with poly v.easy" =
+  test_programm {|type 'a shape = Circle;;|};
+  [%expect {| [(Str_adt (["a"], "shape", (("Circle", []), [])))] |}]
+;;
+
+let%expect_test "adt with multiple poly v1" =
+  test_programm {|type ('a, 'b) shape = Circle | Square of 'a;;|};
+  [%expect
+    {|
+    [(Str_adt (["a"; "b"], "shape",
+        (("Circle", []), [("Square", [(Type_var "a")])])))
+      ] |}]
+;;
+
+(*i guess should be tuple*)
+
+let%expect_test "adt with multiple poly v2" =
+  test_programm {|type ('a, 'b) shape = Circle | Square of ('a,'b) shape;;|};
+  [%expect
+    {|
+    [(Str_adt (["a"; "b"], "shape",
+        (("Circle", []),
+         [("Square",
+           [(Type_construct ("shape", [(Type_var "a"); (Type_var "b")]))])])
+        ))
+      ] |}]
+;;
+
+let%expect_test "adt with poly (con poly variant)" =
+  test_programm {|type 'a shape = Circle | Square of (int * int) shape;;|};
+  [%expect.unreachable]
+[@@expect.uncaught_exn
+  {|
+  (* CR expect_test_collector: This test expectation appears to contain a backtrace.
+     This is strongly discouraged as backtraces are fragile.
+     Please change this test to not include a backtrace. *)
+
+  (Failure ": end_of_input")
+  Raised at Stdlib.failwith in file "stdlib.ml", line 29, characters 17-33
+  Called from Ocamladt_tests__Parser.test_programm in file "tests/parser.ml", line 9, characters 52-67
+  Called from Ocamladt_tests__Parser.(fun) in file "tests/parser.ml", line 1132, characters 2-74
+  Called from Expect_test_collector.Make.Instance_io.exec in file "collector/expect_test_collector.ml", line 234, characters 12-19 |}]
+;;
+
+let%expect_test "adt with tuple in variant" =
+  test_programm {|type shape = Circle | Square of int * int;;|};
+  [%expect
+    {|
+    [(Str_adt ([], "shape",
+        (("Circle", []),
+         [("Square", [(Type_construct ("int", [])); (Type_construct ("int", []))])
+           ])
+        ))
+      ] |}]
+;;
+
+(*mb here should be a tuple*)
+
+let%expect_test "adt with recursive poly variant" =
+  test_programm {|type ('a, 'b) shape = Circle | Square of ('a) shape;;|};
+  [%expect
+    {|
+    [(Str_adt (["a"; "b"], "shape",
+        (("Circle", []),
+         [("Square", [(Type_construct ("shape", [(Type_var "a")]))])])
+        ))
       ] |}]
 ;;
