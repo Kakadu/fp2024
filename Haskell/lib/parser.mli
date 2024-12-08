407f4fe50ecdab0190bd31d943be5dc4bddb91a7
Kostya <koreshin67@gmail.com>
2024-12-09 01:39:16 +0300

    ref: change pprint and carry out function for parsing expr

--- Haskell/lib/parser.mli
+++ Haskell/lib/parser.mli
@@ -4,3 +4,4 @@
 
 val parse_line : string -> (Ast.binding list, string) Result.t
 val parse_and_print_line : string -> unit
+val parse_expr : string -> (Ast.expr, string) Result.t
--- Haskell/lib/pprint.ml
+++ Haskell/lib/pprint.ml
@@ -5,7 +5,7 @@
 open Format
 open Typedtree
 
-let rec pp_ty fmt ty =
+let pp_ty =
   let rec helper fmt = function
     | Ty_maybe ty -> fprintf fmt "Maybe %a" helper ty
     | Ty_prim s -> pp_print_string fmt s
@@ -15,17 +15,25 @@ let rec pp_ty fmt ty =
        | Ty_arrow (_, _) -> fprintf fmt "(%a) -> %a" helper ty1 helper ty2
        | _ -> fprintf fmt "%a -> %a" helper ty1 helper ty2)
     | Ty_list ty -> fprintf fmt "[%a]" helper ty
-    | Ty_tuple ty_list ->
-      fprintf fmt "(";
+    | Ty_tuple (ty1, ty2, ty_list) ->
       fprintf
         fmt
-        "%a"
-        (pp_print_list
-           ~pp_sep:(fun fmt _ -> fprintf fmt ", ")
-           (fun fmt ty -> fprintf fmt "%a" helper ty))
-        ty_list;
-      fprintf fmt ")"
+        "(%a, %a%a)"
+        helper
+        ty1
+        helper
+        ty2
+        (pp_print_list (fun fmt ty -> fprintf fmt ", %a" helper ty))
+        ty_list
     | Ty_tree ty -> fprintf fmt "{%a}" helper ty
+    | Ty_ord ty -> fprintf fmt "Ord t%d" ty
   in
-  helper fmt ty
+  helper
+;;
+
+let pp_error ppf : error -> _ = function
+  | `Occurs_check -> Format.fprintf ppf "Occurs check failed"
+  | `No_variable s -> Format.fprintf ppf "Undefined variable '%s'" s
+  | `Unification_failed (l, r) ->
+    Format.fprintf ppf "unification failed on %a and %a" pp_ty l pp_ty r
 ;;
