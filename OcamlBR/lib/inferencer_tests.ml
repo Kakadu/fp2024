e29476b9055a60b1d627464d25f348dd1883aae1
sofyak0zyreva <k81sofia@gmail.com>
2024-12-08 20:39:49 +0300

    feat: add inferencer tests (failing for now)
    Signed-off-by: sofyak0zyreva <k81sofia@gmail.com>

--- OcamlBR/lib/inferencer_tests.ml
+++ OcamlBR/lib/inferencer_tests.ml
@@ -1,1 +1,19 @@
-(*тестики наши тестики*)
+(** Copyright 2024, Sofya Kozyreva, Maksim Shipilov *)
+
+(** SPDX-License-Identifier: LGPL-3.0-or-later *)
+let infer_program_test str =
+  let parsed = Result.get_ok (Parser.parse_expr str) in
+  match Inferencer.Infer.infer_program parsed with
+  | Ok env -> Format.printf "%a" Inferencer.TypeEnv.pp env
+  | Error err -> Format.printf "%a" Typedtree.pp_error err
+;;
+
+let%expect_test _ =
+  let _ = infer_program_test {|let f x g = g x in f|} in
+  [%expect {| 'a -> ('a -> 'b) -> 'b |}]
+;;
+
+let%expect_test _ =
+  let _ = infer_program_test {|let x = 2 in x - 9 |} in
+  [%expect {|  |}]
+;;
