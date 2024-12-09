26e76a409be371e9814a31909a9c57c79b9fa5dd
Ksenia Kotelnikova <xeniia.ka@gmail.com>
2024-12-09 19:34:54 +0300

    ref: formatting
    Signed-off-by: Ksenia Kotelnikova <xeniia.ka@gmail.com>

--- FSharpActivePatterns/lib/typedTree.mli
+++ FSharpActivePatterns/lib/typedTree.mli
@@ -1,3 +1,6 @@
+(** Copyright 2024-2025, Ksenia Kotelnikova <xeniia.ka@gmail.com>, Gleb Nasretdinov <gleb.nasretdinov@proton.me> *)
+
+(** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
 type binder = int
 
@@ -9,17 +12,18 @@ type typ =
   | Type_tuple of typ * typ * typ list
   | TOption of typ
 
-val arrow_of_types: typ list -> typ -> typ
+val arrow_of_types : typ list -> typ -> typ
 
 module VarSet : sig
-    include module type of Stdlib.Set.Make (Int)
-  
-    val pp: Format.formatter -> t -> unit
+  include module type of Stdlib.Set.Make (Int)
+
+  val pp : Format.formatter -> t -> unit
 end
 
 type binder_set = VarSet.t
 type scheme = S of binder_set * typ [@@deriving show { with_path = false }]
-val int_typ: typ
-val bool_typ: typ
-val string_typ: typ
-val unit_typ: typ
+
+val int_typ : typ
+val bool_typ : typ
+val string_typ : typ
+val unit_typ : typ
