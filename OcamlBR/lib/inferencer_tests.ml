6dc5120632657dbb104f1664411174ddbdbbba23
mshipilov5 <shipilovmaksim05@gmail.com>
2024-12-09 23:40:08 +0300

    fix: comments
    Signed-off-by: mshipilov5 <shipilovmaksim05@gmail.com>

--- OcamlBR/lib/inferencer_tests.ml
+++ OcamlBR/lib/inferencer_tests.ml
@@ -1,16 +1,13 @@
 (** Copyright 2024, Sofya Kozyreva, Maksim Shipilov *)
 
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
-let infer_program_test str =
-  let parsed = Result.get_ok (Parser.parse_expr str) in
-  match Inferencer.Infer.infer_program parsed with
-  | Ok env -> 
-      Format.printf "%a\n" Inferencer.TypeEnv.pp env;
-      Stdlib.print_endline (Ast.show_structure parsed)  (* Передаем уже распарсенный AST *)
-  | Error err -> 
-      Format.printf "Error: %a\n" Typedtree.pp_error err;
-      Stdlib.print_endline (Ast.show_structure parsed) 
-  ;;
+(* let infer_program_test str =
+   let parsed = Result.get_ok (Parser.parse_expr str) in
+   match infer_program parsed with
+   | Ok env -> Format.printf "%a" Inferencer.TypeEnv.pp env
+   | Error err -> Format.printf "%a" Typedtree.pp_error err
+   ;; *)
+   
 let%expect_test _ =
   let _ = infer_program_test {|let f x g = g x in f|} in
   [%expect {| 'a -> ('a -> 'b) -> 'b |}]
