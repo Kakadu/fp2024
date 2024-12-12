089f21a3c5ecebeaa52e9c90d1ab4300a4294fc3
Nikita Shchutskii <nshchutskiy@gmail.com>
2024-12-13 00:32:57 +0300

    tests: add manytests directory

---
+++ Haskell/tests/manytests/typed/016lists.hs
@@ -0,0 +1,15 @@
+length xs = case xs of [] -> 0; h:tl -> 1 + length tl
+
+length_tail = let helper acc xs = case xs of [] -> acc; h:tl -> helper (acc + 1) tl in helper 0
+
+map f xs = case xs of [] -> []; a:[] -> [f a]; a:b:[] -> [f a, f b]; a:b:c:[] -> [f a, f b, f c]; a:b:c:d:tl -> f a : f b : f c : f d : map f tl
+
+append xs ys = case xs of [] -> ys; x:xs -> x:(append xs ys)
+
+concat = let helper xs = case xs of [] -> []; h:tl -> append h (helper tl) in helper
+
+iter f xs = case xs of [] -> (); h:tl -> let () = f h in iter f tl
+
+cartesian xs ys = case xs of [] -> []; h:tl -> append (map (\a -> (h,a)) ys) (cartesian tl ys)
+
+main = let () = iter print_int [1,2,3] in let () = print_int (length (cartesian [1,2] [1,2,3,4])) in 0
