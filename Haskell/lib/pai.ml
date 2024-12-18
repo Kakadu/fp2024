fdbb955ce864e4ee2b3394ca85edcf3b756dbe90
Kostya <koreshin67@gmail.com>
2024-12-19 00:47:16 +0300

    feat: add line-by-line handler in repl

--- Haskell/lib/pai.ml
+++ Haskell/lib/pai.ml
@@ -5,19 +5,32 @@
 let rec parse_and_infer text print env st =
   match text with
   | [] ->
-    if env != Inferencer.typeenv_empty
-    then
-      if env != Inferencer.typeenv_print_int
-      then Format.printf "%a\n" Inferencer.pp_typeenv env
+    if env != Inferencer.typeenv_empty && env != Inferencer.typeenv_print_int
+    then Format.printf "%a\n%!" Inferencer.pp_typeenv env
   | "" :: rest -> parse_and_infer rest print env st
   | line :: rest ->
+    if print then Parser.parse_and_print_line line;
     (match Parser.parse_line line with
      | Result.Ok list ->
        (match Inferencer.w list env st with
         | st, Result.Ok env -> parse_and_infer rest print env st
         | st, Result.Error err ->
-          Format.printf "%a\n" Pprint.pp_error err;
-          parse_and_infer rest print env st);
-       if print then Parser.parse_and_print_line line
-     | Result.Error error -> Format.printf "%s\n" error)
+          Format.printf "%a\n%!" Pprint.pp_error err;
+          parse_and_infer rest print env st)
+     | Result.Error error -> Format.printf "%s\n%!" error)
+;;
+
+let parse_and_infer_line line env st =
+  match Parser.parse_line line with
+  | Result.Ok list ->
+    (match Inferencer.w list env st with
+     | st, Result.Ok env ->
+       Format.printf "%a\n%!" Inferencer.pp_typeenv env;
+       env, st
+     | st, Result.Error err ->
+       Format.printf "%a\n%!" Pprint.pp_error err;
+       env, st)
+  | Result.Error error ->
+    Format.printf "%s\n%!" error;
+    env, st
 ;;
