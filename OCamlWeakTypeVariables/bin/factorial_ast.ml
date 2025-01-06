b28f547ebcb10d6b1bef45a6090d4aa693f6c74a
Ilhom Kombaev <kombaev02@gmail.com>
2025-01-06 12:36:58 +0300

    Redesign AST (OCaml + weak type variables: Damir Yunusov, Ilhom Kombaev) (#65)
    * Project: Initial commit
    
    * Fix: fix cram tests
    
    * Refactor: rename folder and test's name
    
    * Docs: add comments to ast types
    
    * Project: add licences source files
    
    * Refactor: add new line to vscode workspace file
    
    * Update OCamlWeakTypeVariables/lib/ast.ml
    
    Co-authored-by: Andrei <therain.i@yahoo.com>
    
    * Refactor: If then else statement now epression
    
    Signed-off-by: Homka122 <kombaev02@gmail.com>
    
    * Fix: spell mistake in lastname of Damir
    
    Signed-off-by: Homka122 <kombaev02@gmail.com>
    
    * Feat: redesign AST
    
    * Feat: add angstorm lib to dune
    
    Signed-off-by: Homka122 <kombaev02@gmail.com>
    
    * Feat: add tuple expression to ast
    
    Signed-off-by: Homka122 <kombaev02@gmail.com>
    
    * Feat: redesign expression and statement ast types
    
    Signed-off-by: Homka122 <kombaev02@gmail.com>
    
    * Test: remake factorial AST test
    
    Signed-off-by: Homka122 <kombaev02@gmail.com>
    
    * Feat: remake structure value
    
    Signed-off-by: Homka122 <kombaev02@gmail.com>
    
    * Fix: delete unused libraries and prepocess tools
    
    Signed-off-by: Homka122 <kombaev02@gmail.com>
    
    ---------
    
    Signed-off-by: Homka122 <kombaev02@gmail.com>
    Co-authored-by: Dmitrii Kosarev <Kakadu@pm.me>
    Co-authored-by: Andrei <therain.i@yahoo.com>

--- OCamlWeakTypeVariables/bin/factorial_ast.ml
+++ OCamlWeakTypeVariables/bin/factorial_ast.ml
@@ -1,29 +1,43 @@
 [@@@ocaml.text "/*"]
 
-(** Copyright 2024, Damir Yunosov and Ilhom Kombaev *)
+(** Copyright 2024, Damir Yunusov and Ilhom Kombaev *)
 
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
 [@@@ocaml.text "/*"]
 
 (* let rec factorial n = if n = 0 then 1 else n * factorial (n - 1) *)
+
 open Lib.Ast
 
 let () =
-  let fact : expr =
-    Binding
-      ( "factorial"
-      , Recursive
-      , [ "n" ]
-      , Op_tern
-          ( IfThenElse
-          , Op_bin (Equal, Variable "n", Const (Int 0))
-          , Const (Int 1)
-          , Op_bin
-              ( Mul
-              , Variable "n"
-              , Function ("factorial", [ Op_bin (Minus, Variable "n", Const (Int 1)) ]) )
-          ) )
+  let fact : structure_item =
+    Pstr_value
+      ( Recursive
+      , [ { pvb_pat = Ppat_var "factorial"
+          ; pvb_expr =
+              Pexp_fun
+                ( Ppat_var "n"
+                , Pexp_ifthenelse
+                    ( Pexp_apply
+                        ( Pexp_ident (Id "=")
+                        , [ Pexp_ident (Id "n"); Pexp_constant (Pconst_int 5) ] )
+                    , Pexp_constant (Pconst_int 1)
+                    , Some
+                        (Pexp_apply
+                           ( Pexp_ident (Id "*")
+                           , [ Pexp_ident (Id "n")
+                             ; Pexp_apply
+                                 ( Pexp_ident (Id "factorial")
+                                 , [ Pexp_apply
+                                       ( Pexp_ident (Id "-")
+                                       , [ Pexp_ident (Id "n")
+                                         ; Pexp_constant (Pconst_int 1)
+                                         ] )
+                                   ] )
+                             ] )) ) )
+          }
+        ] )
   in
-  print_endline (show_expr fact)
+  print_endline (show_structure_item fact)
 ;;
--- OCamlWeakTypeVariables/lib/ast.ml
+++ OCamlWeakTypeVariables/lib/ast.ml
@@ -1,57 +1,61 @@
 [@@@ocaml.text "/*"]
 
-(** Copyright 2024, Damir Yunosov and Ilhom Kombaev *)
+(** Copyright 2024, Damir Yunusov and Ilhom Kombaev *)
 
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
 [@@@ocaml.text "/*"]
 
-(** literals *)
-type literal =
-  | Int of int (** literal of int. Ex: 5 *)
-  | String of string (** literal of string. Ex: "homka" *)
-  | Boolean of bool (** literal of boolean. Ex: true *)
+(** constants *)
+type constant =
+  | Pconst_int of int (** constant of int. Ex: 5 *)
+  | Pconst_string of string (** constant of string. Ex: "homka" *)
+  | Pconst_boolean of bool (** constant of boolean. Ex: true *)
+  | Pconst_float of float (** constant of float. Ex: 1.22 *)
 [@@deriving show { with_path = false }]
 
-(** indetificator *)
-type id = string [@@deriving show { with_path = false }]
+(** identificator *)
+type id = Id of string [@@deriving show { with_path = false }]
 
-(** binary operators *)
-type op_bin =
-  | Plus (** [ + ] *)
-  | Minus (** [ - ] *)
-  | Mul (** [ * ] *)
-  | Div (** [ / ] *)
-  | Equal (** [ = ] *)
-  | Greater (** [ > ] *)
-  | GreaterEqual (** [ >= ] *)
-  | Less (** [ < ] *)
-  | LessEqual (** [ <= ] *)
-  | And (** [ && ] *)
-  | Or (** [ || ] *)
+type pattern =
+  | Ppat_any (** The pattern _. *)
+  | Ppat_var of string (** A variable pattern such as x *)
+  | Ppat_constant of constant (** Patterns such as 1, 'a', "true", 1.0 *)
+  | Ppat_interval of constant * constant (** Patterns such as 'a'..'z'. *)
+  | Ppat_tuple of pattern list (** Patterns (P1, ..., Pn). Invariant: n >= 2 *)
 [@@deriving show { with_path = false }]
-
-(** unary operators *)
-type op_un =
-  | Not (** [ not ] *)
-  | UnMinus (** unary minus. Ex: '-' in -4 *)
-[@@deriving show { with_path = false }]
-
-(** ternary operators *)
-type op_tern = IfThenElse [@@deriving show { with_path = false }]
 
 (** recursive flag *)
 type rec_flag =
   | Recursive
-  | NotRecursive
+  | NonRecursive
 [@@deriving show { with_path = false }]
 
-type expr =
-  | Const of literal (** const of literal. Ex: 5, "Homka", true *)
-  | Variable of id (** variable with name. Ex: "homka" *)
-  | Op_un of op_un * expr (** Unary operation. Ex: not true *)
-  | Op_bin of op_bin * expr * expr (** Binary operation. Ex: 5 + 5 *)
-  | Op_tern of op_tern * expr * expr * expr (** Ternary operation. Ex: If then else*)
-  | Function of id * expr list (** Function: Ex: print a (5 + 5) b true *)
-  | Binding of id * rec_flag * id list * expr (** Binding. Ex: let homka a = a + a *)
+type expression =
+  | Pexp_ident of id (** Identifiers. Ex: "homka" *)
+  | Pexp_constant of constant (** Expressions constant. Ex: 5, "Homka", true *)
+  | Pexp_let of rec_flag * value_binding list * expression
+  (** Pexp_let(flag, [(P1,E1) ; ... ; (Pn,En)], E) represents:
+      let P1 = E1 and ... and Pn = EN in E when flag is Nonrecursive
+      let rec P1 = E1 and ... and Pn = EN in E when flag is Recursive *)
+  | Pexp_fun of pattern * expression (** fun P -> E *)
+  | Pexp_apply of expression * expression list (** Function: Ex: print a (5 + 5) b true *)
+  | Pexp_tuple of expression list (** Expressions (E1, ..., En). Invariant: n >= 2 *)
+  | Pexp_ifthenelse of expression * expression * expression option
+  (** if E1 then E2 else E3. Ex: If homka then hype else no_hype *)
+[@@deriving show { with_path = false }]
+
+(* let pat : type_constraint = exp *)
+and value_binding =
+  { pvb_pat : pattern
+  ; pvb_expr : expression
+  }
+[@@deriving show { with_path = false }]
+
+type structure_item =
+  | Pstr_eval of expression
+  | Pstr_value of rec_flag * value_binding list
+  (** Pstr_value(rec, [(P1, E1 ; ... ; (Pn, En))]) represents:
+      et P1 = E1 and ... and Pn = EN when rec is Nonrecursive,
+      let rec P1 = E1 and ... and Pn = EN  when rec is Recursive.*)
 [@@deriving show { with_path = false }]
