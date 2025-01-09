85d121f9b3bb2f24aa44e1eae93b53327f5404d5
Homka122 <kombaev02@gmail.com>
2025-01-10 00:34:47 +0300

    Proj: add License to parser.mli
    Signed-off-by: Homka122 <kombaev02@gmail.com>

--- OCamlWeakTypeVariables/lib/parser.mli
+++ OCamlWeakTypeVariables/lib/parser.mli
@@ -1,3 +1,11 @@
+[@@@ocaml.text "/*"]
+
+(** Copyright 2024-2025, Damir Yunusov and Ilhom Kombaev *)
+
+(** SPDX-License-Identifier: LGPL-3.0-or-later *)
+
+[@@@ocaml.text "/*"]
+
 open Ast
 
 val parse : string -> (structure_item, string) result
