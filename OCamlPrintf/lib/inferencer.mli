522ff86caed16ff6fbd8300ac2f39209a2695ed5
Maxim Rodionov <rodionov.maxim05@gmail.com>
2025-01-12 21:26:00 +0300

    docs: type 'error'
    Signed-off-by: Maxim Rodionov <rodionov.maxim05@gmail.com>

--- OCamlPrintf/lib/inferencer.mli
+++ OCamlPrintf/lib/inferencer.mli
@@ -4,11 +4,23 @@
 
 type error =
   [ `No_variable_rec
+    (** Represents an error where a recursive variable is not allowed because that would lead to infinite recursion.
+        E.g. [let rec x = x + 1] *)
   | `No_arg_rec
+    (** Represents an error where the left-hand side of the recursive binding is not a var.
+        E.g. [let rec [ a; b ] = ..] *)
   | `Bound_several_times of string
+    (** Represents an error where a pattern bound the variable multiple times.
+        E.g. [let x, x = ..] *)
   | `Occurs_check of string * Ast.core_type
+    (** Represents an occurs check failure.
+        This occurs when attempting to unify types, and one type is found to occur within another in a way that violates the rules of type systems.
+        E.g. [let rec f x = f] *)
   | `No_variable of string
+    (** Represents an error indicating that a variable could not be found in the current scope. *)
   | `Unification_failed of Ast.core_type * Ast.core_type
+    (** Represents that type unification has failed.
+        This occurs when two types cannot made equivalent during type inference. *)
   ]
 
 val pp_error : Format.formatter -> error -> unit
