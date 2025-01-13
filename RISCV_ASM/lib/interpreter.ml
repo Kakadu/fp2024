75b3d1e7eb3d356bc519bb734b3bd483ee5a8565
Roman Mukovenkov <roman.mukovenkov@gmail.com>
2025-01-13 06:18:14 +0300

    feat: add support of int64, draft revamp of existing jump logic

--- RISCV_ASM/lib/interpreter.ml
+++ RISCV_ASM/lib/interpreter.ml
@@ -3,11 +3,12 @@
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
 module StringMap = Map.Make (String)
+module Int64Map = Map.Make (Int64)
 open Ast
 
 type state =
-  { registers : int StringMap.t
-  ; pc : int
+  { registers : int64 StringMap.t
+  ; pc : int64
   }
 
 let handle_alternative_names = function
@@ -49,7 +50,7 @@ let handle_alternative_names = function
 let init_state () =
   let registers =
     List.fold_left
-      (fun acc reg -> StringMap.add (show_register reg) 0 acc)
+      (fun acc reg -> StringMap.add (show_register reg) 0L acc)
       StringMap.empty
       [ X0
       ; X1
@@ -85,12 +86,12 @@ let init_state () =
       ; X31
       ]
   in
-  { registers; pc = 0 }
+  { registers; pc = 0L }
 ;;
 
 let get_register_value state reg =
   StringMap.find_opt (show_register (handle_alternative_names reg)) state.registers
-  |> Option.value ~default:0
+  |> Option.value ~default:0L
 ;;
 
 let set_register_value state reg value =
@@ -100,10 +101,20 @@ let set_register_value state reg value =
   }
 ;;
 
-let set_pc state new_pc = { state with pc = new_pc }
-let increment_pc state = { state with pc = state.pc + 1 }
+let nth_opt_int64 l n =
+  if n < 0L then
+    failwith "Index cannot be negative"
+  else
+    let rec nth_aux l n =
+      match l with
+      | [] -> None
+      | a::l -> if n = 0L then Some a else nth_aux l (Int64.sub n 1L)
+    in nth_aux l n
 
-let resolve_label program label =
+let set_pc state new_pc = { state with pc = new_pc }
+let increment_pc state = { state with pc = Int64.add state.pc 1L }
+
+let resolve_label_including_directives program label =
   let rec aux idx = function
     | [] -> -1
     | LabelExpr lbl :: _ when lbl = label -> idx
@@ -112,23 +123,67 @@ let resolve_label program label =
   aux 0 program
 ;;
 
+let resolve_label_excluding_directives program label =
+  let rec aux idx = function
+    | [] -> -1
+    | LabelExpr lbl :: _ when lbl = label -> idx
+    | InstructionExpr _ :: tl -> aux (idx + 1) tl
+    | _ :: tl -> aux idx tl
+  in
+  aux 0 program
+;;
+
+type address_info =
+  | Immediate of int
+  | Label of int
+
 let get_address12_value program = function
-  | ImmediateAddress12 value -> value
-  | LabelAddress12 label -> resolve_label program label
+  | ImmediateAddress12 value -> Immediate (value)
+  | LabelAddress12 label -> Label (resolve_label_excluding_directives program label)
 ;;
 
 let get_address20_value program = function
-  | ImmediateAddress20 value -> value
-  | LabelAddress20 label -> resolve_label program label
+  | ImmediateAddress20 value -> Immediate (value)
+  | LabelAddress20 label -> Label (resolve_label_excluding_directives program label)
 ;;
 
 let get_address32_value program = function
-  | ImmediateAddress32 value -> value
-  | LabelAddress32 label -> resolve_label program label
+  | ImmediateAddress32 value -> Immediate (value)
+  | LabelAddress32 label -> Label (resolve_label_excluding_directives program label)
+;;
+
+(* Get index from including directives and labels to excluding *)
+let resolve_address_incl_to_excl program immediate64_value =
+  let rec traverse_program index remaining_value = function
+    | [] -> failwith "End of program reached before resolving immediate address from including directives to excluding"
+    | DirectiveExpr _ :: rest | LabelExpr _ :: rest -> 
+        traverse_program index (Int64.sub remaining_value 1L) rest
+    | InstructionExpr _ :: rest -> 
+        if remaining_value = 1L then
+          (Int64.add index 1L)
+        else
+          traverse_program (Int64.add index 1L) (Int64.sub remaining_value 1L) rest
+  in
+  traverse_program 0L immediate64_value program
+;;
+
+(* Get index from excluding directives and labels to including *)
+let resolve_address_excl_to_incl program immediate64_value =
+  let rec traverse_program index remaining_value = function
+    | [] -> failwith "End of program reached before resolving immediate address from excluding directives to including"
+    | DirectiveExpr _ :: rest | LabelExpr _ :: rest -> 
+        traverse_program (Int64.add index 1L) remaining_value rest
+    | InstructionExpr _ :: rest -> 
+        if remaining_value = 1L then
+          (Int64.add index 1L)
+        else
+          traverse_program (Int64.add index 1L) (Int64.sub remaining_value 1L) rest
+  in
+  traverse_program 0L immediate64_value program
 ;;
 
 let rec interpret state program =
-  match List.nth_opt program state.pc with
+  match nth_opt_int64 program state.pc with
   | None -> state
   | Some (InstructionExpr instr) ->
     let new_state = execute_instruction state instr program in
@@ -141,77 +196,92 @@ and execute_instruction state instr program =
   | Add (rd, rs1, rs2) ->
     let val1 = get_register_value state rs1 in
     let val2 = get_register_value state rs2 in
-    let result = val1 + val2 in
+    let result = Int64.add val1 val2 in
     set_register_value state rd result
   | Sub (rd, rs1, rs2) ->
     let val1 = get_register_value state rs1 in
     let val2 = get_register_value state rs2 in
-    let result = val1 - val2 in
+    let result = Int64.sub val1 val2 in
     set_register_value state rd result
   | Xor (rd, rs1, rs2) ->
     let val1 = get_register_value state rs1 in
     let val2 = get_register_value state rs2 in
-    let result = val1 lxor val2 in
+    let result = Int64.logxor val1 val2 in
     set_register_value state rd result
   | Or (rd, rs1, rs2) ->
     let val1 = get_register_value state rs1 in
     let val2 = get_register_value state rs2 in
-    let result = val1 lor val2 in
+    let result = Int64.logor val1 val2 in
     set_register_value state rd result
   | And (rd, rs1, rs2) ->
     let val1 = get_register_value state rs1 in
     let val2 = get_register_value state rs2 in
-    let result = val1 land val2 in
+    let result = Int64.logand val1 val2 in
     set_register_value state rd result
   | Sll (rd, rs1, rs2) ->
     let val1 = get_register_value state rs1 in
     let val2 = get_register_value state rs2 in
-    let result = val1 lsl val2 in
+    let result = Int64.shift_left val1 (Int64.to_int val2) in
     set_register_value state rd result
   | Srl (rd, rs1, rs2) ->
     let val1 = get_register_value state rs1 in
     let val2 = get_register_value state rs2 in
-    let result = Int.shift_right_logical val1 val2 in
+    let result = Int64.shift_right_logical val1 (Int64.to_int val2) in
     set_register_value state rd result
   | Sra (rd, rs1, rs2) ->
     let val1 = get_register_value state rs1 in
     let val2 = get_register_value state rs2 in
-    let result = Int.shift_right val1 val2 in
+    let result = Int64.shift_right val1 (Int64.to_int val2) in
     set_register_value state rd result
   | Slt (rd, rs1, rs2) ->
     let val1 = get_register_value state rs1 in
     let val2 = get_register_value state rs2 in
-    let result = if val1 < val2 then 1 else 0 in
+    let result = if Int64.compare val1 val2 < 0 then 1L else 0L in
+    set_register_value state rd result
+  | Sltu (rd, rs1, rs2) ->
+    let val1 = get_register_value state rs1 in
+    let val2 = get_register_value state rs2 in
+    let result = if Int64.unsigned_compare val1 val2 < 0 then 1L else 0L in
     set_register_value state rd result
   | Jalr (rd, rs1, imm) ->
     let val_rs1 = get_register_value state rs1 in
-    let imm_value = get_address12_value program imm in
-    let new_pc = val_rs1 + imm_value in
-    let new_state = set_register_value state rd (state.pc + 1) in
+    let address_info = get_address12_value program imm in
+    let new_pc =       
+      match address_info with
+        | Immediate imm_value ->
+          resolve_address_excl_to_incl program (Int64.shift_right_logical (Int64.add val_rs1 (Int64.of_int imm_value)) 2)
+        | Label excluding_directives_label_offset ->
+          resolve_address_excl_to_incl program (Int64.add (Int64.of_int excluding_directives_label_offset) (Int64.shift_right_logical val_rs1 2))
+    in
+    let new_state = set_register_value state rd (Int64.add state.pc 1L) in
     set_pc new_state new_pc
   | Jr rs1 ->
     let val_rs1 = get_register_value state rs1 in
-    let new_state = set_register_value state X0 (state.pc + 1) in
-    set_pc new_state val_rs1
-  | J imm ->
-    let imm_value = get_address20_value program imm in
-    let label_idx = resolve_label program (string_of_int imm_value) in
-    if label_idx = -1
-    then state
-    else (
-      let new_state = set_register_value state X0 (state.pc + 1) in
-      set_pc new_state label_idx)
+    let new_pc = resolve_address_excl_to_incl program (Int64.shift_right_logical val_rs1 2) in
+    let new_state = set_register_value state X0 (Int64.add state.pc 1L) in
+    set_pc new_state new_pc
+  | J imm_value ->
+    let address_info = get_address20_value program imm_value in
+    let new_pc = 
+      match address_info with
+      | Immediate imm_value ->
+        resolve_address_excl_to_incl program (Int64.add (Int64.of_int (imm_value lsr 2)) (resolve_address_incl_to_excl program state.pc))
+      | Label excluding_directives_label_offset ->
+        resolve_address_excl_to_incl program (Int64.add (Int64.of_int excluding_directives_label_offset) (resolve_address_incl_to_excl program state.pc))
+    in
+    let new_state = set_register_value state X0 (Int64.add state.pc 1L) in
+    set_pc new_state new_pc
   | _ -> state
 ;;
 
 let show_state state =
   let registers_str =
     StringMap.fold
-      (fun reg value acc -> acc ^ Printf.sprintf "%s: %d\n" reg value)
+      (fun reg value acc -> acc ^ Printf.sprintf "%s: %Ld\n" reg value)
       state.registers
       ""
   in
-  let pc_str = Printf.sprintf "PC: %d" state.pc in
+  let pc_str = Printf.sprintf "PC: %Ld" state.pc in
   registers_str ^ pc_str
 ;;
 
