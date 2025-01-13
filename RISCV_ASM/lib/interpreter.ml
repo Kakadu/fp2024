01638d5627c002586ece535b3b535b8b38f696c3
Roman Mukovenkov <roman.mukovenkov@gmail.com>
2025-01-13 08:03:18 +0300

    feat: better support for branching, arithmetic, jump instructions. New missing branch instructions in ast

--- RISCV_ASM/lib/interpreter.ml
+++ RISCV_ASM/lib/interpreter.ml
@@ -182,6 +182,70 @@ let resolve_address_excl_to_incl program immediate64_value =
   traverse_program 0L immediate64_value program
 ;;
 
+let handle_branch_condition state program rs1 rs2 imm_value comparison_fn =
+  let val_rs1 = get_register_value state rs1 in
+  let val_rs2 = 
+    match rs2 with
+    | Some rs -> get_register_value state rs
+    | None -> 0L
+  in
+  let address_info = get_address12_value program imm_value in
+  let new_pc =
+  match address_info with
+    | Immediate imm_value ->
+      if comparison_fn val_rs1 val_rs2 then
+        resolve_address_excl_to_incl program (Int64.add (Int64.of_int (imm_value lsr 2)) (resolve_address_incl_to_excl program state.pc))
+      else
+        Int64.add state.pc 1L
+    | Label excluding_directives_label_offset ->
+      if comparison_fn val_rs1 val_rs2 then
+        resolve_address_excl_to_incl program (Int64.add (Int64.of_int excluding_directives_label_offset) (resolve_address_incl_to_excl program state.pc))
+      else
+        Int64.add state.pc 1L
+  in
+  set_pc state new_pc
+;;
+
+let execute_arithmetic_op state rd rs1 rs2 op =
+  let val1 = get_register_value state rs1 in
+  let val2 = get_register_value state rs2 in
+  let result = op val1 val2 in
+  set_register_value state rd result
+
+let execute_shift_op state rd rs1 rs2 op =
+  let val1 = get_register_value state rs1 in
+  let val2 = get_register_value state rs2 in
+  let result = op val1 (Int64.to_int val2) in
+  set_register_value state rd result
+
+let execute_comparison_op state rd rs1 rs2 compare_fn =
+  let val1 = get_register_value state rs1 in
+  let val2 = get_register_value state rs2 in
+  let result = if compare_fn val1 val2 then 1L else 0L in
+  set_register_value state rd result
+
+let execute_immediate_op state program rd rs1 imm op =
+  let val1 = get_register_value state rs1 in
+  let address_info = get_address12_value program imm in
+  let imm_value =
+    match address_info with
+    | Immediate imm_value -> Int64.of_int imm_value
+    | Label excluding_directives_label_offset ->
+      Int64.shift_left (Int64.of_int excluding_directives_label_offset) 2
+  in
+  let result = op val1 imm_value in
+  set_register_value state rd result
+
+let execute_shift_immediate_op state program rd rs1 imm op =
+  let val1 = get_register_value state rs1 in
+  let imm_value = match get_address12_value program imm with
+    | Immediate imm_value -> imm_value
+    | Label excluding_directives_label_offset ->
+      excluding_directives_label_offset lsl 2
+  in
+  let result = op val1 imm_value in
+  set_register_value state rd result
+
 let rec interpret state program =
   match nth_opt_int64 program state.pc with
   | None -> state
@@ -193,65 +257,41 @@ let rec interpret state program =
 
 and execute_instruction state instr program =
   match instr with
-  | Add (rd, rs1, rs2) ->
-    let val1 = get_register_value state rs1 in
-    let val2 = get_register_value state rs2 in
-    let result = Int64.add val1 val2 in
-    set_register_value state rd result
-  | Sub (rd, rs1, rs2) ->
-    let val1 = get_register_value state rs1 in
-    let val2 = get_register_value state rs2 in
-    let result = Int64.sub val1 val2 in
-    set_register_value state rd result
-  | Xor (rd, rs1, rs2) ->
-    let val1 = get_register_value state rs1 in
-    let val2 = get_register_value state rs2 in
-    let result = Int64.logxor val1 val2 in
-    set_register_value state rd result
-  | Or (rd, rs1, rs2) ->
-    let val1 = get_register_value state rs1 in
-    let val2 = get_register_value state rs2 in
-    let result = Int64.logor val1 val2 in
-    set_register_value state rd result
-  | And (rd, rs1, rs2) ->
-    let val1 = get_register_value state rs1 in
-    let val2 = get_register_value state rs2 in
-    let result = Int64.logand val1 val2 in
-    set_register_value state rd result
-  | Sll (rd, rs1, rs2) ->
-    let val1 = get_register_value state rs1 in
-    let val2 = get_register_value state rs2 in
-    let result = Int64.shift_left val1 (Int64.to_int val2) in
-    set_register_value state rd result
-  | Srl (rd, rs1, rs2) ->
-    let val1 = get_register_value state rs1 in
-    let val2 = get_register_value state rs2 in
-    let result = Int64.shift_right_logical val1 (Int64.to_int val2) in
-    set_register_value state rd result
-  | Sra (rd, rs1, rs2) ->
-    let val1 = get_register_value state rs1 in
-    let val2 = get_register_value state rs2 in
-    let result = Int64.shift_right val1 (Int64.to_int val2) in
-    set_register_value state rd result
-  | Slt (rd, rs1, rs2) ->
-    let val1 = get_register_value state rs1 in
-    let val2 = get_register_value state rs2 in
-    let result = if Int64.compare val1 val2 < 0 then 1L else 0L in
-    set_register_value state rd result
-  | Sltu (rd, rs1, rs2) ->
-    let val1 = get_register_value state rs1 in
-    let val2 = get_register_value state rs2 in
-    let result = if Int64.unsigned_compare val1 val2 < 0 then 1L else 0L in
-    set_register_value state rd result
+  | Add (rd, rs1, rs2) -> execute_arithmetic_op state rd rs1 rs2 Int64.add
+  | Sub (rd, rs1, rs2) -> execute_arithmetic_op state rd rs1 rs2 Int64.sub
+  | Xor (rd, rs1, rs2) -> execute_arithmetic_op state rd rs1 rs2 Int64.logxor
+  | Or (rd, rs1, rs2) -> execute_arithmetic_op state rd rs1 rs2 Int64.logor
+  | And (rd, rs1, rs2) -> execute_arithmetic_op state rd rs1 rs2 Int64.logand
+  | Sll (rd, rs1, rs2) -> execute_shift_op state rd rs1 rs2 Int64.shift_left
+  | Srl (rd, rs1, rs2) -> execute_shift_op state rd rs1 rs2 Int64.shift_right_logical
+  | Sra (rd, rs1, rs2) -> execute_shift_op state rd rs1 rs2 Int64.shift_right
+  | Slt (rd, rs1, rs2) -> execute_comparison_op state rd rs1 rs2 (fun arg1 arg2 -> Int64.compare arg1 arg2 < 0)
+  | Sltu (rd, rs1, rs2) -> execute_comparison_op state rd rs1 rs2 (fun arg1 arg2 -> Int64.unsigned_compare arg1 arg2 < 0)
+  | Addi (rd, rs1, imm) -> execute_immediate_op state program rd rs1 imm Int64.add
+  | Xori (rd, rs1, imm) -> execute_immediate_op state program rd rs1 imm Int64.logxor
+  | Ori (rd, rs1, imm) -> execute_immediate_op state program rd rs1 imm Int64.logor
+  | Andi (rd, rs1, imm) -> execute_immediate_op state program rd rs1 imm Int64.logand
+  | Slli (rd, rs1, imm) -> execute_shift_immediate_op state program rd rs1 imm Int64.shift_left
+  | Srli (rd, rs1, imm) -> execute_shift_immediate_op state program rd rs1 imm Int64.shift_right_logical
+  | Srai (rd, rs1, imm) -> execute_shift_immediate_op state program rd rs1 imm Int64.shift_right
+  | Slti (rd, rs1, imm) -> execute_immediate_op state program rd rs1 imm (fun arg1 imm_value -> if Int64.compare arg1 imm_value < 0 then 1L else 0L)
+  | Sltiu (rd, rs1, imm) -> execute_immediate_op state program rd rs1 imm (fun arg1 imm_value -> if Int64.unsigned_compare arg1 imm_value < 0 then 1L else 0L)
+  | Mul (rd, rs1, rs2) -> execute_arithmetic_op state rd rs1 rs2 Int64.mul
+  | Div (rd, rs1, rs2) -> execute_arithmetic_op state rd rs1 rs2 Int64.div
+  | Rem (rd, rs1, rs2) -> execute_arithmetic_op state rd rs1 rs2 Int64.rem
+
+
+
+  (* | Mulh (rd, rs1, rs2) -> *)
   | Jalr (rd, rs1, imm) ->
     let val_rs1 = get_register_value state rs1 in
     let address_info = get_address12_value program imm in
     let new_pc =       
       match address_info with
-        | Immediate imm_value ->
-          resolve_address_excl_to_incl program (Int64.shift_right_logical (Int64.add val_rs1 (Int64.of_int imm_value)) 2)
-        | Label excluding_directives_label_offset ->
-          resolve_address_excl_to_incl program (Int64.add (Int64.of_int excluding_directives_label_offset) (Int64.shift_right_logical val_rs1 2))
+      | Immediate imm_value ->
+        resolve_address_excl_to_incl program (Int64.shift_right_logical (Int64.add val_rs1 (Int64.of_int imm_value)) 2)
+      | Label excluding_directives_label_offset ->
+        resolve_address_excl_to_incl program (Int64.add (Int64.of_int excluding_directives_label_offset) (Int64.shift_right_logical val_rs1 2))
     in
     let new_state = set_register_value state rd (Int64.add state.pc 1L) in
     set_pc new_state new_pc
@@ -271,6 +311,35 @@ and execute_instruction state instr program =
     in
     let new_state = set_register_value state X0 (Int64.add state.pc 1L) in
     set_pc new_state new_pc
+  | Jal (rd, imm_value) ->
+    let address_info = get_address20_value program imm_value in
+    let new_pc = 
+      match address_info with
+      | Immediate imm_value ->
+        resolve_address_excl_to_incl program (Int64.add (Int64.of_int (imm_value lsr 2)) (resolve_address_incl_to_excl program state.pc))
+      | Label excluding_directives_label_offset ->
+        resolve_address_excl_to_incl program (Int64.add (Int64.of_int excluding_directives_label_offset) (resolve_address_incl_to_excl program state.pc))
+    in
+    let new_state = set_register_value state rd (Int64.add state.pc 1L) in
+    set_pc new_state new_pc
+  | Bnez (rs1, imm_value) ->
+    let comparison_fn arg1 arg2 = arg1 <> arg2 in
+    handle_branch_condition state program rs1 None imm_value comparison_fn
+  | Beqz (rs1, imm_value) ->
+    let comparison_fn arg1 arg2 = arg1 = arg2 in
+    handle_branch_condition state program rs1 None imm_value comparison_fn
+  | Bltz (rs1, imm_value) ->
+    let comparison_fn arg1 arg2 = arg1 < arg2 in
+    handle_branch_condition state program rs1 None imm_value comparison_fn
+  | Bgt (rs1, rs2, imm_value) ->
+    let comparison_fn arg1 arg2 = arg1 > arg2 in
+    handle_branch_condition state program rs1 (Some rs2) imm_value comparison_fn
+  | Bgtz (rs1, imm_value) ->
+    let comparison_fn arg1 arg2 = arg1 > arg2 in
+    handle_branch_condition state program rs1 None imm_value comparison_fn
+  | Bne (rs1, rs2, imm_value) ->
+    let comparison_fn arg1 arg2 = arg1 <> arg2 in
+    handle_branch_condition state program rs1 (Some rs2) imm_value comparison_fn
   | _ -> state
 ;;
 
