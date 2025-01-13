c6b226ab7fcf46f19970f0684ba198c1aea01d26
Daniel Vlasenco <vlasenko.daniil26@gmail.com>
2025-01-13 19:29:41 +0300

    feat: add parsers for measure div and prod, support precedence
    Signed-off-by: Daniel Vlasenco <vlasenko.daniil26@gmail.com>

--- FSharpUnitsOfMeasure/lib/parse/units_of_measure.ml
+++ FSharpUnitsOfMeasure/lib/parse/units_of_measure.ml
@@ -14,16 +14,16 @@ let pm_num_int = parse_int >>| fun i -> Mnum_int i
 let pm_num_float = parse_float >>| fun f -> Mnum_float f
 let pm_num = pm_num_int <|> pm_num_float
 let pm_id = parse_ident >>| fun id -> Measure_ident id
+let pm_atom pm = choice [ skip_token "(" *> pm <* skip_token ")"; pm_id ]
 
-(* Power has highest priority*)
-let pm_pow pm =
+let pm_pow pm_atom =
   let p_expo =
     let* sign = skip_token "^" *> option "" (string "-") in
     let* num = skip_ws *> parse_int in
     return (if String.( = ) sign "-" then -num else num)
   in
-  let* m = pm in
-  let* exp = p_expo in
+  let* m = pm_atom in
+  let* exp = option 1 p_expo in
   match exp with
   | 0 -> return @@ Measure_dimless
   | 1 -> return @@ m
@@ -35,10 +35,8 @@ let pm_pow pm =
   | _ -> fail "Cannot parse measure power"
 ;;
 
-(* Sequence should have higher priority than product and division *)
-(* I think that chainls are needed to save left associativity *)
-let pm_pow_seq pm =
-  let* seq = many1 (skip_ws *> pm_pow pm) in
+let pm_pow_seq pm_p =
+  let* seq = many1 (skip_ws *> pm_pow pm_p) in
   let wrap = function
     | h :: tl -> return @@ List.fold tl ~init:h ~f:(fun x y -> Measure_prod (x, y))
     | _ -> fail "Cannot parse measure sequence"
@@ -46,9 +44,14 @@ let pm_pow_seq pm =
   wrap seq
 ;;
 
+let pm_prod = skip_token "*" *> return (fun m1 m2 -> Measure_prod (m1, m2))
+let pm_div = skip_token "/" *> return (fun m1 m2 -> Measure_div (m1, m2))
+
 let pm =
-  let pm = pm_pow_seq pm_id <|> pm_id in
-  skip_ws *> pm <* skip_ws
+  fix (fun pm ->
+    let pm = pm_pow_seq (pm_pow (pm_atom pm)) in
+    let pm = chainl pm (pm_prod <|> pm_div) <|> pm in
+    skip_ws *> pm <* skip_ws)
 ;;
 
 let puom =
--- FSharpUnitsOfMeasure/tests/unitTests/tests_units_of_measure.ml
+++ FSharpUnitsOfMeasure/tests/unitTests/tests_units_of_measure.ml
@@ -51,7 +51,7 @@ let%expect_test "parse two measure powers in sequence as product" =
        (Measure_pow ((Measure_ident "cm"), 3)))) |}]
 ;;
 
-let%expect_test "parse three measure powers in sequence as left-associated product" =
+let%expect_test "parse three measure powers in sequence" =
   pp pp_measure pm {| kg^3 cm^3 lb^3 |};
   [%expect
     {|
@@ -61,11 +61,214 @@ let%expect_test "parse three measure powers in sequence as left-associated produ
        (Measure_pow ((Measure_ident "lb"), 3)))) |}]
 ;;
 
+let%expect_test "parse two measure powers and measure id in sequence " =
+  pp pp_measure pm {| kg^3 cm^3 lb |};
+  [%expect
+    {|
+    (Measure_prod (
+       (Measure_prod ((Measure_pow ((Measure_ident "kg"), 3)),
+          (Measure_pow ((Measure_ident "cm"), 3)))),
+       (Measure_ident "lb"))) |}]
+;;
+
 let%expect_test "parse many measure powers in sequence as product" =
   pp pp_measure pm {| kg cm^3 N^-3 v v^4 |};
   [%expect
     {|
-    : end_of_input |}]
+    (Measure_prod (
+       (Measure_prod (
+          (Measure_prod (
+             (Measure_prod ((Measure_ident "kg"),
+                (Measure_pow ((Measure_ident "cm"), 3)))),
+             (Measure_div (Measure_dimless,
+                (Measure_pow ((Measure_ident "N"), 3))))
+             )),
+          (Measure_ident "v"))),
+       (Measure_pow ((Measure_ident "v"), 4)))) |}]
+;;
+
+(************************** Measure product **************************)
+
+let%expect_test "parse product of two measure idents" =
+  pp pp_measure pm {| kg * m |};
+  [%expect {|
+    (Measure_prod ((Measure_ident "kg"), (Measure_ident "m"))) |}]
+;;
+
+let%expect_test "parse product of three measure idents" =
+  pp pp_measure pm {| kg * m * s |};
+  [%expect
+    {|
+    (Measure_prod ((Measure_prod ((Measure_ident "kg"), (Measure_ident "m"))),
+       (Measure_ident "s"))) |}]
+;;
+
+let%expect_test "parse product of two measure powers" =
+  pp pp_measure pm {| kg^2 * m^2 |};
+  [%expect
+    {|
+    (Measure_prod ((Measure_pow ((Measure_ident "kg"), 2)),
+       (Measure_pow ((Measure_ident "m"), 2)))) |}]
+;;
+
+let%expect_test "parse product of three measure powers" =
+  pp pp_measure pm {| kg^2 * m^2 * s^2 |};
+  [%expect
+    {|
+    (Measure_prod (
+       (Measure_prod ((Measure_pow ((Measure_ident "kg"), 2)),
+          (Measure_pow ((Measure_ident "m"), 2)))),
+       (Measure_pow ((Measure_ident "s"), 2)))) |}]
+;;
+
+let%expect_test "parse product of mixed measures" =
+  pp pp_measure pm {| kg^2 * m |};
+  [%expect
+    {|
+    (Measure_prod ((Measure_pow ((Measure_ident "kg"), 2)), (Measure_ident "m"))) |}]
+;;
+
+(************************** Measure division **************************)
+
+let%expect_test "parse division of two measure idents" =
+  pp pp_measure pm {| kg / m |};
+  [%expect {|
+    (Measure_div ((Measure_ident "kg"), (Measure_ident "m"))) |}]
+;;
+
+let%expect_test "parse division of three measure idents" =
+  pp pp_measure pm {| kg / m / s |};
+  [%expect
+    {|
+    (Measure_div ((Measure_div ((Measure_ident "kg"), (Measure_ident "m"))),
+       (Measure_ident "s"))) |}]
+;;
+
+let%expect_test "parse division of two measure powers" =
+  pp pp_measure pm {| kg^2 / m^2 |};
+  [%expect
+    {|
+    (Measure_div ((Measure_pow ((Measure_ident "kg"), 2)),
+       (Measure_pow ((Measure_ident "m"), 2)))) |}]
+;;
+
+let%expect_test "parse division of three measure powers" =
+  pp pp_measure pm {| kg^2 / m^2 / s^2 |};
+  [%expect
+    {|
+    (Measure_div (
+       (Measure_div ((Measure_pow ((Measure_ident "kg"), 2)),
+          (Measure_pow ((Measure_ident "m"), 2)))),
+       (Measure_pow ((Measure_ident "s"), 2))))  |}]
+;;
+
+let%expect_test "parse division of mixed measures" =
+  pp pp_measure pm {| kg^2 / m |};
+  [%expect
+    {|
+    (Measure_div ((Measure_pow ((Measure_ident "kg"), 2)), (Measure_ident "m"))) |}]
+;;
+
+(************************** Parentheses **************************)
+
+let%expect_test "parse sequence of idents with parentheses RA" =
+  pp pp_measure pm {| kg (cm s g) |};
+  [%expect
+    {|
+    (Measure_prod ((Measure_ident "kg"),
+       (Measure_prod ((Measure_prod ((Measure_ident "cm"), (Measure_ident "s"))),
+          (Measure_ident "g")))
+       )) |}]
+;;
+
+let%expect_test "parse sequence of idents and powers with parentheses" =
+  pp pp_measure pm {| kg (cm s^2 g^-1) |};
+  [%expect
+    {|
+    (Measure_prod ((Measure_ident "kg"),
+       (Measure_prod (
+          (Measure_prod ((Measure_ident "cm"),
+             (Measure_pow ((Measure_ident "s"), 2)))),
+          (Measure_div (Measure_dimless, (Measure_ident "g")))))
+       )) |}]
+;;
+
+let%expect_test "parse sequence of idents with parentheses LA" =
+  pp pp_measure pm {| (kg cm s) g |};
+  [%expect
+    {|
+    (Measure_prod (
+       (Measure_prod (
+          (Measure_prod ((Measure_ident "kg"), (Measure_ident "cm"))),
+          (Measure_ident "s"))),
+       (Measure_ident "g"))) |}]
+;;
+
+let%expect_test "parse RA product" =
+  pp pp_measure pm {| kg * (cm * s) |};
+  [%expect
+    {|
+    (Measure_prod ((Measure_ident "kg"),
+       (Measure_prod ((Measure_ident "cm"), (Measure_ident "s"))))) |}]
+;;
+
+let%expect_test "parse RA division" =
+  pp pp_measure pm {| kg / (cm / s) |};
+  [%expect
+    {|
+    (Measure_div ((Measure_ident "kg"),
+       (Measure_div ((Measure_ident "cm"), (Measure_ident "s"))))) |}]
+;;
+
+(************************** Other **************************)
+
+let%expect_test "parse something strange" =
+  pp
+    pp_measure
+    pm
+    {| ((kg * cm^3 / (s^-1 / y^2) * cm kg^2 / (g^-3 pb^2)) m^3) cm (u^3 r^-1)|};
+  [%expect
+    {|
+    (Measure_prod (
+       (Measure_prod (
+          (Measure_prod (
+             (Measure_div (
+                (Measure_prod (
+                   (Measure_div (
+                      (Measure_prod ((Measure_ident "kg"),
+                         (Measure_pow ((Measure_ident "cm"), 3)))),
+                      (Measure_div (
+                         (Measure_div (Measure_dimless, (Measure_ident "s"))),
+                         (Measure_pow ((Measure_ident "y"), 2))))
+                      )),
+                   (Measure_prod ((Measure_ident "cm"),
+                      (Measure_pow ((Measure_ident "kg"), 2))))
+                   )),
+                (Measure_prod (
+                   (Measure_div (Measure_dimless,
+                      (Measure_pow ((Measure_ident "g"), 3)))),
+                   (Measure_pow ((Measure_ident "pb"), 2))))
+                )),
+             (Measure_pow ((Measure_ident "m"), 3)))),
+          (Measure_ident "cm"))),
+       (Measure_prod ((Measure_pow ((Measure_ident "u"), 3)),
+          (Measure_div (Measure_dimless, (Measure_ident "r")))))
+       )) |}]
+;;
+
+let%expect_test "parse something strange" =
+  pp
+    pp_measure
+    pm
+    {| (kg * cm^3 / (s^-1 / y^2))|};
+  [%expect
+    {|
+    (Measure_div (
+       (Measure_prod ((Measure_ident "kg"),
+          (Measure_pow ((Measure_ident "cm"), 3)))),
+       (Measure_div ((Measure_div (Measure_dimless, (Measure_ident "s"))),
+          (Measure_pow ((Measure_ident "y"), 2))))
+       )) |}]
 ;;
 
 (************************** Units of measure **************************)
