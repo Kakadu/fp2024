0fa18abd531ea281798bac54006f72fba398a46f
sofyak0zyreva <k81sofia@gmail.com>
2025-01-15 03:12:34 +0300

    feat: eval unary and binary exprs
    Signed-off-by: sofyak0zyreva <k81sofia@gmail.com>

--- OcamlBR/lib/interpreter.ml
+++ OcamlBR/lib/interpreter.ml
@@ -84,4 +84,74 @@ module Eval (M : MONAD) : sig end = struct
     Base.Map.iteri env ~f:(fun ~key ~data -> printf "%s = %a\n" key pp_value data);
     printf "}\n"
   ;;
+
+  let eval_un_op = function
+    | Negative, VInt i -> return (VInt (-i))
+    | Positive, VInt i -> return (VInt i)
+    | Not, VBool b -> return (VBool (not b))
+    | _ ->
+      Format.printf "ty err8\n";
+      fail `Type_error
+  ;;
+
+  let rec eval_bin_op = function
+    | Add, VInt i1, VInt i2 -> return (VInt (i1 + i2))
+    | Mult, VInt i1, VInt i2 -> return (VInt (i1 * i2))
+    | Sub, VInt i1, VInt i2 -> return (VInt (i1 - i2))
+    | Div, VInt _, VInt i2 when i2 = 0 -> fail `Division_by_zero
+    | Div, VInt i1, VInt i2 -> return (VInt (i1 / i2))
+    | Cons, v, VList vl -> return (VList (v :: vl))
+    | Gt, VInt i1, VInt i2 -> return (VBool (i1 > i2))
+    | Lt, VInt i1, VInt i2 -> return (VBool (i1 < i2))
+    | Gte, VInt i1, VInt i2 -> return (VBool (i1 >= i2))
+    | Lte, VInt i1, VInt i2 -> return (VBool (i1 <= i2))
+    | And, VBool b1, VBool b2 -> return (VBool (b1 && b2))
+    | Or, VBool b1, VBool b2 -> return (VBool (b1 || b2))
+    | Eq, VInt i1, VInt i2 -> return (VBool (i1 = i2))
+    | Neq, VInt i1, VInt i2 -> return (VBool (i1 <> i2))
+    | Eq, VString s1, VString s2 -> return (VBool (s1 = s2))
+    | Neq, VString s1, VString s2 -> return (VBool (s1 <> s2))
+    | Eq, VBool b1, VBool b2 -> return (VBool (b1 = b2))
+    | Neq, VBool b1, VBool b2 -> return (VBool (b1 <> b2))
+    | Eq, VUnit, VUnit -> return (VBool true)
+    | Neq, VUnit, VUnit -> return (VBool false)
+    | Eq, VList l1, VList l2 -> eval_eq_list Eq l1 l2
+    | Neq, VList l1, VList l2 -> eval_eq_list Neq l1 l2
+    | Eq, VTuple (v1, v2, v_rest), VTuple (v1', v2', v_rest') ->
+      eval_eq_list Eq (v1 :: v2 :: v_rest) (v1' :: v2' :: v_rest')
+    | Neq, VTuple (v1, v2, v_rest), VTuple (v1', v2', v_rest') ->
+      eval_eq_list Neq (v1 :: v2 :: v_rest) (v1' :: v2' :: v_rest')
+    | Eq, VOption o1, VOption o2 ->
+      (match o1, o2 with
+       | Some o1, Some o2 -> eval_bin_op (Eq, o1, o2)
+       | None, None -> return (VBool true)
+       | _ -> return (VBool false))
+    | Neq, VOption o1, VOption o2 ->
+      (match o1, o2 with
+       | Some o1, Some o2 -> eval_bin_op (Neq, o1, o2)
+       | None, None -> return (VBool true)
+       | _ -> return (VBool false))
+    | _ ->
+      Format.printf "ty err-1\n";
+      fail `Type_error
+
+  and eval_eq_list op l1 l2 =
+    if List.length l1 <> List.length l2
+    then return (VBool false)
+    else (
+      let f1 acc el1 el2 =
+        let* acc = acc in
+        match acc with
+        | VBool false -> return (VBool false)
+        | VBool true ->
+          let* res = eval_bin_op (op, el1, el2) in
+          (match res with
+           | VBool true -> return (VBool true)
+           | _ -> return (VBool false))
+        | _ ->
+          Format.printf "ty err1\n";
+          fail `Type_error
+      in
+      List.fold_left2 f1 (return (VBool true)) l1 l2)
+  ;;
 end
