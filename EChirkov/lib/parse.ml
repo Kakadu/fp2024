cd0281efc12008ec30871748dc0030fe15948022
Chirkov Dmitri <chdmitry2@gmail.com>
2025-01-17 21:24:09 +0300

    add(parse): struct
    Signed-off-by: Chirkov Dmitri <chdmitry2@gmail.com>

--- EChirkov/lib/parse.ml
+++ EChirkov/lib/parse.ml
@@ -4,6 +4,7 @@
 
 open Ast
 open Angstrom
+open Base
 
 let is_ws = function
   | ' ' -> true
@@ -12,5 +13,45 @@ let is_ws = function
   | _ -> false
 ;;
 
-let skip_ws = skip_while is_ws
-let parse str = parse_string ~consume:All (skip_ws *> string str <* skip_ws) str
+let ws = skip_while is_ws
+
+let safe_tl = function
+  | [] -> []
+  | _ :: tail -> tail
+;;
+
+let token str = ws *> string str (* TODO *)
+
+let parse_rec_flag = option Nonrecursive (token "rec" *> return Recursive)
+
+let parse_expression = (*TODO*)
+let parse_struct_value =
+  token "let"
+  *>
+  let* rec_flag = parse_rec_flag in
+  let* value_binding_list = parse_value_binding_list parse_expression in
+  option
+    (SValue (rec_flag, List.hd_exn value_binding_list, safe_tl value_binding_list))
+    (token "in" *> parse_expression
+     >>| fun exp ->
+     SEval
+       (ELet (rec_flag, List.hd_exn value_binding_list, safe_tl value_binding_list, exp))
+    )
+;;
+
+let parse_expression =
+  ws
+  *> fix (fun exp -> (* TODO *) )
+;;
+
+let parse_structure =
+  ws
+  *>
+  let parse_structure_item =
+    parse_struct_value <|> (parse_expression >>| fun exp -> SEval exp)
+  in
+  let semicolons = many (token ";;") in
+  sep_by semicolons parse_structure_item <* semicolons <* ws
+;;
+
+let parse str = parse_string ~consume:All (ws *> parse_structure <* ws) str
