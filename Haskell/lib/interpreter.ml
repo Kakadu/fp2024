6472ecd589100b921298b5df4b77a56e0c357a48
Kostya <koreshin67@gmail.com>
2025-01-21 15:46:48 +0300

    ref: add interpreter to separate function

--- Haskell/lib/interpreter.ml
+++ Haskell/lib/interpreter.ml
@@ -2,9 +2,65 @@
 
 (** SPDX-License-Identifier: MIT *)
 
-open Inferencer
-open Typedtree
+let interpret =
+  let rec helper st names text dump_parsetree print_types env enviroment n =
+    match text with
+    | [] ->
+      if env != Inferencer.typeenv_empty && env != Inferencer.initial_env && print_types
+      then Format.printf "%a\n%!" Inferencer.pp_some_typeenv (names, env)
+    | "" :: rest -> helper st names rest dump_parsetree print_types env enviroment n
+    | line :: rest ->
+      if dump_parsetree then Parser.parse_and_print_line line;
+      (match Parser.parse_line line with
+       | Result.Ok list ->
+         (match Inferencer.w list env st with
+          | st, Result.Ok (env, nn) ->
+            (match Eval.eval list enviroment n with
+             | Result.Ok (enviroment, n) ->
+               helper
+                 st
+                 (List.fold_left (fun nn n -> n :: nn) names nn)
+                 rest
+                 dump_parsetree
+                 print_types
+                 env
+                 enviroment
+                 n
+             | Result.Error (err, enviroment, n) ->
+               Format.printf "%a\n%!" Pprint.pp_error err;
+               helper
+                 st
+                 (List.fold_left (fun nn n -> n :: nn) names nn)
+                 rest
+                 dump_parsetree
+                 print_types
+                 env
+                 enviroment
+                 n)
+          | st, Result.Error err ->
+            Format.printf "%a\n%!" Pprint.pp_error err;
+            helper st names rest dump_parsetree print_types env enviroment n)
+       | Result.Error error -> Format.printf "%s\n%!" error)
+  in
+  helper 2 []
+;;
 
-type enviroment = int
-
-let eval (binding : Ast.binding list) (env : enviroment) (n : int) = Result.Ok (env, 0)
+let interpret_line line env st dump_parsetree print_types enviroment n =
+  match Parser.parse_line line with
+  | Result.Ok list ->
+    if dump_parsetree then Parser.parse_and_print_line line;
+    (match Inferencer.w list env st with
+     | st, Result.Ok (env, names) ->
+       if print_types then Format.printf "%a\n%!" Inferencer.pp_some_typeenv (names, env);
+       (match Eval.eval list enviroment n with
+        | Result.Ok (enviroment, n) -> env, st, enviroment, n
+        | Result.Error (err, enviroment, n) ->
+          Format.printf "%a\n%!" Pprint.pp_error err;
+          env, st, enviroment, n)
+     | st, Result.Error err ->
+       Format.printf "%a\n%!" Pprint.pp_error err;
+       env, st, enviroment, n)
+  | Result.Error error ->
+    Format.printf "%s\n%!" error;
+    env, st, enviroment, n
+;;
