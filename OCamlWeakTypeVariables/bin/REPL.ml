fc53700b777b847cfbd642b2d4d7b37974fd12b5
Homka122 <kombaev02@gmail.com>
2025-01-30 20:24:21 +0300

    Fix: spell mistake in REPL option msg
    Signed-off-by: Homka122 <kombaev02@gmail.com>

--- OCamlWeakTypeVariables/bin/REPL.ml
+++ OCamlWeakTypeVariables/bin/REPL.ml
@@ -1,31 +1,38 @@
 open Lib
 
-type opts = { mutable dump_parsetree : bool }
+type opts =
+  { mutable dump_parsetree : bool
+  ; mutable dump_inference : bool
+  }
 
-let run_single dump_parsetree =
+let run_single opts =
   let text = In_channel.(input_all stdin) |> String.trim in
   let ast = Parser.parse text in
   match ast with
   | Error e -> Format.printf "Error: %s\n%!" e
   | Result.Ok ast ->
-    if dump_parsetree
-    then Format.printf "Parsed result: @[%a@]\n%!" Lib.Ast.pp_structure_item ast
-    else Format.printf "Not implemented. Only -dparsertree support"
+    (match opts.dump_inference with
+     | true -> Format.printf "val"
+     | false ->
+       (match opts.dump_parsetree with
+        | true -> Format.printf "Parsed result: @[%a@]\n%!" Lib.Ast.pp_structure_item ast
+        | false ->
+          Format.printf "Not implemented. Only -dparsertree and -dinference support"))
 ;;
 
 let () =
-  let opts = { dump_parsetree = false } in
+  let opts = { dump_parsetree = false; dump_inference = false } in
   let () =
     let open Stdlib.Arg in
     parse
       [ ( "-dparsetree"
         , Unit (fun () -> opts.dump_parsetree <- true)
-        , "Dump parse tree, don't eval enything" )
+        , "Dump parse tree, don't eval anything" )
       ]
       (fun _ ->
         Stdlib.Format.eprintf "Anonymous arguments are not supported\n";
         Stdlib.exit 1)
       "Read-Eval-Print-Loop for my cool homka&damir's parser"
   in
-  run_single opts.dump_parsetree
+  run_single opts
 ;;
