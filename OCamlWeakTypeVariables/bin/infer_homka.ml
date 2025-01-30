2e922f6c86cf583d553c37bdebd8c240ac85b166
Homka122 <kombaev02@gmail.com>
2025-01-30 20:25:47 +0300

    Feat: infer_homka now can infer exprs
    Signed-off-by: Homka122 <kombaev02@gmail.com>

--- OCamlWeakTypeVariables/bin/infer_homka.ml
+++ OCamlWeakTypeVariables/bin/infer_homka.ml
@@ -1,7 +1,7 @@
-open Lib.Infer
 open Lib.Parser
 open Lib.Infer_print
 open Lib.Types
+open Lib.Infer
 
 let str = "let f x y z homka damir chtooo = x y (homka + damir) chtooo"
 let fac self n = if n <= 1 then 1 else n * self (n - 1)
@@ -12,6 +12,9 @@ let str =
    and homka = 122\n\
    and fac self n = if true then 1 else n * self (n - 1)"
 ;;
+
+let str = "let rec homka n = damir 4\nand damir n = homka 5"
+let str = "let rec homka n = damir 4\nand damir n = homka 5 in homka"
 
 let homka =
   match parse str with
@@ -41,16 +44,26 @@ let () =
     names
 ;;
 
-(* let homka =
-   match parse_expr "fun foo -> foo + 2" with
-   | Result.Ok x -> x
-   | Result.Error e -> failwith e
-   ;;
+let str_expr =
+  "let rec foo b = if b then fun foo -> foo + 2 else fun foo -> foo * 10\n\
+   and homka = 122\n\
+   and fac self n = if true then 1 else n * self (n - 1) in fac"
+;;
 
-   let homka_infer =
-   match run_expr_inferencer homka with
-   | Result.Ok x -> x
-   | Result.Error e -> failwith (show_error e)
-   ;;
+let str_expr = "let rec homka n = damir 4\nand damir n = homka 5 in homka"
 
-   let () = print_endline ("\nANSWER: " ^ show_typ homka_infer) *)
+let homka =
+  match parse_expr str_expr with
+  | Result.Ok x -> x
+  | Result.Error e -> failwith e
+;;
+
+let homka_infer =
+  match run_expr_inferencer homka with
+  | Result.Ok x -> x
+  | Result.Error e -> failwith (show_error e)
+;;
+
+let () = Format.printf "\n"
+let () = Format.printf "> %s" str_expr
+let () = Format.printf "\nANSWER: %a\n" pp_typ_my homka_infer
