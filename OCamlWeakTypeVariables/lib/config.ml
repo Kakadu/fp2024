13b1be23fa9cb9c0968ebb1f81ff9ca5b9809299
Homka122 <kombaev02@gmail.com>
2025-02-10 15:47:43 +0300

    Refactor: add coments to config variables
    Signed-off-by: Homka122 <kombaev02@gmail.com>

--- OCamlWeakTypeVariables/lib/config.ml
+++ OCamlWeakTypeVariables/lib/config.ml
@@ -1,3 +1,11 @@
+[@@@ocaml.text "/*"]
+
+(** Copyright 2024, Damir Yunusov and Ilhom Kombaev *)
+
+(** SPDX-License-Identifier: LGPL-3.0-or-later *)
+
+[@@@ocaml.text "/*"]
+
 let debug = false
 let vars_min = true
 let vars_char = true
--- OCamlWeakTypeVariables/lib/config.mli
+++ OCamlWeakTypeVariables/lib/config.mli
@@ -1,4 +1,21 @@
+(** Enable debug output to stdout.
+    If true, prints debug messages to stdout. *)
 val debug : bool
+
+(** Minimize type variables.
+    If true, type variables will minimize as much as possible.
+    Example: '4 -> '2 -> ('5 option) -> '4
+    becomes: '1 -> '2 -> '3 option -> '1 *)
 val vars_min : bool
+
+(** Representing type variables with letters instead of numbers.
+    If true, type variables use alphabetical characters (e.g., 'a 'b).
+    Example: '4 -> '2 -> >('5 option) -> '4
+    becomes: 'd -> 'b -> ('e option) -> 'd *)
 val vars_char : bool
+
+(** Display quantified type variables explicitly.
+    If true, binding variables are printed before the type.
+    For example 'a -> 'b -> int
+    will be 'b . 'a -> 'b -> int *)
 val show_scheme_vars : bool
