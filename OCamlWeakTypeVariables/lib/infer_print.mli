a2ce110dadcf2f52a0a99a4d4d083cf1cf834ff7
Homka122 <kombaev02@gmail.com>
2025-02-10 16:15:08 +0300

    Fix: fix ocaml comments
    Signed-off-by: Homka122 <kombaev02@gmail.com>

--- OCamlWeakTypeVariables/lib/infer_print.mli
+++ OCamlWeakTypeVariables/lib/infer_print.mli
@@ -6,12 +6,14 @@
 
 [@@@ocaml.text "/*"]
 
-
 (** Pretty printer for base type *)
 val pp_base_type_my : Format.formatter -> Types.base_type -> unit
+
 (** Pretty printer for type *)
 val pp_typ_my : Format.formatter -> Types.typ -> unit
+
 (** Pretty printf for inference errors *)
 val pp_error_my : Format.formatter -> Types.error -> unit
+
 (** Print type *)
 val print_typ : ?name:string -> Types.typ -> unit
