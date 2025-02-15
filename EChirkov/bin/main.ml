d926c4a563def908e1cc93f3961250fd7db9386c
Chirkov Dmitri <chdmitry2@gmail.com>
2025-02-15 21:38:34 +0300

    fix: some rework
    Signed-off-by: Chirkov Dmitri <chdmitry2@gmail.com>

--- EChirkov/bin/main.ml
+++ EChirkov/bin/main.ml
@@ -1,3 +1,14 @@
 (** Copyright 2024, Dmitri Chirkov*)
 
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
+
+open MiniML.Parser
+open Angstrom
+
+let r = parse_string ~consume:All parse "let x = 23"
+
+let () =
+  match r with
+  | Ok s -> print_endline s
+  | Error e -> print_endline e
+;;
