ff09d2a36ade9f4f373240857fc36239aef7dc42
Rodion Suvorov <rodion.suvorov.94@mail.ru>
2025-02-24 23:15:52 +0300

    refactor: improve pprint for errors, remove comments

--- OcamlADT/lib/inferTypes.ml
+++ OcamlADT/lib/inferTypes.ml
@@ -15,30 +15,7 @@ end
 
 type binder_set = VarSet.t [@@deriving show { with_path = false }]
 
-(*maybe change name*)
-(* type typ =
-   | Typ_prim of string
-   | Typ_var of binder
-   | Typ_arrow of typ * typ
-   | Typ_tuple of typ list
-   | Typ_list of typ
-   | Typ_unit
-   [@@deriving show { with_path = false }] *)
-
 type scheme = Forall of binder_set * t [@@deriving show { with_path = false }]
-
-(* let int_typ = Typ_prim "int"
-let ch_typ = Typ_prim "char"
-let string_typ = Typ_prim "string"
-let bool_typ = Typ_prim "bool"
-let var_typ x = Typ_var x
-let arrow_typ l r = Typ_arrow (l, r)
-let ( @-> ) = arrow_typ
-let list_typ typ = Typ_list typ
-let tuple_typ typ_list = Typ_tuple typ_list *)
-
-(*printers*)
-(*maybe use pprinter ??*)
 let rec pprint_type_tuple fmt = function
   | [] -> ()
   | [ h ] ->
@@ -75,8 +52,6 @@ and pprint_type_with_parens_if_tuple fmt ty =
   | Type_tuple _ -> fprintf fmt "(%a)" pprint_type ty
   | _ -> pprint_type fmt ty
 
-(* | Type_list ty1 -> fprintf fmt "%a list" pprint_type ty1 *)
-
 (*errors*)
 
 type error =
@@ -96,7 +71,7 @@ type error =
   ]
 
 let pp_inf_err fmt = function
-  | `Occurs_check (string, t) -> fprintf fmt "Occurs_check"
+  | `Occurs_check (string, t) -> fprintf fmt "Occurs_check: %s and %a\n" string pprint_type t 
   | `Unification_failed (typ1, typ2) ->
     fprintf fmt "Unification_failed: %a # %a" pprint_type typ1 pprint_type typ2
   | `Wrong_exp -> fprintf fmt "Wrong_exp"
