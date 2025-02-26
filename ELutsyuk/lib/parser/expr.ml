dbfaf22698d74ffba16b3e5e62bf8b5cbf65830d
Lutsyuk Victoria <victorialutsyuk.t@gmail.com>
2025-02-26 03:13:44 +0300

    ref(prs): expression parser
    Signed-off-by: Lutsyuk Victoria <victorialutsyuk.t@gmail.com>

--- ELutsyuk/lib/parser/expr.ml
+++ ELutsyuk/lib/parser/expr.ml
@@ -4,109 +4,108 @@
 
 open Angstrom
 open Ast
-open Aux
-open Pat
+open Auxilary
+open Const
+open Patterns
 
-let pexpr_var =
+let prs_expr_var =
   trim
   @@
-  let* var = pid in
-  return @@ Var var
+  let+ parsed = prs_id in
+  Var parsed
 ;;
 
-let pexpr_const =
+let prs_expr_const =
   trim
   @@
-  let* cons = pconst in
-  return @@ Const cons
+  let+ parsed = prs_const in
+  Const parsed
 ;;
 
-let pexpr_list parse_expr =
+let prs_expr_list prs_expr =
   token "[]" *> (return @@ List [])
   <|> square_par
       @@
-      let* el1 = parse_expr in
-      let* rest = many (token ";" *> parse_expr) in
-      return @@ List (el1 :: rest)
+      let* el1 = prs_expr in
+      let+ rest = many (token ";" *> prs_expr) in
+      List (el1 :: rest)
 ;;
 
-let pexpr_tuple parse_expr =
+let prs_expr_tuple prs_expr =
   round_par
   @@
-  let* el1 = parse_expr in
-  let* el2 = token "," *> parse_expr in
-  let* rest = many (token "," *> parse_expr) in
-  return (Tup (el1, el2, rest))
+  let* el1 = prs_expr in
+  let* el2 = token "," *> prs_expr in
+  let+ rest = many (token "," *> prs_expr) in
+  Tup (el1, el2, rest)
 ;;
 
-let pexpr_fun parse_pat parse_expr =
-  token "fun"
-  *>
-  let* pat = parse_pat in
-  let* params = many parse_pat in
-  token "->"
-  *>
-  let* body_expr = parse_expr in
+let prs_expr_fun prs_pat prs_expr =
+  let* _ = token "fun" in
+  let* pat = prs_pat in
+  let* params = many prs_pat in
+  let* _ = token "->" in
+  let+ body_expr = prs_expr in
   let expr =
     match params with
     | [] -> body_expr
     | _ -> List.fold_right (fun par acc -> Fun (par, acc)) params body_expr
   in
-  return @@ Fun (pat, expr)
+  Fun (pat, expr)
 ;;
 
-let plet_binding parse_expr =
+let prs_let_binding prs_expr =
+  let* pat = prs_pat in
+  let* params = many prs_pat in
+  let* body_expr = token "=" *> prs_expr in
+  let expr =
+    match params with
+    | [] -> body_expr
+    | _ -> List.fold_right (fun par acc -> Fun (par, acc)) params body_expr
+  in
+  return { pat : pat; expr : expr }
+;;
+
+let prs_expr_let prs_expr =
+  let* _ = token "let" in
   let* is_rec = token "rec" *> return Rec <|> return NonRec in
-  let* let_pat = ppat in
-  let* params = many ppat in
-  let* body_expr = token "=" *> parse_expr in
-  let expr =
-    match params with
-    | [] -> body_expr
-    | _ -> List.fold_right (fun par acc -> Fun (par, acc)) params body_expr
-  in
-  return (is_rec, let_pat, expr)
+  let* binding = prs_let_binding prs_expr in
+  let* bindings_list = many (token "and" *> prs_let_binding prs_expr) in
+  let+ in_expr = token "in" *> prs_expr <|> return @@ Const Unit in
+  Let (is_rec, binding, bindings_list, in_expr)
 ;;
 
-let pexpr_let parse_expr =
-  token "let"
-  *>
-  let* is_rec, pat, expr = plet_binding parse_expr in
-  let* in_expr = token "in" *> parse_expr <|> return @@ Const Unit in
-  return @@ Let ({ is_rec; pat; expr }, in_expr)
+let prs_expr_branch prs_expr =
+  let* if_cond = token "if" *> prs_expr in
+  let* then_cond = token "then" *> prs_expr in
+  let+ else_cond = token "else" *> prs_expr <|> return @@ Const Unit in
+  Branch (if_cond, then_cond, else_cond)
 ;;
 
-let pexpr_branch pexpr =
-  let* if_cond = token "if" *> pexpr in
-  let* then_cond = token "then" *> pexpr in
-  let* else_cond = token "else" *> pexpr <|> return @@ Const Unit in
-  return @@ Branch (if_cond, then_cond, else_cond)
-;;
-
-let pexpr_match ppat pexpr =
+(* let prs_expr_match prs_pat prs_expr =
   let p_case =
     token "|"
     *>
-    let* case_pat = ppat in
-    let* case_expr = token "->" *> pexpr in
+    let* case_pat = prs_pat in
+    let* case_expr = token "->" *> prs_expr in
     return { match_pat = case_pat; match_expr = case_expr }
   in
-  let* exp = token "match" *> pexpr in
+  let* exp = token "match" *> prs_expr in
   let* case1 = token "with" *> p_case in
   let* rest_cases = many1 p_case in
   return @@ Match (exp, case1, rest_cases)
-;;
+;; *)
 
 let chainl1 expr oper =
   let rec go acc = lift2 (fun f x -> f acc x) oper expr >>= go <|> return acc in
   expr >>= go
 ;;
 
-let pbin_op l_exp binop =
+let prs_bin_op l_exp binop =
   chainl1 l_exp (binop >>| fun op exp1 exp2 -> BinOp (op, exp1, exp2))
 ;;
 
-let prel =
+let prs_rel =
   choice
     [ token "=" *> return Eq
     ; token "<>" *> return Ne
@@ -117,37 +116,47 @@ let prel =
     ]
 ;;
 
-let plogical = choice [ token "&&" *> return And; token "||" *> return Or ]
-let pmul = token "*" *> return Mul
-let padd = token "+" *> return Add
-let psub = token "-" *> return Sub
-let pdiv = token "/" *> return Div
+let prs_logical = choice [ token "&&" *> return And; token "||" *> return Or ]
+let prs_mul = token "*" *> return Mul
+let prs_add = token "+" *> return Add
+let prs_sub = token "-" *> return Sub
+let prs_div = token "/" *> return Div
 
-let pexpr_app pexpr =
+let prs_expr_app expr =
   let app = return @@ fun exp1 exp2 -> App (exp1, exp2) in
-  chainl1 pexpr app
+  chainl1 expr app
 ;;
 
-let pexpr_unary pexpr =
-  let p_minus = token "-" *> return (fun exp -> BinOp (Sub, Const (Int 0), exp)) in
-  let p_plus = token "+" *> return (fun exp -> exp) in
-  p_minus <|> p_plus <*> pexpr <|> pexpr
+let prs_expr_unary prs_expr =
+  let not_followed_by_space p =
+    let* c = p in
+    let* next = peek_char in
+    match next with
+    | Some (' ' | '\t' | '\n') -> fail "Unexpected space after unary operator"
+    | _ -> return c
+  in
+  let p_minus =
+    not_followed_by_space (char '-')
+    *> return (fun exp -> BinOp (Sub, Const (Int 0), exp))
+  in
+  let p_plus = not_followed_by_space (char '+') *> return (fun exp -> exp) in
+  choice [ p_minus <*> prs_expr; p_plus <*> prs_expr; prs_expr ]
 ;;
 
-let pexpr =
+let prs_expr =
   fix
   @@ fun expr ->
-  let atomary = choice [ round_par expr; pexpr_const; pexpr_var; pexpr_list expr ] in
-  let apply = pexpr_app atomary <|> atomary in
-  let unary = pexpr_unary apply <|> apply in
-  let mul = pbin_op unary (pmul <|> pdiv) in
-  let add = pbin_op mul (padd <|> psub) in
-  let compr = pbin_op add (prel <|> plogical) in
-  let branch = pexpr_branch compr <|> compr in
-  let match_exp = pexpr_match ppat branch <|> branch in
-  let tup = pexpr_tuple match_exp <|> match_exp in
-  let list = pexpr_list tup <|> tup in
-  let fun_exp = pexpr_fun ppat list <|> list in
-  let let_exp = pexpr_let fun_exp <|> fun_exp in
+  let atomary = choice [ round_par expr; prs_expr_const; prs_expr_var ] in
+  let unary = prs_expr_unary atomary <|> atomary in
+  let apply = prs_expr_app unary <|> unary in
+  let mul = prs_bin_op apply (prs_mul <|> prs_div) in
+  let add = prs_bin_op mul (prs_add <|> prs_sub) in
+  let compr = prs_bin_op add (prs_rel <|> prs_logical) in
+  let branch = prs_expr_branch compr <|> compr in
+  (* let match_exp = prs_expr_match prs_pat branch <|> branch in *)
+  let tup = prs_expr_tuple branch <|> branch in
+  let list = prs_expr_list tup <|> tup in
+  let fun_exp = prs_expr_fun prs_pat list <|> list in
+  let let_exp = prs_expr_let fun_exp <|> fun_exp in
   let_exp
 ;;
--- ELutsyuk/lib/parser/expr.mli
+++ ELutsyuk/lib/parser/expr.mli
@@ -5,18 +5,17 @@
 open Angstrom
 open Ast
 
-val pexpr_var : expr t
-val pexpr_const : expr t
-val pexpr_list : expr t -> expr t
-val pexpr_tuple : expr t -> expr t
+val prs_expr_var : expr t
+val prs_expr_const : expr t
+val prs_expr_list : expr t -> expr t
+val prs_expr_tuple : expr t -> expr t
+val prs_expr_app : expr t -> expr t
+val prs_expr_branch : expr t -> expr t
+val prs_expr_fun : pat t -> expr t -> expr t
 
-(* val pexpr_let : expr t -> expr t *)
-val pexpr_app : expr t -> expr t
-val pexpr_branch : expr t -> expr t
-val pexpr_fun : pat t -> expr t -> expr t
-val pexpr_match : pat t -> expr t -> expr t
-val pexpr_let : expr t -> expr t
-val plet_binding : expr t -> (rec_state * pat * expr) t
-val pexpr_unary : expr t -> expr t
-val pbin_op : expr t -> binop t -> expr t
-val pexpr : expr t
+(* val pexpr_match : pat t -> expr t -> expr t *)
+val prs_expr_let : expr t -> expr t
+val prs_let_binding : expr t -> let_binding t
+val prs_expr_unary : expr t -> expr t
+val prs_bin_op : expr t -> binop t -> expr t
+val prs_expr : expr t
