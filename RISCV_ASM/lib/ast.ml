963ed9532aab9ffab4baa23e31207fdfe3d06dc8
VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>
2025-03-03 18:29:23 +0300

    [WIP] refactor: change years in copyright, change description of project, interpreter beautyfying
    Signed-off-by: VyacheslavIurevich <vyacheslav.kochergin1@gmail.com>

--- RISCV_ASM/lib/ast.ml
+++ RISCV_ASM/lib/ast.ml
@@ -1,4 +1,4 @@
-(** Copyright 2024, Vyacheslav Kochergin, Roman Mukovenkov, Yuliana Ementyan *)
+(** Copyright 2024-2025, Vyacheslav Kochergin, Roman Mukovenkov, Yuliana Ementyan *)
 
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
