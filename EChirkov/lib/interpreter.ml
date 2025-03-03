2976fa6844e662fe756032d09b02a03d3c8eddcf
Chirkov Dmitri <chdmitry2@gmail.com>
2025-03-03 22:32:41 +0300

    add(test): parser
    Signed-off-by: Chirkov Dmitri <chdmitry2@gmail.com>

--- EChirkov/lib/interpreter.ml
+++ EChirkov/lib/interpreter.ml
@@ -25,10 +25,10 @@ type value =
   | VBool of bool
   | VTuple of value * value * value list
   | VFun of rec_flag * pattern * pattern list * expression * environment
-  | VList of value list 
+  | VList of value list
   | VOption of value option
   | VUnit
-  | VPrintInt 
+  | VPrintInt
 
 and environment = (string, value, Base.String.comparator_witness) Base.Map.t
 
@@ -57,7 +57,6 @@ module Environment (M : Monad) = struct
 
   (* put in env binding *)
   let extend env k v = Base.Map.update env k ~f:(fun _ -> v)
-
 end
 
 (* ========== evaluation ========== *)
@@ -66,42 +65,41 @@ module Evaluate (M : Monad) = struct
   open M
   open Environment (M)
 
+  (* (* let eval_nonrec_bs env bl =
 
-  let eval_nonrec_bs env bl =  
+     let eval_rec_bs env bl =
+     let* env2 =
+     Base.List.fold_left
+     ~f:(fun env b ->
+     let* env = env in
+     let p, e = b in
+     match p with
+     | PVar name ->
+     (match e with
+     | EFun (p1, e1) ->
+     return (extend env name (VFun (p1, Rec, e1, env)))
+     | _ -> return env)
+     | _ -> return env)
+     ~init:(return env)
+     bl
+     in
+     return env2 *)
 
-    let eval_rec_bs env bl = 
-      let* env2 =
-        Base.List.fold_left
-          ~f:(fun env b ->
-            let* env = env in
-            let p, e = b in
-            match p with
-            | PVar name ->
-              (match e with
-               | EFun (p1, e1) ->
-                 return (extend env name (VMutualFun (p1, Rec, e1, env)))
-               | _ -> return env)
-            | _ -> return env)
-          ~init:(return env)
-          bl
-      in
-      return env2
+     let eval_structure_item env = function
+     | SValue (Nonrecursive, b, bl) -> eval_nonrec_bs env (b :: bl)
+     | SValue (Recursive, b, bl) -> eval_rec_bs env (b :: bl)
+     ;;
 
-  let eval_structure_item env = function  
-    | SValue (Nonrecursive, b, bl) -> eval_nonrec_bs env (b :: bl) 
-      | SValue (Recursive, b, bl) -> eval_rec_bs env (b :: bl)
-
-  let eval_program (p : program) =
-    List.fold_left
-      ~f:(fun env structure_item ->
-        let* env = env in
-        let* env = eval_structure_item env structure_item in
-        return env)
-      ~init:(return empty)
-      p
-  ;;
+     let eval_program (p : program) =
+     List.fold_left
+     ~f:(fun env structure_item ->
+     let* env = env in
+     let* env = eval_structure_item env structure_item in
+     return env)
+     ~init:(return empty)
+     p
+     ;; *)
 end
-
-let initial_environment = Environment.extend Environment.empty "print_int" VPrintInt
-let interpret_structure_item = Evaluate.eval_structure_item initial_environment
-
+(*
+   let initial_environment = Environment.extend Environment.empty "print_int" VPrintInt
+   let interpret_structure_item = Evaluate.eval_structure_item initial_environment *)
