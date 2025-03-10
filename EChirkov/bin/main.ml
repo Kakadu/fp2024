601c816136ec59143268fe695b8baa57a2e7c634
Chirkov Dmitri <chdmitry2@gmail.com>
2025-03-10 16:51:39 +0300

    add(cli): cli for miniml
    Signed-off-by: Chirkov Dmitri <chdmitry2@gmail.com>

--- EChirkov/bin/main.ml
+++ EChirkov/bin/main.ml
@@ -2,26 +2,42 @@
 
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
+open Base
+open Stdio
+open MiniML.Ast
 open MiniML.Parser
 open MiniML.Interpreter
-open MiniML.Ast
-open Angstrom
+(* open MiniML.Inferencer *)
 
-(* let r = parse "let x = 23" *)
+let usage () =
+  printf "Usage: miniML [--interpret | --dump-ast | --infer] <file.ml>\n";
+  exit 1
+;;
 
-let r = parse_string ~consume:All p_program "let () = print_int (10 / 0)"
-
-let () =
-  match r with
-  | Ok s -> print_endline (show_program s)
-  | Error e -> print_endline e
+let read_file filename =
+  try In_channel.read_all filename with
+  | Sys_error err ->
+    printf "Error: %s\n" err;
+    exit 1
 ;;
 
 let () =
-  match r with
-  | Ok s ->
-    (match interpret s with
-     | Ok _ -> ()
-     | Error e -> print_endline ("E:" ^ pp_error e))
-  | Error e -> print_endline ("E" ^ e)
+  if Array.length Sys.argv <> 3 then usage ();
+  let mode = Sys.argv.(1) in
+  let filename = Sys.argv.(2) in
+  let source_code = read_file filename in
+  match parse source_code with
+  | Error msg -> printf "Parsing error: %s\n" msg
+  | Ok ast ->
+    (match mode with
+     | "--dump-ast" -> printf "AST:\n%s\n" (show_program ast)
+     | "--interpret" ->
+       (match interpret ast with
+        | Ok _ -> ()
+        | Error err -> printf "Interpretation error: %s\n" (pp_error err))
+     (* | "--infer" ->
+        (match inference ast with
+        | Ok _ -> printf "Type inference successful\n"
+        | Error msg -> printf "Type inference error: %s\n" msg) *)
+     | _ -> usage ())
 ;;
