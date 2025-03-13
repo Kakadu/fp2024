0adcf1ac0663e4305d87a84c88c510f5d7c962cc
kinokotakenoko9 <chdmitry2@gmail.com>
2025-03-13 19:46:56 +0300

    fix(ci): rename
    Signed-off-by: kinokotakenoko9 <chdmitry2@gmail.com>

--- EChirkov/tests/inferencer_tests.ml
+++ EChirkov/tests/inferencer_tests.ml
@@ -2,16 +2,16 @@
 
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
-open MiniML.Parser
-open MiniML.Inferencer
+open EChirkov.Parser
+open EChirkov.Inferencer
 
 let test_inferencer s =
   match parse s with
   | Ok s ->
     (* print_endline (show_program s); *)
     (match inference s with
-     | Ok env -> MiniML.Inferencer.print_env env
-     | Error e -> Format.printf "Type inference error: %a" MiniML.Inferencer.pp_error e)
+     | Ok env -> EChirkov.Inferencer.print_env env
+     | Error e -> Format.printf "Type inference error: %a" EChirkov.Inferencer.pp_error e)
   | Error _ -> print_endline "Parsing error"
 ;;
 
