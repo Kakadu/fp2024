4a6e5ecad3534114c003f44153871dac27f9d0e4
Daniel Vlasenco <vlasenko.daniil26@gmail.com>
2025-03-22 00:03:04 +0300

    feat: add manytests and a few repl options
    Signed-off-by: Daniel Vlasenco <vlasenko.daniil26@gmail.com>

--- FSharpUnitsOfMeasure/bin/repl.ml
+++ FSharpUnitsOfMeasure/bin/repl.ml
@@ -7,8 +7,12 @@ open Checks
 open Interp.Interpret
 open Interp.Misc
 open Format
-
-type config = {mutable file_path : string option}
+ 
+type config =
+  { mutable file_path : string option
+  ; mutable do_not_type : bool
+  ; mutable greet_user : bool
+  }
 
 let pprog = Angstrom.parse_string ~consume:Angstrom.Consume.All pprog
 
@@ -17,10 +21,7 @@ let help_msg =
    F# with units of measure interpreter\n\n\
    Read and interpret file: dune exec ./bin/repl.exe --file <filepath>\n\
    REPL: dune exec ./bin/repl.exe\n\n\
-
-   REPL commands:\n\
-   help         Display usage message\n\
-   quit         Quit the REPL mode\n"
+"
 ;;
 
 let greetings_msg =
@@ -55,19 +56,18 @@ let run_single options =
   let run text =
     match pprog text with
     | Error _ -> print_endline (Format.asprintf "Syntax error");
-    print_endline hori_line
     (* env *)
     | Ok ast ->
       (* Infer *)
+      if options.do_not_type = false then print_endline "Inference can't be done. To turn off this message, run REPL with --do-not-type option";
       match eval ast with
       | Ok (env, out_lst) ->
         List.iter (fun v -> match v with
         | Ok v' -> print_endline (Format.asprintf "- : %s = %a" "<type>" pp_value v')
         | _ -> ()) out_lst;
         pp_env env;
-        print_endline hori_line;
       | Error e -> print_endline (Format.asprintf "Interpreter error: %a" pp_error e);
-      print_endline hori_line
+      if options.greet_user then print_endline hori_line else print_endline "";
     in
     let open In_channel in
     match options.file_path with
@@ -89,15 +89,21 @@ let run_single options =
 ;;
 
 let () =
-  print_endline greetings_msg;
-  let options = {file_path = None} in
+  let options = {file_path = None; do_not_type = false; greet_user = true} in
   let () =
     let open Arg in
     parse
       [( "--file"
         , String (fun filename -> options.file_path <- Some filename)
         , "Read code from the file and interpret" )
-      ] (fun _ -> exit 1) "REPL"
+      ;( "--do-not-type"
+        , Unit (fun () -> options.do_not_type <- true)
+        , "Turn off inference" )
+      ;( "--no-hi"
+        , Unit (fun () -> options.greet_user <- false)
+        , "Turn off greetings message" )] (fun _ -> exit 1) "REPL"
   in
+  if options.greet_user then
+  print_endline greetings_msg;
   run_single options
 ;;
--- FSharpUnitsOfMeasure/tests/parse/unitTests/tests_structure.ml
+++ FSharpUnitsOfMeasure/tests/parse/unitTests/tests_structure.ml
@@ -133,18 +133,14 @@ let%expect_test "parse example 1 program" =
 ;;
 
 let%expect_test "parse example 2 program" =
-  pp
-    pprint_program
-    pprog
+  run_prog
     {| let _Sy = "<X&D(" and _QQf = tOWs and _ = -3625.090462<b9o'5>;; |};
   [%expect {|
     let _Sy = "<X&D(" and _QQf = tOWs and _ = -3625.090462<b9o'5> |}]
 ;;
 
 let%expect_test "parse example 3 program" =
-  pp
-    pprint_program
-    pprog
+  run_prog
     {| match ('X' : char) with _ -> _j531 | _O -> false | "$TNsq^]am" -> a47 | t5 -> gs | __U4' -> g | zqy'p -> true;;
  |};
   [%expect
@@ -152,39 +148,8 @@ let%expect_test "parse example 3 program" =
     match ('X' : char) with _ -> _j531 | _O -> false | "$TNsq^]am" -> a47 | t5 -> gs | __U4' -> g | zqy'p -> true |}]
 ;;
 
-let%expect_test "parse example 4 program" =
-  pp
-    pprint_program
-    pprog
-    {|
-  let rec true = v and _ = q and _ = -4532424078885192588 in _p;;
-  
-  let 1483337398865069489 = kr and _ = _6r and _ = viwYZ and -4486306013525438741 = 3265597.524462 and _ = cDCL and l7 = false and "A4%" = -546.405974;;
-  
-  let 'A' = 0.000023 and lO1xQ = true and false = -3569041840172302343 and _ = -3411816405488708691 and -774524459665610219 = "ID]G<b[" and r8hu = y7O0 and aSS0b = -1580.381791 and false = 3014.442914 and eO_S8 = -0.103727<((dd_ / (((((bht * (1 * xd)) * (fkQC ^ 99)) / ((g ^ 64) / (1 ^ 5))) * (((1 ^ 37) * 1) / 1)) / a_2N)) ^ 49) ^ 57>;;
-  
-  let _ = '(' and j18jw = _Z6Qn and "zy@&qhIfoFgqh=&7R)\nS@E#\ned{7" = true and _ = 'Z' and _e'Jt = acp;;
-  
-  match let y = zdB and true = z1'13 and i = 4541.311808 in -0.069444 with _ -> _v9x8 | d -> "*.X4eW*<i,y{\n=8X$/2^^V% t3K6SigH?(WH<1'nq{WmS6.Npq:p[a5_<^H>=p;{Eq*TA!`n,!KsD7VO#D0m)GiY" | _'sH0 -> false | c -> pR8q | -2407035380919485009 -> "n=" | 2179.187086 -> false | -0.000855<xM4> -> _C;;
-  
-    |};
-  [%expect
-    {|
-    let rec true = v and _ = q and _ = -4532424078885192588 in _p
-
-    let 1483337398865069489 = kr and _ = _6r and _ = viwYZ and -4486306013525438741 = 3265597.52446 and _ = cDCL and l7 = false and "A4%" = -546.405974
-
-    let 'A' = 2.3e-05 and lO1xQ = true and false = -3569041840172302343 and _ = -3411816405488708691 and -774524459665610219 = "ID]G<b[" and r8hu = y7O0 and aSS0b = -1580.381791 and false = 3014.442914 and eO_S8 = -0.103727<((dd_ / (((((bht * (1 * xd)) * (fkQC ^ 99)) / ((g ^ 64) / (1 ^ 5))) * (((1 ^ 37) * 1) / 1)) / a_2N)) ^ 49) ^ 57>
-
-    let _ = '(' and j18jw = _Z6Qn and "zy@&qhIfoFgqh=&7R)\\nS@E#\\ned{7" = true and _ = 'Z' and _e'Jt = acp
-
-    match let y = zdB and true = z1'13 and i = 4541.311808 in -0.069444 with _ -> _v9x8 | d -> "*.X4eW*<i,y{\\n=8X$/2^^V% t3K6SigH?(WH<1'nq{WmS6.Npq:p[a5_<^H>=p;{Eq*TA!`n,!KsD7VO#D0m)GiY" | _'sH0 -> false | c -> pR8q | -2407035380919485009 -> "n=" | 2179.187086 -> false | -0.000855<xM4> -> _C |}]
-;;
-
 let%expect_test "parse example 5 program" =
-  pp
-    pprint_program
-    pprog
+  run_prog
     {|
   let rec -2202013031993883487<o0H6t * (((1 * (((1 / 1) / 1) ^ 2)) * (1 * (1 * ((rQ1_R ^ 7) * 1)))) ^ 6)> = y ;;
     |};
@@ -194,9 +159,7 @@ let%expect_test "parse example 5 program" =
 ;;
 
 let%expect_test "parse example 6 program" =
-  pp
-    pprint_program
-    pprog
+  run_prog
     {|
 let rec true = -4534607695307062870<((v / 1) ^ 5) * ((_L0 * m) ^ 7)>;;   |};
   [%expect {|
@@ -211,9 +174,7 @@ let%expect_test "parse example 7 program" =
 ;;
 
 let%expect_test "parse example 8 program" =
-  pp
-    pprint_program
-    pprog
+  run_prog
     {|
     (match a with 0. -> -1312004488025042530 | _ -> "", match a with a -> a | a -> a, a);;
  |};
